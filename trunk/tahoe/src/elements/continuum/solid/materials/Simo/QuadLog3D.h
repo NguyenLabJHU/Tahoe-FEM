71ceb1654895dc5a8e1d4c2895c2c38f13d6f09d
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-05-04 19:12:46 +0000

    move to using SpectralDecompT's Jacobi iteration solver

--- trunk/tahoe/src/elements/continuum/solid/materials/Simo/QuadLog3D.h
+++ trunk/tahoe/src/elements/continuum/solid/materials/Simo/QuadLog3D.h
@@ -1,4 +1,4 @@
-/* $Id: QuadLog3D.h,v 1.2 2001-02-20 00:28:19 paklein Exp $ */
+/* $Id: QuadLog3D.h,v 1.3 2001-05-04 19:12:46 paklein Exp $ */
 /* created: paklein (06/27/1997)                                          */
 /* Hyperelastic material governed by quadratic logarithmic potential.     */
 
@@ -8,6 +8,7 @@
 /* base classes */
 #include "FDStructMatT.h"
 #include "IsotropicT.h"
+#include "SpectralDecompT.h"
 
 class QuadLog3D: public FDStructMatT, public IsotropicT
 {
@@ -42,67 +43,26 @@ protected:
 	void ComputeCauchy(const dSymMatrixT& b, dSymMatrixT& cauchy);
 	double ComputeEnergy(const dArrayT& loge);
 
-	/* compute spectral decomposition of b
-*
-* NOTE: Repeated eigenvalues are perturbed
-	 */
-	void SpectralDecomp(const dSymMatrixT& b, dArrayT& eigs, bool perturb_repeated);
-
 	/* compute logarithmic stretches from the given eigenvalues */
-	void LogStretches(dArrayT& eigs);
-
-	/* components of algorithmic tangent moduli */
-	void Set_b_Tensor(const dSymMatrixT& b);
-	const dMatrixT& SpatialTensor(const dSymMatrixT& b, int A);
-	
-private:
-
-	/* returns min */
-	static double Min(double d1, double d2, double d3);
+	void LogStretches(const dArrayT& eigs);
 
 protected:
+
+	/* spectral decomposition solver */
+	SpectralDecompT fSpectral;
 
 	/* return value */
 	dSymMatrixT	fStress;
 	dMatrixT	fModulus;
 
-	/* fixed forms */
-	dSymMatrixT	fISym;
-	dMatrixT	fIdentity3;
-	dMatrixT	f1x1;
-	dMatrixT	fDevOp3;
+	/* deviatoric operator */
+	dSymMatrixT fDevOp3;
 
 	/* spectral decomposition */
-	dArrayT	 fEigs;  //principal value of b
-	dArrayT	 floge;  //logarithmic stretches
-	dArrayT	 fBeta;  //principal stresses
-	dMatrixT fEigMod;//modulus in principal stretches
-
-	ArrayT<dSymMatrixT> fm; //array of rank 1 matrices	
-	dSymMatrixT& fn0xn0; //rank 1 principal direction matricies
-	dSymMatrixT& fn1xn1;
-	dSymMatrixT& fn2xn2;
-	
-	/* decomp work space */
-	dSymMatrixT fm1;
-	dSymMatrixT fm2;
-	
-	/* spatial tensor work space */
-	dMatrixT   fSpatTensor;
-	dMatrixT   fc_b; //part of spatial tensor dependent only on b
-	dMatrixT   fRank4;
-	dSymMatrixT fRank2;
-	dMatrixT   fIdentity4;
+	dArrayT	fEigs;  //principal value of b
+	dArrayT	floge;  //logarithmic stretches
+	dArrayT	fBeta;  //principal stresses
+	dSymMatrixT fEigMod;//modulus in principal stretches
 };
-
-/* inline functions */
-
-/* returns max */
-inline double QuadLog3D::Min(double d1, double d2, double d3)
-{
-	return ( (d1 < d2) ?
-			((d1 < d3) ? d1 : d3) :
-			((d2 < d3) ? d2 : d3) );
-}
 
 #endif /* _QUAD_LOG_3D_H_ */
