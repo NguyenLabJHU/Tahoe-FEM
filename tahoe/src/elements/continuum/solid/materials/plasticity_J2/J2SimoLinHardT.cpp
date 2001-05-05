d4764a4a9d67e2c67b3c2af261d006662d374bb6
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-05-05 19:26:29 +0000

    - removed redundant calls - corrected update to trial elastic state

--- tahoe/src/elements/continuum/solid/materials/plasticity_J2/J2SimoLinHardT.cpp
+++ tahoe/src/elements/continuum/solid/materials/plasticity_J2/J2SimoLinHardT.cpp
@@ -1,4 +1,4 @@
-/* $Id: J2SimoLinHardT.cpp,v 1.2 2001-05-01 23:22:54 paklein Exp $ */
+/* $Id: J2SimoLinHardT.cpp,v 1.3 2001-05-05 19:26:29 paklein Exp $ */
 /* created: paklein (06/19/1997)                                          */
 /* Interface for a elastoplastic material that is linearly                */
 /* isotropically elastic subject to the Huber-von Mises yield             */
@@ -31,34 +31,26 @@ const int kIsElastic = 1;
 /* init flag values */
 const int kIsNotInit = 0;
 const int kIsInit    = 1;
-	
-/* class constants */
-const int	kNumInternal   = 6;
-const int	kalpha         = 0; /* isotropic hardening         */
-const int	kstressnorm    = 1; /* norm of the relative stress */
-const int	kdgamma        = 2; /* consistency parameter       */
-const int	kftrial        = 3; /* yield function value        */
-const int	kI_bar         = 4; /* trace of b_e_bar            */
-const int	kDetF_tot      = 5; /* determinant of total F      */
 
-const double sqrt23        = sqrt(2.0/3.0);
-const double kYieldTol     = 1.0e-10;
+const double sqrt23    = sqrt(2.0/3.0);
+const double kYieldTol = 1.0e-10;
 
 const int kNSD = 3;
+
+/* static variables */
+const int J2SimoLinHardT::kNumInternal = 7;
 
 /* constructor */
 J2SimoLinHardT::J2SimoLinHardT(ifstreamT& in, int num_ip, double mu):
 	J2PrimitiveT(in),
 	fNumIP(num_ip),
 	fmu(mu),
-	fbelastic(kNSD),
 	fStressCorr(kNSD),
 	fModuliCorr(dSymMatrixT::NumValues(kNSD)),
 	fRelStress(kNSD),
-	fDev_b(kNSD),
-	fUnitNorm(kNSD),
 	f_f_bar(kNSD),
-	f_b_trial(kNSD),
+	fb_bar_trial(kNSD),
+	fbeta_bar_trial(kNSD),
 	fMatrixTemp1(kNSD),
 	fMatrixTemp2(kNSD),
 	fRed2Temp(kNSD),
@@ -68,12 +60,13 @@ J2SimoLinHardT::J2SimoLinHardT(ifstreamT& in, int num_ip, double mu):
 
 }
 
-/* returns isochoric elastic stretch */
-const dSymMatrixT& J2SimoLinHardT::ElasticStretch(const dMatrixT& F_total,
+/** compute trial elastic state - return reference to isochoric,
+ * trial elastic stretch */
+const dSymMatrixT& J2SimoLinHardT::TrialElasticState(const dMatrixT& F_total,
 	const dMatrixT& f_relative, ElementCardT& element, int ip)
 {
 	/* compute left Cauchy-Green */
-	if (element.IsAllocated()) /* element is plastic */
+	if (element.IsAllocated()) /* element has been plastic */
 	{
 		/* load internal variables */
 		LoadData(element, ip);
@@ -86,23 +79,35 @@ const dSymMatrixT& J2SimoLinHardT::ElasticStretch(const dMatrixT& F_total,
 			Flags[ip] = kIsElastic;
 		}
 	
-		/* isochoric */
-		f_f_bar.SetToScaled( pow(f_relative.Det(),-1.0/3.0), f_relative);
+		/* isochoric relative deformation gradient */
+		f_f_bar.SetToScaled(pow(f_relative.Det(),-1.0/3.0), f_relative);
 
-		fb_bar.ToMatrix(fMatrixTemp1);		
-		fMatrixTemp2.MultAB(f_f_bar, fMatrixTemp1);
-		f_b_trial.MultABT(fMatrixTemp2, f_f_bar);
+		/* push stretch forward */
+		fb_bar_trial.MultQBQT(f_f_bar, fb_bar);
+
+		/* push kinematic hardening forward */
+		fbeta_bar_trial.MultQBQT(f_f_bar, fbeta_bar);
+		ftrace_beta_trial = fbeta_bar_trial.Trace();
+		fbeta_bar_trial.PlusIdentity(-ftrace_beta_trial/3.0); /* deviatoric part */
+		
+		/* save */
+		fInternal[kDetF_tot] = F_total.Det();
+		fb_bar_trial_ = fb_bar_trial;
+		fbeta_bar_trial_ = fbeta_bar_trial;
 	}
 	else /* element is elastic */
 	{
-		f_b_trial.MultABT(F_total, F_total);
-		f_b_trial *= pow( F_total.Det(), -2.0/3.0);
+		/* trial stretch */
+		fb_bar_trial.MultAAT(F_total);
+		fb_bar_trial *= pow(F_total.Det(), -2.0/3.0);
+		
+		/* trial kinematic hardening */
+		fbeta_bar_trial = 0.0;
+		ftrace_beta_trial = 0.0;
 	}
 
 	/* make reduced index */
-	fbelastic.FromMatrix(f_b_trial);
-	
-	return fbelastic;
+	return fb_bar_trial;
 }
 
 /* return the correction to stress vector computed by the mapping the
@@ -116,6 +121,9 @@ const dSymMatrixT& J2SimoLinHardT::StressCorrection(const dMatrixT& F_total,
 	{
 		/* new element */
 		AllocateElement(element);
+		
+		/* set trial state and load data */
+		TrialElasticState(F_total, f_relative, element, ip);
 
 		/* initialize element data */
 		PlasticLoading(F_total, f_relative, element, ip);
@@ -134,13 +142,47 @@ const dSymMatrixT& J2SimoLinHardT::StressCorrection(const dMatrixT& F_total,
 		if (ftrial > kYieldTol)
 		{	
 			/* constants */
-			double I_bar = fInternal[kI_bar];
-
+			double alpha  = fInternal[kalpha];
+			double mu_bar = fInternal[kmu_bar];
+			double mu_bar_bar = fInternal[kmu_bar_bar];
+			
 			/* update internal variables */
-			dgamma = (3.0*ftrial)/(6.0*fmu*I_bar + 2.0*fH_bar);
+			dgamma = ftrial/(2.0*mu_bar_bar)/
+				(1.0 + (dH(alpha)/3.0/fmu) + (dK(alpha)/3.0/mu_bar_bar));
 	
 			/* plastic correction */
-			fStressCorr.SetToScaled(-2.0*fmu*I_bar*dgamma,fUnitNorm);
+			fStressCorr.SetToScaled(-2.0*mu_bar_bar*dgamma, fUnitNorm);
+			
+			/* debugging - check the results of the return mapping */
+			bool check_map = false;
+			if (check_map)
+			{
+				/* internal variables */
+				double alpha = fInternal[kalpha];
+				double mu_bar_bar = fInternal[kmu_bar_bar];
+				dSymMatrixT beta_bar = fbeta_bar;
+
+				/* update variables */
+				double k = 2.0*mu_bar_bar*dgamma/3.0/fmu;
+				alpha += sqrt23*dgamma;
+				beta_bar.AddScaled(k*dH(alpha), fUnitNorm);
+				
+				/* compute relative stress */
+				dSymMatrixT relative_stress(3);
+				relative_stress.Deviatoric(fb_bar_trial);
+				relative_stress *= fmu;
+				relative_stress -= fbeta_bar_trial;
+
+				/* return mapping */
+				relative_stress += fStressCorr;
+
+				/* compute update yield condition */
+				double f = YieldCondition(relative_stress, alpha);
+				double t = sqrt(relative_stress.ScalarProduct())/sqrt23;
+				cout << "\n J2SimoLinHardT::StressCorrection: check\n"
+				     <<   "          f = " << f << '\n'
+				     <<   " ||dev[t]|| = " << t << endl;
+			}			
 		}
 		else
 			dgamma = 0.0;
@@ -168,42 +210,37 @@ const dMatrixT& J2SimoLinHardT::ModuliCorrection(ElementCardT& element, int ip)
 		/* fetch element data */
 		double stressnorm = fInternal[kstressnorm];
 		double dgamma     = fInternal[kdgamma];		
-		double I_bar      = fInternal[kI_bar];
-		double mu_bar     = fmu*I_bar;
+		double alpha      = fInternal[kalpha];		
+		double mu_bar     = fInternal[kmu_bar];
+		double mu_bar_bar = fInternal[kmu_bar_bar];
+			
+		/* scaling factors */
+		double f0 = 2.0*mu_bar*dgamma/stressnorm;
+		double d0 = 1.0 + dH(alpha)/3.0/fmu + dK(alpha)/3.0/mu_bar_bar;
 
-		/* the betas */
-		double beta0 = 1.0 + fH_bar/(3.0*mu_bar);
-		double beta1 = 2.0*mu_bar*dgamma/stressnorm;
-//note: beta2 = 0.0 for perfect plastic
-		double beta2 =(1.0 - 1.0/beta0)*(2.0*stressnorm*dgamma)/(3.0*mu_bar);
-//		double beta2 =(1.0 - 1.0/beta0)*(2.0*dgamma)/(3.0*mu_bar);
-		double beta3 = 1.0/beta0 - beta1 + beta2;
-		double beta4 =(1.0/beta0 - beta1)*(stressnorm/mu_bar);
+		double f1 = 1.0/d0 - f0;
+		double d1 = 2.0*mu_bar_bar*f1 - (4.0/3.0)*dgamma*((1.0 + dH(alpha)/3.0/fmu)/d0 - 1.0);
 		
+		double d2 = 2.0*stressnorm*f1;
+
 		/* assemble deviatoric moduli contribution */
 		fRed4Temp1.ReducedIndexDeviatoric();
-		fModuliCorr.AddScaled(-beta1*2.0*mu_bar,fRed4Temp1);
+		fModuliCorr.AddScaled(-2.0*mu_bar_bar*f0, fRed4Temp1);
 
 		fRed2Temp.Identity();
-		fRed4Temp1.Outer(fUnitNorm,fRed2Temp);
+		fRed4Temp1.Outer(fUnitNorm, fRed2Temp);
 		fRed4Temp1.Symmetrize();
-		fModuliCorr.AddScaled(beta1*(4.0/3.0)*stressnorm,fRed4Temp1);
+		fModuliCorr.AddScaled(f0*(4.0/3.0)*stressnorm, fRed4Temp1);
 		
-		fRed4Temp1.Outer(fUnitNorm,fUnitNorm);
-		fModuliCorr.AddScaled(-2.0*mu_bar*beta3,fRed4Temp1);
+		fRed4Temp1.Outer(fUnitNorm, fUnitNorm);
+		fModuliCorr.AddScaled(-d1, fRed4Temp1);
 
-		fRed2Temp.MultAB(fUnitNorm,fUnitNorm);
+		fRed2Temp.MultAB(fUnitNorm, fUnitNorm);
 		fRed2Temp.Deviatoric();
-		//fRed4Temp1.Outer(fUnitNorm,fRed2Temp);
-		fRed4Temp1.Outer(fRed2Temp,fUnitNorm);
-		//fRed4Temp1.Symmetrize();
-		fModuliCorr.AddScaled(-2.0*mu_bar*beta4,fRed4Temp1);
-		//fModuliCorr.AddScaled(-4.0*mu_bar*beta4,fRed4Temp1);
+		fRed4Temp1.Outer(fUnitNorm, fRed2Temp);
+		fModuliCorr.AddScaled(-d2, fRed4Temp1);
 
-		//make symmetric
-		//fModuliCorr.Symmetrize();
-
-		/* J factor */
+		/* J factor: Simo's Kirchhoff to spatial tangent modulus */
 		//fModuliCorr /= fInternal[kDetF_tot];		
 	}
 
@@ -218,10 +255,13 @@ void J2SimoLinHardT::AllocateElement(ElementCardT& element)
 	i_size += fNumIP; //flags
 
 	int d_size = 0;
+	d_size += kNumInternal*fNumIP;                 //fInternal
 	d_size += dSymMatrixT::NumValues(kNSD)*fNumIP; //fb_bar
-	d_size += dSymMatrixT::NumValues(kNSD)*fNumIP; //fUnitNorm
 	d_size += dSymMatrixT::NumValues(kNSD)*fNumIP; //fBeta
-	d_size += kNumInternal*fNumIP;          //fInternal
+
+	d_size += dSymMatrixT::NumValues(kNSD)*fNumIP; //fUnitNorm
+	d_size += dSymMatrixT::NumValues(kNSD)*fNumIP; //fb_bar_trial_
+	d_size += dSymMatrixT::NumValues(kNSD)*fNumIP; //fbeta_bar_trial_
 
 	/* construct new plastic element */
 	element.Allocate(i_size, d_size);
@@ -229,14 +269,6 @@ void J2SimoLinHardT::AllocateElement(ElementCardT& element)
 	/* initialize values */
 	element.IntegerData() = kNotInit;
 	element.DoubleData()  = 0.0;
-//	for (int ip = 0; ip < fNumInt; ip++)
-//	{
-//		/* fetch element data */
-//		LoadData(newplastic,ip);
-//		
-//		/* initialize intermediate config */			
-//		fb_bar.Identity();
-//	}
 }
 
 /***********************************************************************
@@ -262,22 +294,24 @@ void J2SimoLinHardT::Update(ElementCardT& element)
 		LoadData(element,ip);
 		
 		/* elastic update */
-		fb_bar.SetToScaled(fInternal[kstressnorm]/fmu,fUnitNorm);
-		fb_bar.PlusIdentity(fInternal[kI_bar]);
+		fb_bar = fb_bar_trial_;
+		fbeta_bar = fbeta_bar_trial_;
 	
 		/* plastic update */
 		if (Flags[ip] ==  kIsPlastic)
 		{
+			/* factors */
+			double alpha = fInternal[kalpha];
+			double dgamma = fInternal[kdgamma];
+			double mu_bar_bar = fInternal[kmu_bar_bar];
+			double k = 2.0*mu_bar_bar*dgamma/fmu;
+		
 			/* internal variables */
-			fInternal[kalpha] += sqrt23*fInternal[kdgamma];
-	
-			/* kinematic hardening */
-			//fBeta.AddScaled(2.0*(1.0-ftheta)*fH_bar*dgamma/3.0, fUnitNorm);
-			//??What's the finite deform version??
-							
-			fb_bar.AddScaled(-2.0*fInternal[kI_bar]*fInternal[kdgamma],
-			                  fUnitNorm);
-			
+			fInternal[kalpha] += sqrt23*dgamma;
+			fbeta_bar.AddScaled(k*dH(alpha)/3.0, fUnitNorm);
+				
+			/* update intermediate configuration */
+			fb_bar.AddScaled(-k, fUnitNorm);
 		}
 	}
 }
@@ -309,11 +343,13 @@ void J2SimoLinHardT::InitIntermediate(const dMatrixT& F_total,
 	fMatrixTemp2.MultAB(fMatrixTemp1, F_total);
 	
 	/* b */
-	f_b_trial.MultABT(fMatrixTemp2, fMatrixTemp2);
+	fb_bar.MultAAT(fMatrixTemp2);
 	
-	/* b_bar */
-	f_b_trial *= pow(f_b_trial.Det(), -1.0/3.0);
-	fb_bar.FromMatrix(f_b_trial);
+	/* b_bar - isochoric b */
+	fb_bar *= pow(fb_bar.Det(), -1.0/3.0);
+	
+	/* initialize kinematic hardening */
+	fbeta_bar = 0.0;
 }
 
 /* load element data */
@@ -327,10 +363,12 @@ void J2SimoLinHardT::LoadData(const ElementCardT& element, int ip)
 	int offset    = stressdim*fNumIP;
 	int dex       = ip*stressdim;
 
-	        fb_bar.Set(        kNSD, &d_array[           dex]);
-	     fUnitNorm.Set(        kNSD, &d_array[  offset + dex]);
-	         fBeta.Set(        kNSD, &d_array[2*offset + dex]);
-	     fInternal.Set(kNumInternal, &d_array[3*offset + ip*kNumInternal]);     	
+	fb_bar.Set(kNSD, &d_array[dex]);
+    fUnitNorm.Set(kNSD, &d_array[offset + dex]);
+    fbeta_bar.Set(kNSD, &d_array[2*offset + dex]);
+    fb_bar_trial_.Set(kNSD, &d_array[3*offset + dex]);
+    fbeta_bar_trial_.Set(kNSD, &d_array[4*offset + dex]);
+    fInternal.Set(kNumInternal, &d_array[5*offset + ip*kNumInternal]);     	
 }
 
 /*
@@ -343,19 +381,18 @@ void J2SimoLinHardT::LoadData(const ElementCardT& element, int ip)
 int J2SimoLinHardT::PlasticLoading(const dMatrixT& F_total,
 	const dMatrixT& f_relative, ElementCardT& element, int ip)
 {
+	/* compute relative stress */
+	fRed2Temp.Deviatoric(fb_bar_trial);
+	fRelStress.SetToCombination(fmu, fRed2Temp, -1.0, fbeta_bar_trial);
+
 	/* not yet plastic */
-	if (!element.IsAllocated())
-		return(YieldCondition(RelativeStress(F_total, f_relative, element),
-					0.0) > kYieldTol);
-	/* already plastic */
-	else
+	if (!element.IsAllocated()) 
+		return YieldCondition(fRelStress, 0.0) > kYieldTol;
+	else /* already plastic */
 	{
 		/* get flags */
 		iArrayT& Flags = element.IntegerData();
 
-		/* load internal variables */
-		LoadData(element,ip);
-	
 		/* initialize intermediate state */
 		if (Flags[ip] == kNotInit)
 		{
@@ -364,13 +401,10 @@ int J2SimoLinHardT::PlasticLoading(const dMatrixT& F_total,
 		}
 	
 		/* set internal variables */		
-		fInternal[kftrial] = YieldCondition(
-								RelativeStress(F_total, f_relative, element),
-								fInternal[kalpha] );
+		fInternal[kftrial]     = YieldCondition(fRelStress, fInternal[kalpha]);
 		fInternal[kstressnorm] = sqrt(fRelStress.ScalarProduct());
-//		fInternal[kI_bar]      = f_b_trial.Trace()/3.0;		
-		fInternal[kI_bar]      = fb_bar.Trace()/3.0;
-		fInternal[kDetF_tot]   = F_total.Det();
+		fInternal[kmu_bar]     = fmu*fb_bar_trial.Trace()/3.0;
+		fInternal[kmu_bar_bar] = fInternal[kmu_bar] - ftrace_beta_trial/3.0;
 		
 		/* compute unit normal */
 		fUnitNorm.SetToScaled(1.0/fInternal[kstressnorm], fRelStress);
@@ -380,50 +414,18 @@ int J2SimoLinHardT::PlasticLoading(const dMatrixT& F_total,
 		{
 			/* compute unit normal */
 //			double& norm = fInternal[kstressnorm];
-
 //			norm = sqrt(fRelStress.ScalarProduct());
 //			fUnitNorm.SetToScaled(1.0/norm, fRelStress);
 		
 			/* set flag */
-			Flags[ip] = kIsPlastic;
-	
+			Flags[ip] = kIsPlastic;	
 			return 1;
 		}
-		/* elastic */
-		else
+		else /* elastic */
 		{
 			/* set flag */
 			Flags[ip] = kIsElastic;
-			
 			return 0;
 		}
 	}
 }	
-
-/* computes the relative stress and returns a reference to the
-* relative stress in fRelStress */
-dSymMatrixT& J2SimoLinHardT::RelativeStress(const dMatrixT& F_total,
-	const dMatrixT& f_relative, const ElementCardT& element)
-{
-	/* compute left Cauchy-Green */
-	if (element.IsAllocated()) /* element is plastic */
-	{
-		/* isochoric */
-		f_f_bar.SetToScaled( pow(f_relative.Det(),-1.0/3.0), f_relative);
-
-		fb_bar.ToMatrix(fMatrixTemp1);		
-		fMatrixTemp2.MultAB(f_f_bar, fMatrixTemp1);
-		f_b_trial.MultABT(fMatrixTemp2, f_f_bar);
-	}
-	else /* element is elastic */
-	{
-		f_b_trial.MultABT(F_total, F_total);
-		f_b_trial *= pow( F_total.Det(), -2.0/3.0);
-	}
-
-	/* deviatoric trial stress */
-	fDev_b.FromMatrix(f_b_trial);
-	fRelStress.SetToScaled(fmu,fDev_b.Deviatoric());
-	
-	return fRelStress;
-}
--- tahoe/src/elements/continuum/solid/materials/plasticity_J2/J2SimoLinHardT.h
+++ tahoe/src/elements/continuum/solid/materials/plasticity_J2/J2SimoLinHardT.h
@@ -1,16 +1,12 @@
-/* $Id: J2SimoLinHardT.h,v 1.1.1.1 2001-01-29 08:20:30 paklein Exp $ */
+/* $Id: J2SimoLinHardT.h,v 1.2 2001-05-05 19:26:29 paklein Exp $ */
 /* created: paklein (06/19/1997)                                          */
-/* NOTE: THIS FORMULATION OF PLASTICITY RESULTS IN A NON-SYMMETRIC        */
-/* CONSISTENT TANGENT MODULI MATRIX. DO NOT USE UNLESS THE                */
-/* ELEMENT FORMULATION AND GLOBAL EQUATION MATRIX HAVE BEEN               */
-/* CHECK FOR ASSUMPTIONS ABOUT SYMMETRY                                   */
 /* Interface for a elastoplastic material that is linearly                */
 /* isotropically elastic subject to the Huber-von Mises yield             */
 /* condition as fYield with kinematic/isotropic hardening laws            */
 /* given by:                                                              */
-/* 		H(a) = (1 - ftheta) fH_bar a                                         */
-/* K(a) = fYield + ftheta fH_bar a                                        */
-/* 		where a is the internal hardening variable                           */
+/* 		H(a) = (1 - ftheta) fH_bar a                                      */
+/*      K(a) = fYield + ftheta fH_bar a                                   */
+/* 		where a is the internal hardening variable                        */
 /* 	Note: all calculations are peformed in 3D.                            */
 
 #ifndef _J2_SIMO_LIN_HARD_T_H_
@@ -28,87 +24,107 @@
 /* forward declarations */
 class ElementCardT;
 
+/** finite strain, J2 elastoplasticity following framework in
+ * Simo, J.C. (1988) CMAME v66 and v68. */
 class J2SimoLinHardT: public J2PrimitiveT
 {
 public:
 
-	/* constructor */
+	/** constructor */
 	J2SimoLinHardT(ifstreamT& in, int num_ip, double mu);
+
+protected:
+
+	/** number of stored variables */
+	static const int kNumInternal;
+
+	/** index of stored values */
+	enum VariablesT {
+        kalpha = 0, /* isotropic hardening         */
+   kstressnorm = 1, /* norm of the relative stress */
+       kdgamma = 2, /* consistency parameter       */
+       kftrial = 3, /* yield function value        */
+       kmu_bar = 4, 
+   kmu_bar_bar = 5,
+     kDetF_tot = 6};/* determinant of total F      */
 	
-	/* returns isochoric elastic stretch */
-	const dSymMatrixT& ElasticStretch(const dMatrixT& F_total,
+	/** compute trial elastic state - return reference to isochoric,
+	 * trial elastic stretch */
+	const dSymMatrixT& TrialElasticState(const dMatrixT& F_total,
 		const dMatrixT& f_relative, ElementCardT& element, int ip);
 			
-	/* return the correction to stress vector computed by the mapping the
+	/** return the correction to stress vector computed by the mapping the
 	 * stress back to the yield surface */
 	const dSymMatrixT& StressCorrection(const dMatrixT& F_total,
 		const dMatrixT& f_relative, ElementCardT& element, int ip);
 
-	/* return the correction to moduli due to plasticity (if any)
+	/** return the correction to moduli due to plasticity (if any)
 	 *
 	 * Note: Return mapping occurs during the call to StressCorrection.
 	 *       The element passed in is already assumed to carry current
 	 *       internal variable values. */
 	const dMatrixT& ModuliCorrection(ElementCardT& element, int ip);
 
-	/* allocate element storage */
+	/** allocate element storage */
 	void AllocateElement(ElementCardT& element);
 
-protected:
-
-	/* element level data */
+	/** element level data */
 	void Update(ElementCardT& element);
 	void Reset(ElementCardT& element);
 
 private:
 
-	/* initialize intermediate state from F_n (for ) */
+	/** initialize intermediate state from F_n (for ) */
 	void InitIntermediate(const dMatrixT& F_total, const dMatrixT& f_relative);
 
-	/* load element data for the specified integration point */
+	/** load element data for the specified integration point */
 	void LoadData(const ElementCardT& element, int ip);
 
-	/* returns 1 if the trial elastic strain state lies outside of the
-	 * yield surface */
+	/** returns 1 if the trial elastic strain state lies outside of the
+	 * yield surface - assumes trial elastic state has been set first */
 	int PlasticLoading(const dMatrixT& F_total, const dMatrixT& f_relative,
 		ElementCardT& element, int ip);
 
-	/* computes the relative stress and returns a reference to the
-	 * relative stress in fRelStress */
-	dSymMatrixT& RelativeStress(const dMatrixT& F_total,
-		const dMatrixT& f_relative, const ElementCardT& element);
-		
+protected:
+
+	/* internal state variables */
+	dArrayT     fInternal; //internal variables
+	dSymMatrixT fb_bar;    //isochoric, elastic part of b
+	dSymMatrixT fbeta_bar; //stress surface "center", kinematic hardening
+
 private:
 
-	/* number of integration points */
+	/** number of integration points */
 	int fNumIP;
 
-	/* material parameters */
+	/** shear modulus */
 	double fmu;
 
 	/* return values */
-	dSymMatrixT	fbelastic;
 	dSymMatrixT	fStressCorr;
 	dMatrixT	fModuliCorr;
 
 	/* work space */
 	dSymMatrixT fRelStress;
-	dSymMatrixT fDev_b;
 	
-	dMatrixT f_f_bar;
-	dMatrixT f_b_trial;
+	dMatrixT    f_f_bar;
+	dSymMatrixT fb_bar_trial;
+	
+	/* kinematic hardening */
+	double      ftrace_beta_trial;
+	dSymMatrixT fbeta_bar_trial;
 
 	dMatrixT   fMatrixTemp1;
 	dMatrixT   fMatrixTemp2;
 
 	dSymMatrixT fRed2Temp;
-	dMatrixT   fRed4Temp1;
-	dMatrixT   fRed4Temp2;
-	
-	dSymMatrixT fb_bar;    //isochoric, elastic part of b
-	dSymMatrixT fUnitNorm; //unit normal to the stress surface
-	dSymMatrixT fBeta;     //stress surface "center", kinematic hardening
-	dArrayT    fInternal; //internal variables
+	dMatrixT    fRed4Temp1;
+	dMatrixT    fRed4Temp2;
+
+	/* internal state variable work space */
+	dSymMatrixT fUnitNorm;        //unit normal to the stress surface
+	dSymMatrixT fb_bar_trial_;    //unit normal to the stress surface
+	dSymMatrixT fbeta_bar_trial_; //unit normal to the stress surface
 };
 
 #endif /* _J2_SIMO_LIN_HARD_T_H_ */
