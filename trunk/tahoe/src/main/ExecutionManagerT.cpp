0f8416deb21b236997bb8dbf882021af102bf691
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-05-16 23:02:57 +0000

    send status from batch files to cout rather than a .stat file

--- trunk/tahoe/src/main/ExecutionManagerT.cpp
+++ trunk/tahoe/src/main/ExecutionManagerT.cpp
@@ -1,4 +1,4 @@
-/* $Id: ExecutionManagerT.cpp,v 1.2 2001-04-27 10:50:11 paklein Exp $ */
+/* $Id: ExecutionManagerT.cpp,v 1.3 2001-05-16 23:02:57 paklein Exp $ */
 /* created: paklein (08/27/1997)                                          */
 /* Manages input file driven jobs.                                        */
 /* MUST overload private:RunJob().                                        */
@@ -283,12 +283,6 @@ void ExecutionManagerT::RunBatch(ifstreamT& in, ostream& status)
 
 	/* mark status */
 	status << "\n Processing batch file: " << in.filename() << '\n';
-
-	/* open status stream */
-	StringT statusfilename;
-	statusfilename.Root(in.filename());
-	statusfilename.Append(".stat");
-	ofstreamT stat(statusfilename);	
 	
 	/* start day/date info */
 	time_t starttime;
@@ -314,9 +308,9 @@ void ExecutionManagerT::RunBatch(ifstreamT& in, ostream& status)
 	
 		/* process if valid */
 		if (nextin.is_open())
-			JobOrBatch(nextin, stat);
+			JobOrBatch(nextin, cout);
 		else
-			stat << " File not found: " << nextinfilename << '\n';
+			cout << " File not found: " << nextinfilename << '\n';
 			
 		/* get next entry */
 		in >> nextinfilename;
@@ -325,6 +319,6 @@ void ExecutionManagerT::RunBatch(ifstreamT& in, ostream& status)
 	/* stop day/date info */
 	time_t stoptime;
 	time(&stoptime);
-	stat << "\n Batch start time  : " << ctime(&starttime);
-	stat <<   " Batch stop time   : " << ctime(&stoptime);
+	cout << "\n Batch start time  : " << ctime(&starttime);
+	cout <<   " Batch stop time   : " << ctime(&stoptime);
 }
