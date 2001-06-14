4ca5a5d73f038caa74ec3c8659ccec53845d0bc6
hspark <hspark@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-06-14 17:18:36 +0000

    initial check-in for Gaussian Window function class for spherical supports

--- trunk/tahoe/src/elements/shape_functions/meshfree/windows/GaussianWindowT.h
+++ trunk/tahoe/src/elements/shape_functions/meshfree/windows/GaussianWindowT.h
@@ -1,10 +1,13 @@
-/* $Id: GaussianWindowT.h,v 1.1 2001-06-13 20:53:57 paklein Exp $ */
+/* $Id: GaussianWindowT.h,v 1.2 2001-06-14 17:18:36 hspark Exp $ */
 
 #ifndef _GAUSSIAN_WINDOW_T_H_
 #define _GAUSSIAN_WINDOW_T_H_
 
 /* base class */
 #include "WindowT.h"
+#include "dArrayT.h"
+#include "dArray2DT.h"
+#include "dSymMatrixT.h"
 
 /** Spherical/circular Gaussian window function */
 class GaussianWindowT
@@ -21,8 +24,31 @@ class GaussianWindowT
 	 * \return 1 since the function varies from node to node only
 	 * depending on the support radius. */
 	virtual int NumberOfParameters(void) const { return 1; };
+	
+	virtual void WriteParameters(ostream& out) const;
 
 	/* single point evaluations */
+	virtual void window(const dArrayT& x_0, const dArrayT& param, const dArrayT& x,
+		double& w);
+	virtual void Dwindow(const dArrayT& x_0, const dArrayT& param, const dArrayT& x,
+		dArrayT& Dw);
+	virtual void DDwindow(const dArrayT& x_0, const dArrayT& param, const dArrayT& x,
+		dArrayT& DDw);	
+
+	/* multiple point evaluations */
+	virtual void window(const dArrayT& x_0, const dArray2DT& param, const dArray2DT& x,
+		dArrayT& w);	
+	virtual void Dwindow(const dArrayT& x_0, const dArray2DT& param, const dArray2DT& x,
+		dArray2DT& Dw);
+	virtual void DDwindow(const dArrayT& x_0, const dArray2DT& param, const dArray2DT& x,
+		dArray2DT& DDw);
+
+	/* coverage tests */
+	/* single point */
+	virtual bool Covers(const dArrayT& x_0, const dArrayT& x);
+
+	/* multiple points */
+	virtual void Covers(const dArrayT& x_0, const dArray2DT& x, const ArrayT<bool>& covers);
 	
 	//etc...
 	
