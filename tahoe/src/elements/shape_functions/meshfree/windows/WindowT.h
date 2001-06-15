c63caf4c34bcbbe758276033e9ec31512145b035
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-06-15 08:33:48 +0000

    corrected interface - multiple window function at a single field point, not the other way around.

--- tahoe/src/elements/shape_functions/meshfree/windows/WindowT.h
+++ tahoe/src/elements/shape_functions/meshfree/windows/WindowT.h
@@ -1,4 +1,4 @@
-/* $Id: WindowT.h,v 1.2 2001-06-14 22:08:15 paklein Exp $ */
+/* $Id: WindowT.h,v 1.3 2001-06-15 08:33:48 paklein Exp $ */
 
 #ifndef _WINDOW_T_H_
 #define _WINDOW_T_H_
@@ -45,40 +45,40 @@ class WindowT
 	/* single point evaluations */
 	
 	/** window function evaluation.
-	 * \param x_0 center of the window function
-	 * \param param array of window function parameters associated with x_0
-	 * \param x field point
+	 * \param x_n center of the window function
+	 * \param param_n array of window function parameters associated with x_n
+	 * \param x field point of evaluation
 	 * \param order highest order derivative to be calculated
-	 * \param w the value at x of the window function centered at x_0
+	 * \param w the value at x of the window function centered at x_n
 	 * \param Dw window function derivatives: [nsd]
 	 * \param DDw window function second derivatives: [nstr] */
-	virtual void window(const dArrayT& x_0, const dArrayT& param, const dArrayT& x,
+	virtual void window(const dArrayT& x_n, const dArrayT& param_n, const dArrayT& x,
 		int order, double& w, dArrayT& Dw, dArrayT& DDw) = 0;
 
 	/* multi-point evaluations */
 	
 	/** window function evaluation. 
-	 * \param x_0 center of the window function
-	 * \param param array of window function parameters: [npts] x [nparam]
-	 * \param x array of field point coordinates: [npts] x [nsd]
+	 * \param x_n array of window function centers: [npts] x [nsd]
+	 * \param param_n array of window function parameters: [npts] x [nparam]
+	 * \param x field point of evaluation
 	 * \param order highest order derivative to be calculated
 	 * \param w values of the window function: [npts]
 	 * \param w values of the window function derivaties: [npts] x [nsd]
 	 * \param w values of the window function derivaties: [npts] x [nstr] 
 	 * \return the number of points covered by the window function */
-	virtual int window(const dArrayT& x_0, const dArray2DT& param, const dArray2DT& x,
+	virtual int window(const dArray2DT& x_n, const dArray2DT& param_n, const dArrayT& x,
 		int order, dArrayT& w, dArray2DT& Dw, dArray2DT& DDw) = 0;	
 	
 	/** coverage test.
-	 * \return true if the window function centered at x_0 covers the
+	 * \return true if the window function centered at x_n covers the
 	 * point x. */
-	virtual bool Covers(const dArrayT& x_0, const dArrayT& x) = 0;
+	virtual bool Covers(const dArrayT& x_n, const dArrayT& x) = 0;
 
 	/** coverage test for multiple points.
-	 * \param x_0 center of the window function
-	 * \param x array of field point coordinates: [npts] x [nsd]
+	 * \param x_n array of window function centers: [npts] x [nsd]
+	 * \param x field point of evaluation
 	 * \param covers array of coverage test results: [npts] */
-	virtual void Covers(const dArrayT& x_0, const dArray2DT& x, const ArrayT<bool>& covers) = 0;
+	virtual void Covers(const dArray2DT& x_n, const dArrayT& x, ArrayT<bool>& covers) = 0;
 };
 
 #endif /* _WINDOW_T_H_ */
