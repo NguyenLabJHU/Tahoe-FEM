41e177009c0f598430784c3665ea21601816e261
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-06-22 01:31:43 +0000

    initial check-in of renamed finite deformation element classes. Aside from the flashy new names, these elements now share a new, finite deformation base class.

---
+++ tahoe/src/elements/continuum/solid/TotalLagrangianT.h
@@ -0,0 +1,69 @@
+/* $Id: TotalLagrangianT.h,v 1.1.2.1 2001-06-22 01:31:42 paklein Exp $ */
+/* created: paklein (07/03/1996)                                          */
+
+#ifndef _TOTAL_LAGRANGRIAN_T_H_
+#define _TOTAL_LAGRANGRIAN_T_H_
+
+/* base class */
+#include "FiniteStrainT.h"
+
+/* direct members */
+#include "dMatrixT.h"
+
+class TotalLagrangianT: public FiniteStrainT
+{
+public:
+
+	/* constructors */
+	TotalLagrangianT(FEManagerT& fe_manager);
+
+	/* data initialization */
+	virtual void Initialize(void);
+		
+protected:
+
+	/* construct materials manager and read data */
+	virtual void ReadMaterialData(ifstreamT& in);
+
+	/* return a pointer to a new material list */
+	virtual MaterialListT* NewMaterialList(int size) const;
+
+	/* form the element stiffness matrix */
+	virtual void FormStiffness(double constK);
+	
+	/* compute the effective acceleration and velocities based
+	 * on the algorithmic flags formXx and the given constants
+	 * constXx.
+	 *
+	 *		acc_eff  = constMa acc  + constCv a vel
+	 *      disp_eff = constKd disp
+	 *
+	 * where a and b are the Rayleigh damping coefficients.
+	 *
+	 *        ***The effective displacement does not include
+	 *           velocity since the internal force is a nonlinear
+	 *           function of the displacements
+*/
+	virtual void ComputeEffectiveDVA(int formBody,
+		int formMa, double constMa, int formCv, double constCv,
+		int formKd, double constKd);
+
+	/* calculate the damping force contribution ("-c*v") */
+	virtual void FormCv(double constC);
+
+	/* calculate the internal force contribution ("-k*d") */
+	virtual void FormKd(double constK);
+
+protected:
+
+	/* workspace */
+	dMatrixT fStressMat;   // space for a stress tensor
+	dMatrixT fStressStiff; // compact stress stiffness contribution
+	dMatrixT fGradNa;      // shape function gradients matrix
+	
+	dArrayT   fTemp2;
+	dMatrixT  fTempMat1, fTempMat2;
+	dArray2DT fDNa_x;
+};
+
+#endif /* _TOTAL_LAGRANGRIAN_T_H_ */
---
+++ tahoe/src/elements/continuum/solid/UpdatedLagrangianT.cpp
@@ -0,0 +1,281 @@
+/* $Id: UpdatedLagrangianT.cpp,v 1.1.2.1 2001-06-22 01:31:42 paklein Exp $ */
+/* created: paklein (07/03/1996)                                          */
+
+#include "UpdatedLagrangianT.h"
+
+#include <math.h>
+#include <iostream.h>
+#include <iomanip.h>
+
+#include "fstreamT.h"
+#include "Constants.h"
+#include "FEManagerT.h"
+#include "StructuralMaterialT.h"
+#include "MaterialList2DT.h"
+#include "MaterialList3DT.h"
+#include "ShapeFunctionT.h"
+
+/* constructor */
+UpdatedLagrangianT::UpdatedLagrangianT(FEManagerT& fe_manager):
+	FiniteStrainT(fe_manager),
+	fCurrShapes(NULL),
+	fCauchyStress(fNumSD),
+	fLocCurrCoords(LocalArrayT::kCurrCoords)
+{
+	/* disable any strain-displacement options */
+	if (fStrainDispOpt != 0)
+	{
+		cout << "\nUpLag_FDElasticT::UpdatedLagrangianT: no strain-displacement options\n" << endl;
+		fStrainDispOpt = 0;
+	}
+
+	/* consistency check */
+	if (fAnalysisCode == GlobalT::kLinStatic ||
+	    fAnalysisCode == GlobalT::kLinDynamic)
+	{
+		cout << "\nUpLag_FDElasticT::UpdatedLagrangianT: no current coordinates required\n" << endl;
+		fLocCurrCoords.SetType(LocalArrayT::kInitCoords);
+	}	
+}
+
+/* destructors */
+UpdatedLagrangianT::~UpdatedLagrangianT(void)
+{
+	delete fCurrShapes;
+	fCurrShapes = NULL;
+}
+
+/* data initialization */
+void UpdatedLagrangianT::Initialize(void)
+{
+	/* inherited */
+	ElasticT::Initialize();
+
+	/* dimension */
+	fGradNa.Allocate(fNumSD, fNumElemNodes);
+	fStressStiff.Allocate(fNumElemNodes);
+	fTemp2.Allocate(fNumElemNodes*fNumDOF);
+}
+
+/***********************************************************************
+* Protected
+***********************************************************************/
+
+/* initialize local arrays */
+void UpdatedLagrangianT::SetLocalArrays(void)
+{
+	/* inherited */
+	ElasticT::SetLocalArrays();
+
+	/* allocate and set source */
+	fLocCurrCoords.Allocate(fNumElemNodes, fNumSD);
+	fFEManager.RegisterLocal(fLocCurrCoords);
+}
+
+/* initialization functions */
+void UpdatedLagrangianT::SetShape(void)
+{
+	/* inherited */
+	ElasticT::SetShape();
+
+	/* linked shape functions */
+	fCurrShapes = new ShapeFunctionT(*fShapes, fLocCurrCoords);
+	if (!fCurrShapes) throw eOutOfMemory ;
+
+	fCurrShapes->Initialize();
+}
+
+/* current element operations */
+//int	UpdatedLagrangianT::NextElement(void)
+//{
+//	int result = ElasticT::NextElement();
+//	while ( result &&
+//		fElementMonitor.Status(fElementCards.Position()) == kMonitorOFF)
+//		result = ElasticT::NextElement();
+//
+//	/* forming LHS matrix */
+//	if (result && fLHSRHSflag == kLHS) //no specialization for RHS
+//	{
+//		fStressStiff.Clear();
+//		SetLocal(fLocDisp);
+//	}
+//	
+//	return(result);
+//}
+
+/* form shape functions and derivatives */
+void UpdatedLagrangianT::SetGlobalShape(void)
+{
+	/* inherited */
+	ElasticT::SetGlobalShape();
+
+	/* shape function wrt current config */
+	SetLocalX(fLocCurrCoords);
+	fCurrShapes->SetDerivatives();
+}
+
+/* construct materials manager and read data */
+MaterialListT* UpdatedLagrangianT::NewMaterialList(int size) const
+{
+	if (fNumSD == 2)
+		return new MaterialList2DT(size, *this);
+	else if (fNumSD == 3)
+		return new MaterialList3DT(size, *this);
+	else
+		return NULL;			
+}
+
+/* form the element stiffness matrix */
+void UpdatedLagrangianT::FormStiffness(double constK)
+{		
+	/* matrix format */
+	dMatrixT::SymmetryFlagT format =
+		(fLHS.Format() == ElementMatrixT::kNonSymmetric) ?
+		dMatrixT::kWhole :
+		dMatrixT::kUpperOnly;
+
+	/* integration */
+	const double* Det    = fCurrShapes->IPDets();
+	const double* Weight = fCurrShapes->IPWeights();
+
+	/* initialize */
+	fStressStiff = 0.0;
+	SetLocalU(fLocDisp);
+	
+	fShapes->TopIP();
+	while ( fShapes->NextIP() )
+	{
+		/* double scale factor */
+		double scale = constK*(*Det++)*(*Weight++);
+	
+	/* S T R E S S   S T I F F N E S S */			
+		/* compute Cauchy stress */
+		(fCurrMaterial->s_ij()).ToMatrix(fCauchyStress);
+	
+		/* integration constants */		
+		fCauchyStress *= scale;
+
+		/* get shape function gradients matrix */
+		fCurrShapes->GradNa(fGradNa);
+	
+		/* using the stress symmetry */
+		fStressStiff.MultQTBQ(fGradNa, fCauchyStress,
+			format, dMatrixT::kAccumulate);
+
+	/* M A T E R I A L   S T I F F N E S S */									
+		/* strain displacement matrix */
+		fCurrShapes->B(fB);
+
+		/* get D matrix */
+		fD.SetToScaled(scale, fCurrMaterial->c_ijkl());
+						
+		/* accumulate */
+		fLHS.MultQTBQ(fB, fD, format, dMatrixT::kAccumulate);	
+	}
+						
+	/* stress stiffness into fLHS */
+	fLHS.Expand(fStressStiff, fNumDOF);
+}
+
+/*
+* Compute the effective acceleration and velocities based
+* on the algorithmic flags formXx and the given constants
+* constXx.
+*
+*      acc_eff  = constMa acc  + constCv a vel
+*      vel_eff  = constCv b vel;
+*      disp_eff = constKd disp
+*
+* where a and b are the Rayleigh damping coefficients.
+*
+*        ***The effective displacement does not include
+*           velocity since the internal force is a nonlinear
+*           function of the displacements
+*/
+void UpdatedLagrangianT::ComputeEffectiveDVA(int formBody,
+	int formMa, double constMa, int formCv, double constCv,
+	int formKd, double constKd)
+{
+	/* acceleration */
+	if (formMa || formBody)
+	{
+		if (formMa)
+			SetLocalU(fLocAcc);
+		else
+			fLocAcc = 0.0;
+		
+		if (formBody) AddBodyForce(fLocAcc);
+
+		fLocAcc *= constMa;	
+	}
+	else
+		fLocAcc = 0.0;
+	
+	/* displacement */
+	if (formKd)
+	{
+		SetLocalU(fLocDisp);
+		fLocDisp *= constKd;	
+	}
+	else
+		fLocDisp = 0.0;
+	
+	/* Rayleigh damping */
+	if (formCv)
+	{
+		SetLocalU(fLocVel);
+		fLocVel *= constCv;
+		
+		/* effective a */
+		fLocAcc.AddScaled(fCurrMaterial->MassDamping(), fLocVel);
+		
+		/* effective v */
+		fLocVel *= fCurrMaterial->StiffnessDamping();
+	}
+	else
+		fLocVel = 0.0;
+}	
+
+/* calculate the damping force contribution ("-c*v") */
+void UpdatedLagrangianT::FormCv(double constC)
+{
+//TEMP
+//This is wrong.  No nonlinear Rayleigh damping
+
+	/* clear workspace */
+	fLHS = 0.0;
+	fStressStiff = 0.0;
+
+	/* form tangent stiffness */
+	FormStiffness(constC);
+	fLHS.CopySymmetric();
+
+	/* reorder */
+	fLocVel.ReturnTranspose(fTemp2);
+	
+	/* C*v */
+	fLHS.MultTx(fTemp2, fNEEvec);
+	
+	/* Accumulate */
+	fRHS += fNEEvec;
+}
+
+/* calculate the internal force contribution ("-k*d") */
+void UpdatedLagrangianT::FormKd(double constK)
+{
+	const double* Det    = fCurrShapes->IPDets();
+	const double* Weight = fCurrShapes->IPWeights();
+
+	fCurrShapes->TopIP();
+	while ( fCurrShapes->NextIP() )
+	{
+		/* get strain-displacement matrix */
+		fCurrShapes->B(fB);
+
+		/* B^T * Cauchy stress */
+		fB.MultTx(fCurrMaterial->s_ij(), fNEEvec);
+
+		/* accumulate */
+		fRHS.AddScaled(constK*(*Weight++)*(*Det++), fNEEvec);
+	}	
+}
