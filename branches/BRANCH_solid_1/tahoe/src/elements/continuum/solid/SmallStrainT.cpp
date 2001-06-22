a93fde917fcd62de8ffa10ff210aecc725465191
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-06-22 01:26:52 +0000

    initial check-in of new base class for all small deformation, solid elements. Is responsible for return measures of deformation and gradients of the nodal fields.

---
+++ branches/BRANCH_solid_1/tahoe/src/elements/continuum/solid/SmallStrainT.cpp
@@ -0,0 +1,10 @@
+/* $Id: SmallStrainT.cpp,v 1.1.2.1 2001-06-22 01:26:52 paklein Exp $ */
+
+#include "SmallStrainT.h"
+
+/* constructor */
+SmallStrainT::SmallStrainT(FEManagerT& fe_manager):
+	ElasticT(fe_manager)
+{
+
+}
