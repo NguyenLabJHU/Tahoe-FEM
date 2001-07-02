e50b0ea190752d00dcffe7c99c811afd6833db8c
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-07-02 21:53:31 +0000

    moved DetCheckT to material/primitives

---
+++ tahoe/src/elements/continuum/solid/materials/primitives/DetCheckT.h
@@ -0,0 +1,49 @@
+/* $Id: DetCheckT.h,v 1.1.2.1 2001-07-02 21:53:30 paklein Exp $ */
+/* created: paklein (09/11/1997)                                          */
+/* Note: Class does not dynamically allocate memory on construction       */
+
+#ifndef _DETCHECK_T_H_
+#define _DETCHECK_T_H_
+
+/* forward declarations */
+class dSymMatrixT;
+class dMatrixT;
+class dArrayT;
+
+class DetCheckT
+{
+public:
+
+	/* Constructor */
+	DetCheckT(const dSymMatrixT& s_jl, const dMatrixT& c_ijkl);
+
+	/*
+	 * Returns 1 if acoustic tensor isn't positive definite,
+	 * and returns the normal to the surface of localization.
+	 * Returns 0, otherwise.
+	 */
+	int IsLocalized(dArrayT& normal);
+
+private:
+
+	/* 2D determinant check function */
+	int DetCheck2D(dArrayT& normal);
+
+	/* compute coefficients of det(theta) function */
+	void ComputeCoefficients(void);
+
+	/* determinant function and derivatives */
+	double det(double t) const;
+	double ddet(double t) const;
+	double dddet(double t) const;
+
+private:
+
+	const dSymMatrixT& 	fs_jl;	/* Cauchy stress          */
+	const dMatrixT&		fc_ijkl;/* spatial tangent moduli */
+
+	double phi2, phi4;	/* phase shifts */
+	double A0, A2, A4;	/* amplitudes   */
+};
+
+#endif /* _DETCHECK_T_H_ */
