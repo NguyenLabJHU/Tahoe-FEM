553bcfa406397e326a60d377768318a18aa62d78
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-07-22 21:11:51 +0000

    switch check to read the "/proc/meminfo" file

--- toolbox/src/common/NewMemory.h
+++ toolbox/src/common/NewMemory.h
@@ -1,4 +1,4 @@
-/* $Id: NewMemory.h,v 1.3 2001-07-22 20:53:15 paklein Exp $ */
+/* $Id: NewMemory.h,v 1.4 2001-07-22 21:11:51 paklein Exp $ */
 
 /* memory allocation function to handle platform dependent
  * differences associated with memory allocation failures and
@@ -7,8 +7,11 @@
 #ifndef _NEW_MEMORY_H_
 #define _NEW_MEMORY_H_
 
+#include <iostream.h>
+
 /* environment */
 #include "Environment.h"
+#include "ExceptionCodes.h"
 
 /* new that throws bad_alloc on fail instead of returning NULL */
 #ifdef __NEW_THROWS__
@@ -17,31 +20,73 @@
 
 /* certain CPLANT's don't like to hit memory limits */
 #if defined(__DELMAR__) || defined(__ASILOMAR__)
-#include <sys/sysinfo.h>
 #define _SPACE_CHECK_
-const int  min_space_check_size = 100; /* skip check for very small arrays */
+
+/* memory check methods */
+#define MEM_CHECK_SYSINFO      0
+#define MEM_CHECK_PROC_MEMINFO 1
+
+#define MEM_CHECK_METHOD MEM_CHECK_PROC_MEMINFO
+
+/* size parameters */
+const int min_space_check_size = 100; /* skip check for very small arrays */
 const unsigned long min_free_memory = 5*1000000; /* ~5MB */
-#endif
+
+/* headers needed for memory check implementation */
+#if (MEM_CHECK_METHOD == MEM_CHECK_SYSINFO)
+#include <sys/sysinfo.h>
+#else (MEM_CHECK_METHOD == MEM_CHECK_PROC_MEMINFO)
+#include <fstream.h>
+#endif /* (MEM_CHECK_METHOD == MEM_CHECK_SYSINFO) */
+
+#endif /* defined(__DELMAR__) || defined(__ASILOMAR__) */
 
 #ifdef _SPACE_CHECK_
+#if (MEM_CHECK_METHOD == MEM_CHECK_SYSINFO)
+/* assess free memory based on the return from sysinfo. The
+ * free memory returned here does not include memory used for
+ * disk cache */
 inline bool HasFreeMemory(unsigned long size)
 {
 	/* get sys info structure */
 	struct sysinfo s_info;
 	sysinfo(&s_info);
 
-	//TEMP
-	cout << " size: " << size << '\n';
-	cout << " free: " << s_info.freeram << '\n';
-	cout << " buff: " << s_info.bufferram << endl;
-
-	//TEMP
-	return true;
-
 	/* check free memory */
 	return s_info.freeram - size > min_free_memory;
 };
-#endif
+#else
+/* assess free memory based on the contents of /proc/meminfo:
+
+        total:    used:    free:  shared: buffers:  cached:
+Mem:  260939776 55083008 205856768 23363584   516096 23576576
+
+* the total free memory is taken as: free + buffers + cached */
+inline bool HasFreeMemory(unsigned long size)
+{
+	/* buffer */
+	char buffer[255];
+	
+	/* meminfo file */
+	ifstream in("/proc/meminfo");
+
+	/* clear 1st line and 1st word on second line */
+	in.getline(buffer, 254);
+	in >> buffer;
+	unsigned long total, used, free, shared, buffers, cached;
+	total = used = free = shared = buffers = cached = -1;
+	in >> total >> used >> free >> shared >> buffers >> cached;
+	if (free < 0 || buffers < 0 || cached < 0)
+	{
+		cout << "::HasFreeMemory: error reading \"/proc/meminfo\"" << endl;
+		throw eGeneralFail;
+	}
+
+	/* check free memory */
+	return (free + buffers + cached) - size > min_free_memory;
+};
+#endif /* defined(__DELMAR__) || defined(__ASILOMAR__) */
+#endif /* _SPACE_CHECK_ */
 
 template <class TYPE>
 TYPE* New(int size)
@@ -81,8 +126,3 @@ struct sysinfo {
 #endif
 
 #endif /* _NEW_MEMORY_H_ */
-
-
-
-
-
