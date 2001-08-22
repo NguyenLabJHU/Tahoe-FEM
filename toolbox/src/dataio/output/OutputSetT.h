7271c4f59ef57b7a6899cf7f8c78c7695bc452ea
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-08-22 00:20:22 +0000

    added documentation to explain how the class is used to specify a set of output data

--- toolbox/src/dataio/output/OutputSetT.h
+++ toolbox/src/dataio/output/OutputSetT.h
@@ -1,5 +1,5 @@
-/* $Id: OutputSetT.h,v 1.3 2001-07-19 00:58:09 paklein Exp $ */
-/* created: paklein (03/07/2000)                                          */
+/* $Id: OutputSetT.h,v 1.4 2001-08-22 00:20:22 paklein Exp $ */
+/* created: paklein (03/07/2000) */
 
 #ifndef _OUTPUTSET_T_H_
 #define _OUTPUTSET_T_H_
@@ -12,15 +12,58 @@
 /* forward declarations */
 class iArray2DT;
 
+/** class to act as specifier for output data. A class that would
+ * like to write output, needs to use the following procedure:\n
+ * (1) construct an OutputSetT with its output data specifications\n
+ * (2) register this output data specification with an output formatter.
+ *     Output formatters are classes derived from OutputBaseT. Output
+ *     is registered using OutputBaseT::AddElementSet. Note that this
+ *     function is accessed in Tahoe through FEManagerT::RegisterOutput
+ *     which passes the call through IOManager::AddElementSet to
+ *     OutputBaseT::AddElementSet. Registering output returns an integer
+ *     output ID (OID) which must be used in step (3). Note that the OID 
+ *     obtained during registration is generally not the same as the ID 
+ *     passed into the OutputSetT constructor. A class with a single ID 
+ *     may register an arbitary number of output sets, receiving a unique
+ *     OID for each set\n
+ * (3) write output by sending output data to the output formatter using
+ *     OutputBaseT::WriteOutput with the ID obtained during registration
+ *     in step (2). Note that this function is accessed in Tahoe through 
+ *     FEManagerT::WriteOutput which passes the call through 
+ *     IOManager::AddElementSet to the output formatter\n
+ *
+ * Outside of the constructor, the remainder of the interface is intended
+ * for use by the output formatter, not the class sending the data for
+ * output\n */
 class OutputSetT
 {
 public:
 
-	/* constructor */
+	/* generate output data record.
+	 * \param ID identifier to denote the class generating the OutputSetT
+	 * \param geometry_code GeometryT::CodeT defining the geometry associated
+	 *        with the connectivities.
+	 * \param block_ID list of element block ID's comprising the connectivities.
+	 *        This list is used to declare that the connectivities passed in are
+	 *        actually a union of connectivities defined by ID's declared in an 
+	 *        associated geometry database file. This information is needed to
+	 *        join element-based data from parallel calculations. This list may
+	 *        be empty.
+	 * \param connectivities elements over which data will be written. The
+	 *        output formatters retain a reference to these connectivities
+	 *        for use during output.
+	 * \param n_labels list of labels for the nodal variables. The length of
+	 *        this list defines the number of nodal output variables.
+	 * \param e_labels list of labels for the element variables. The length of
+	 *        this list defines the number of element output variables.
+	 * \param changing flag to indicate whether the connectivities may change
+	 *        from output step to output step. */
 	OutputSetT(int ID, GeometryT::CodeT geometry_code,
 		const iArrayT& block_ID, const iArray2DT& connectivities, 
 		const ArrayT<StringT>& n_labels, const ArrayT<StringT>& e_labels, 
 		bool changing);
+
+	/** copy constructor */
 	OutputSetT(const OutputSetT& source);
 
 	/* print step counter */
@@ -49,15 +92,15 @@ public:
 
 private:
 
-	/* not allowed */
+	/** make private to avoid accidental use. */
 	OutputSetT& operator=(OutputSetT&) { return *this; }
 
-	/* set nodes used */
+	/** determine the nodes used in the output set */
 	void SetNodesUsed(void);
 
 private:
 
-	/* current print step */
+	/** count of number of output steps */
 	int fPrintStep;
 
 	int  fID;
