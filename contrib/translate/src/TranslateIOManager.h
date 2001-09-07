ef6ae1c6f609d10ca6de958514e481afe69d5a01
sawimme <sawimme@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-09-07 13:25:29 +0000

    Update to use Model Manager and not to use IOManager.

--- contrib/translate/src/TranslateIOManager.h
+++ contrib/translate/src/TranslateIOManager.h
@@ -1,17 +1,58 @@
-/* $Id: TranslateIOManager.h,v 1.1 2001-09-01 00:05:36 paklein Exp $ */
+/* $Id: TranslateIOManager.h,v 1.2 2001-09-07 13:25:29 sawimme Exp $ */
 
 #ifndef _TRANSLATE_IOMANAGER_H_
 #define _TRANSLATE_IOMANAGER_H_
 
-#include "IOManager.h"
+#include "DataManagerT.h"
+#include "OutputBaseT.h"
+#include "StringT.h"
+#include "ArrayT.h"
+#include "iArrayT.h"
+#include "dArrayT.h"
+#include "iArray2DT.h"
 
-class TranslateIOManager : public IOManager
+class TranslateIOManager
 {
  public:
 
-  TranslateIOManager (ostream& outfile);
+  TranslateIOManager (ostream& message);
+  void Translate (const StringT& program, const StringT& version, const StringT& title);
 
-  virtual void Interactive (void);
+ private:
+  void SetOutput (const StringT& program, const StringT& version, const StringT& title);
+
+  void InitializeVariables (void);
+  void InitializeTime (void);
+
+  void TranslateVariables (bool xy);
+
+  void WriteGeometry (void);
+  void WriteNodes (void);
+  void WriteNodeSets (void);
+  void WriteElements (void);
+  void WriteSideSets (void);
+
+ private:
+  ostream& fMessage;
+
+  DataManagerT fModel;
+
+  OutputBaseT* fOutput;
+  StringT fOutputName;
+  iArrayT fOutputID;
+
+  int fNumNV;
+  int fNumEV;
+  int fNumQV;
+  ArrayT<StringT> fNodeLabels;
+  ArrayT<StringT> fElementLabels;
+
+  int fNumTS;
+  dArrayT fTimeSteps;
+
+  iArrayT fNodeMap;
+
+  ArrayT<iArray2DT> fGlobalSideSets;
 };
 
 #endif
