1f3ce55ca1cfda4cb1f026a3d3de51915f9d4ee7
recampb <recampb@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-10-24 00:52:08 +0000

    working on adjustments for frame-by-frame

--- trunk/contrib/vtk/src/VTKConsoleT.h
+++ trunk/contrib/vtk/src/VTKConsoleT.h
@@ -1,4 +1,4 @@
-/* $Id: VTKConsoleT.h,v 1.14 2001-10-23 00:23:08 recampb Exp $ */
+/* $Id: VTKConsoleT.h,v 1.15 2001-10-24 00:52:08 recampb Exp $ */
 
 #ifndef _VTK_CONSOLE_T_H_
 #define _VTK_CONSOLE_T_H_
@@ -48,54 +48,20 @@ class VTKConsoleT: public iConsoleObjectT
   virtual bool iDoCommand(const StringT& command, StringT& line);
 
  private:
-  double valRange1, valRange2;
-  double hueRange1,hueRange2;
-  double satRange1, satRange2;
-  double alphaRange1, alphaRange2;
-  double scalarRange1[100], scalarRange2[100];
+
   double time;
-  double scale_factor;
-  int numColors;
-  int num_node_variables;
-  int currentVarNum;
+
   int test;
   double numRen;
-  StringT source_file;
-  StringT output_file;
-  StringT outFileName;
-  StringT sbTitle;
-  StringT varList;
-  vtkUnstructuredGridReader *ugr;
-  vtkRenderer *renderer;
-  vtkRenderer *renderer2;
-  vtkRenderer *renderer3;
-  vtkRenderer *renderer4;
+  // StringT source_file;
+
   vtkRenderWindow *renWin;
   vtkRenderWindowInteractor *iren;
-  vtkLookupTable *lut;
-  vtkDataSetMapper *ugridMapper;
-  vtkActor *ugridActor;
-  vtkActor *wireActor;
-  vtkScalarBarActor *scalarBar;
-  vtkRendererSource *renSrc;
+ 
   vtkTIFFWriter *writer;
-  vtkIdFilter *ids;
-  vtkSelectVisiblePoints *visPts;
-  vtkLabeledDataMapper *ldm;
-  vtkActor2D *pointLabels;
-  vtkUnstructuredGrid *ugrid;  
+   
   /* vtkScalars *scalars[num_time_steps]; */
-  vtkScalars *scalars [1000][100];
-  vtkVectors *vectors [1000][100];
-  int num_time_steps;
-  vtkCamera *cam;
-  vtkCubeAxesActor2D *axes;
-  vtkPoints *points;
-  ArrayT<StringT> node_labels;
-  vtkUnstructuredGrid *warpGrid;
-  vtkPoints *dPoints;
-  vtkWarpVector *warp;
-  int frameNum;
+ 
 
   ArrayT<VTKFrameT> fFrames;
 };
