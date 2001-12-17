34922830387804c177c520c64d7b03d008baec28
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2001-12-17 00:00:52 +0000

    (1) added Union functions (2) added documentation

--- trunk/toolbox/src/abc/array/iArrayT.h
+++ trunk/toolbox/src/abc/array/iArrayT.h
@@ -1,5 +1,5 @@
-/* $Id: iArrayT.h,v 1.2 2001-06-20 22:49:57 paklein Exp $ */
-/* created: paklein (08/10/1996)                                          */
+/* $Id: iArrayT.h,v 1.3 2001-12-17 00:00:52 paklein Exp $ */
+/* created: paklein (08/10/1996) */
 
 #ifndef _IARRAY_T_H_
 #define _IARRAY_T_H_
@@ -7,6 +7,7 @@
 /* base class */
 #include "nArrayT.h"
 
+/* integer array class. Most functionality is inherited from nArrayT. */
 class iArrayT: public nArrayT<int>
 {
 public:
@@ -21,19 +22,47 @@ public:
 	iArrayT& operator=(const iArrayT& RHS);
 	iArrayT& operator=(const int value);
 	
-	/* flagging operations */
-	int ChangeValue(int from, int to); // returns the number changed
-	int	Count(int value) const;	
-	int HasValue(int value) const;	           // returns 1 if present, 0 otherwise.
-	int HasValue(int value, int& index) const; // first occurence at index.
+
+	/** flip specified values in the array
+	 * \param from values to match
+	 * \param to new value for matching entries
+	 * \return number of values changed */
+	int ChangeValue(int from, int to);
+
+	/** count the instances of value in the array */
+	int	Count(int value) const;
+	
+	/** return 1 of the value is present, 0 otherwise */
+	int HasValue(int value) const;
+	
+	/** find the first occurence of a value in the array
+	 * \param value value to match
+	 * \param index returns with the index of the first occurence, if found, -1 otherwise
+	 * \return 1 if value found, 0 otherwise */
+	int HasValue(int value, int& index) const;
 	
 	/* set array value to its position in the array */
 	void SetValueToPosition(void);
 	
-	/* sort both arrays by the values in master */
+	/** sort values in ascending order */
 	void SortAscending(void);
+
+	/** sort values in ascending order determined by the master array. Both *this
+	 * and master are returned in ascending order. */
 	void SortAscending(ArrayT<int>& master);
+
+	/** sort values in ascending order determined by the master array. Both *this
+	 * and master are returned in ascending order. */
 	void SortAscending(ArrayT<double>& master);
+	
+	/** determine union of the given array. This function does allocate a map array
+	 * the with a length the range of values in source. Determining the union requires
+	 * 3 passes through the source array and 2 passes through the map. 
+	 * \return a reference to *this. */
+	iArrayT& Union(const nArrayT<int>& source);
+
+	/** determine the union of the given arrays */
+	iArrayT& Union(const ArrayT<const nArrayT<int>*>& source);
 };
 
 /* inlines */
