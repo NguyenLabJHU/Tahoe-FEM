2f2caf8445751dda9cc8775ce788cb515da50865
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-01-07 20:40:14 +0000

    work around for CW7.x bug that corrupts stream if close()-ed when not open

--- trunk/toolbox/src/fstreamT/fstreamT.h
+++ trunk/toolbox/src/fstreamT/fstreamT.h
@@ -1,4 +1,4 @@
-/* $Id: fstreamT.h,v 1.2 2002-01-05 06:55:01 paklein Exp $ */
+/* $Id: fstreamT.h,v 1.3 2002-01-07 20:40:13 paklein Exp $ */
 /* created: paklein (12/30/2000) */
 
 #ifndef _FSTREAM_T_H_
@@ -15,6 +15,12 @@ class fstreamT
 	/** C-C++(2.4.6)/MSL doesn't to path's right. temporarily needed to
 	 * remove "ups" and "downs" from pathnames. Used only for Mac */
 	static void FixPath(const char* path_old, StringT& path);
+
+	/** work around check. This function returns false if the MW
+	 * compiler is older than those released CW7.x, true if it is
+	 * a compiler version for which the workaround is needed and
+	 * throws eStop if the compiler version is newer. */
+	static bool need_MW_workaround(void);
 };
 
 #endif /* _FSTREAM_T_H_ */
