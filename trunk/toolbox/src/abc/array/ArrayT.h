92218a1aa5bb8edc1a217dfa22fb3051790be57d
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-02-18 08:40:38 +0000

    (1) added documentation (2) Allocate is being deprecated in favor of Dimension (3) Dimension no longer allocates if the array is shallow     but of the correct dimension

--- trunk/toolbox/src/abc/array/ArrayT.h
+++ trunk/toolbox/src/abc/array/ArrayT.h
@@ -1,4 +1,4 @@
-/* $Id: ArrayT.h,v 1.7 2002-01-02 06:25:56 paklein Exp $ */
+/* $Id: ArrayT.h,v 1.8 2002-02-18 08:40:38 paklein Exp $ */
 /* created: paklein (06/19/1996) */
 
 #ifndef _ARRAY_T_H_
@@ -17,97 +17,168 @@
 
 #include "ExceptionCodes.h"
 
-/** templated class for arrays */
+/** templated class for arrays. Memory is dynamically allocated and freed.
+ * Contents of the array is copied and moved using memcpy and memmove if
+ * the static memory ArrayT::fByteCopy is true. Otherwise, the templated
+ * type must define a propoer operator=. */
 template <class TYPE>
 class ArrayT
 {
 public:
 
-	/* constructors */
+	/** construct an empty array */
 	ArrayT(void);
+
+	/** construct a array of the specified length */
 	explicit ArrayT(int length);
+
+	/** construct an array of the specified length using the given memory.
+	 * \param length dimension of the array
+	 * \param TYPEPtr pointer to a block of memory with a dimension of at
+	 *        least length. It is assumed that this memory will remain valid
+	 *        for the lifetime of this array */
 	ArrayT(int length, TYPE* TYPEPtr);
+
+	/** construct a deep copy of the source array */
 	ArrayT(const ArrayT& source);
 
-	/* destructor */
+	/** destructor */
 	~ArrayT(void);
 	
-	/* set fields */
+	/** configure an array of the specified length using the given memory.
+	 * Any memory previously owned by the array will be freed and previous
+	 * content is lost.
+	 * \param length dimension of the array
+	 * \param TYPEPtr pointer to a block of memory with a dimension of at
+	 *        least length. It is assumed that this memory will remain valid
+	 *        for the lifetime of this array */
 	void Set(int length, TYPE* TYPEPtr);
-	
-	/* allocate an array of the specified size  */
-	void Allocate(int length);
+
+	/** set the array size to the given length. No change occurs if the array
+	 * is already the specified length. The previous contents of the array is
+	 * not preserved. To preserve the array contents while changing the dimension
+	 * use ArrayT::Resize. */
+	void Dimension(int length);
+
+	/** \deprecated replaced by ArrayT::Dimension on 02/13/2002 */
+	void Allocate(int length) { Dimension(length); };
+
+	/** return true if the array owns the memory it is using */
 	bool IsAllocated(void) const;
 
-	/* resize to new dimension, copying in at most what fits.
-	 * extra space is initialized by specifying the fill. */
+	/** dimension the array to the new length keeping as much of the previous
+	 * data as fits in the new space. Added space is not initialized. */
 	void Resize(int new_length);
+
+	/** dimension the array to the new length keeping as much of the previous
+	 * data as fits in the new space 
+	 * \param new_length dimension of the array following the function call
+	 * \param value to initialize elements if the new length is greater than
+	 *        the previous length */
 	void Resize(int new_length, const TYPE& fill);
 	
-	/* length of the array */
+	/** length of the array */
 	int Length(void) const;
 
-	/* free memory (if allocated) and set size to zero */
+	/** free memory (if allocated) and set size to zero */
 	void Free(void);
 
-	/* returns a pointer specified element in the array - offset
+	/** returns a pointer specified element in the array - offset
 	 * must be 0 <= offset <= Length() <--- one beyond the end! */
 	TYPE* Pointer(int offset = 0) const;
 
-	/* element accessor */
+	/** element accessor */
 	TYPE& operator[](int index) const;
+
+	/** reference to the first element in the array. Array must be at
+	 * least length 1 */
 	TYPE& First(void) const;
+
+	/** reference to the last element in the array. Array must be at
+	 * least length 1 */
 	TYPE& Last(void) const;
 		
-	/* assignment operators */
+	/** set all elements in the array to the given value */
 	ArrayT<TYPE>& operator=(const TYPE& value);
+
+	/** create a deep copy of the source array. */
 	ArrayT<TYPE>& operator=(const ArrayT<TYPE>& RHS);
+
+	/** copy contents of the array. The valid portion of the source array is
+	 * assumed to be at least as long as the length of this array */ 
 	void Copy(const TYPE* pRHS);
 
-	/* shallow copy/conversion */
+	/** create shallow copy of the source array. Any memory previously owned 
+	 * by the array will be freed and previous content is lost. The source array
+	 * is assumed to stay valid for the lifetime of this array. */
 	void Alias(const ArrayT<TYPE>& RHS);
 	
-	/* copy length elements of source beginning at offset */
+	/** copy source into this array. The length of this array, accounting for
+	 * the offset must be long enough to accept the contents of the source.
+	 * \param offset destination in this array for the first element of source
+	 * \param source source array */
 	void CopyIn(int offset, const ArrayT<TYPE>& source);
+
+	/** copy part of source into this array. The length of this array, accounting for
+	 * the offsets must be long enough to accept the specified part of source.
+	 * \param offset destination in this array for the first element copied 
+	 *        from source
+	 * \param source source array 
+	 * \param source_offset offset to the first element copied from source
+	 * \param length number of elements copied from source. */
 	void CopyPart(int offset, const ArrayT<TYPE>& source, int source_offset, int length);
+
+	/** copy selected values of source into this array
+	 * \param keys indicies of the source array to copy. The number of
+	 *        keys must be the same as the length of this array
+	 * \param source source array. */
 	void Collect(const ArrayT<int>& keys, const ArrayT<TYPE>& source);
 	
-	/* exchange data */
+	/** exchange data with the source array. Exchanges all fields of the arrays. */
 	void Swap(ArrayT<TYPE>& source);
 
 	/* array no longer responsible for freeing memory. error if
 	 * the array is already shallow. DANGER. */
+
+	/** transform this array into a shallow copy. The array gives up ownership of
+	 * its memory. This memory must be released elsewhere. An exception is thrown
+	 * if this array is already shallow 
+	 * \param array returns with a pointer to the memory owned by the array */
 	void ReleasePointer(TYPE** array);
 
-	/* binary I/O - entire length of fArray */
+	/** read array from stream as binary */
 	void ReadBinary(istream& in);
+
+	/** write array to stream as binary */
 	void WriteBinary(ostream& out) const;
 
 protected:
 
-	/* return allocate memory and return a pointer (new C++ error handling)
-	 * returns NULL on fail */
+	/** return allocate memory and return a pointer (new C++ error handling)
+	 * \return NULL on fail */
 	TYPE* New(int size) const;
 
-	/* safe memory copying - ie. like memcpy if fByteCopy is true */
+	/** copying memory depending on the ArrayT:fByteCopy flag */
 	static void MemCopy(TYPE* to, const TYPE* from, int length);
+
+	/** moving memory depending on the ArrayT:fByteCopy flag */
 	static void MemMove(TYPE* to, const TYPE* from, int length);
 	  	
 protected:
 	
-	int fLength;  /* size */
-	TYPE* fArray; /* data */
+	int fLength;  /**< logical size of the array */
+	TYPE* fArray; /**< the data */
 	
 private:
 	
-	int fDelete; /* allocation flag */
+	int fDelete; /**< allocation flag */
 
 //NOTE: could use fDelete as "memory protection" flag
 //      0: not owned => no guarantees
 //      1: owned     => will free on destruction
 //      2: locked    => must free on destruction, i.e., no Swap or ReleasePointer
 
-	/* must be uniquely defined at file scope if
+	/** must be uniquely defined at file scope if
 	 * copying operations are instantiated */
 	static const bool fByteCopy;
 };
@@ -132,7 +203,7 @@ inline ArrayT<TYPE>::ArrayT(int length):
 	fArray(NULL),
 	fDelete(0)
 {
-	Allocate(length);	
+	Dimension(length);	
 }
 
 template <class TYPE>
@@ -253,12 +324,13 @@ inline void ArrayT<TYPE>::Set(int length, TYPE* TYPEPtr)
 
 /* allocate an array of the specified size.*/
 template <class TYPE>
-void ArrayT<TYPE>::Allocate(int length)
+void ArrayT<TYPE>::Dimension(int length)
 {
 	/* abort on negative lengths */
 	if (length < 0) throw eGeneralFail;
 
-	if (length != fLength || !fDelete)
+	/* do nothing if the correct length already */
+	if (length != fLength)
 	{
 		/* old data is lost if it has new length */
 		if (fDelete) delete[] fArray;
@@ -393,7 +465,7 @@ inline ArrayT<TYPE>& ArrayT<TYPE>::operator=(const ArrayT<TYPE>& RHS)
 	if (fArray != RHS.fArray)
 	{
 		/* allocate space if needed */
-		if (fLength != RHS.fLength) Allocate(RHS.fLength); // old data is gone
+		if (fLength != RHS.fLength) Dimension(RHS.fLength); // old data is gone
 				
 		/* copy data */
 		MemCopy(fArray, RHS.fArray, fLength);	
