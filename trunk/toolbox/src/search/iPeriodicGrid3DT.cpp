577fded41b41d51525c0adc33b4f65afc601fcfd
xiang <xiang@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-02-25 17:50:49 +0000

    Modified: hx -> h_x hy -> h_y hz -> h_z

--- trunk/toolbox/src/search/iPeriodicGrid3DT.cpp
+++ trunk/toolbox/src/search/iPeriodicGrid3DT.cpp
@@ -1,4 +1,4 @@
-/* $Id: iPeriodicGrid3DT.cpp,v 1.1.1.1 2001-01-25 20:56:26 paklein Exp $ */
+/* $Id: iPeriodicGrid3DT.cpp,v 1.2 2002-02-25 17:50:49 xiang Exp $ */
 /* created: paklein (12/18/1997)                                          */
 
 #include "iPeriodicGrid3DT.h"
@@ -22,9 +22,9 @@ int iPeriodicGrid3DT::PeriodicNeighbors(int n, double tol, iArrayT& neighbors)
 	fSortedHits.Allocate(0);
 	int skiptag = n;
 
-	double hx = fPeriodicity[0];
-	double hy = fPeriodicity[1];
-	double hz = fPeriodicity[2];
+	double h_x = fPeriodicity[0];
+	double h_y = fPeriodicity[1];
+	double h_z = fPeriodicity[2];
 	
 	/* fetch prospective neighbors */
 	double* target = fCoords(n);
@@ -33,67 +33,67 @@ int iPeriodicGrid3DT::PeriodicNeighbors(int n, double tol, iArrayT& neighbors)
 	ProcessHitsSorted(target, tol, skiptag, fSortedHits);
 	
 	/* overlap flags */
-	int ix = (hx < 0.0) ? 0 :
+	int ix = (h_x < 0.0) ? 0 :
 	         ((target[0] + tol > fxmax) ? -1 :
 			  ((target[0] - tol < fxmin) ? 1 : 0));
-	int iy = (hy < 0.0) ? 0 :
+	int iy = (h_y < 0.0) ? 0 :
 	         ((target[1] + tol > fymax) ? -1 :
 			  ((target[1] - tol < fymin) ? 1 : 0));
-	int iz = (hz < 0.0) ? 0 :
+	int iz = (h_z < 0.0) ? 0 :
 	         ((target[2] + tol > fzmax) ? -1 :
 			  ((target[2] - tol < fzmin) ? 1 : 0));
 
 	/* periodic extensions */
 	if (ix != 0)
 	{
-		target[0] += ix*hx;
+		target[0] += ix*h_x;
 		ProcessHitsSorted(target, tol, skiptag, fSortedHits);
 		
 		if (iy != 0)
 		{
-			target[1] += iy*hy;
+			target[1] += iy*h_y;
 			ProcessHitsSorted(target, tol, skiptag, fSortedHits);
 
 			if (iz != 0)
 			{
-				target[2] += iz*hz;
+				target[2] += iz*h_z;
 				ProcessHitsSorted(target, tol, skiptag, fSortedHits);
-				target[2] -= iz*hz;
+				target[2] -= iz*h_z;
 			}
 
-			target[1] -= iy*hy;
+			target[1] -= iy*h_y;
 		}
 
 		if (iz != 0)
 		{
-			target[2] += iz*hz;
+			target[2] += iz*h_z;
 			ProcessHitsSorted(target, tol, skiptag, fSortedHits);
-			target[2] -= iz*hz;
+			target[2] -= iz*h_z;
 		}
 			
-		target[0] -= ix*hx;
+		target[0] -= ix*h_x;
 	}
 
 	if (iy != 0)
 	{
-		target[1] += iy*hy;
+		target[1] += iy*h_y;
 		ProcessHitsSorted(target, tol, skiptag, fSortedHits);
 			
 		if (iz != 0)
 		{
-			target[2] += iz*hz;
+			target[2] += iz*h_z;
 			ProcessHitsSorted(target, tol, skiptag, fSortedHits);
-			target[2] -= iz*hz;
+			target[2] -= iz*h_z;
 		}
 
-		target[1] -= iy*hy;
+		target[1] -= iy*h_y;
 	}
 
 	if (iz != 0)
 	{
-		target[2] += iz*hz;
+		target[2] += iz*h_z;
 		ProcessHitsSorted(target, tol, skiptag, fSortedHits);
-		target[2] -= iz*hz;
+		target[2] -= iz*h_z;
 	}
 	
 	/* copy to output */
