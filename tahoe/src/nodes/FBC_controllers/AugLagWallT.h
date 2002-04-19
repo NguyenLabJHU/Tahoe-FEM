aa6dac2a5c475bb08357192199a5cbdd3721ac13
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-04-19 17:22:21 +0000

    stiffness matrix was singular if constraints are applied to nodes with prescribed DOF's. Rather than making the contact node list changes dynamically, check nodes for prescribed DOF's and make sure the stiffness contribution does not have a zero pivot for those.

--- tahoe/src/nodes/FBC_controllers/AugLagWallT.h
+++ tahoe/src/nodes/FBC_controllers/AugLagWallT.h
@@ -1,4 +1,4 @@
-/* $Id: AugLagWallT.h,v 1.1 2001-09-11 23:43:48 paklein Exp $ */
+/* $Id: AugLagWallT.h,v 1.2 2002-04-19 17:22:21 paklein Exp $ */
 
 #ifndef _AUGLAG_WALL_T_H_
 #define _AUGLAG_WALL_T_H_
@@ -63,16 +63,19 @@ private:
 
 private:
 
-	/* nodemanager(s) */
+	/** nodemanager */
 	XDOF_ManagerT* fXDOF_Nodes;
 	
 	/* contact equation sets (shallow copy of contact node equs) */
 	iArray2DT fContactEqnos2D;
 	iArray2DT fContactTags;
 	
-	/* contact DOF tags and DOF's */
-	iArrayT fContactDOFtags;
-	dArrayT fLastDOF;
+	/** \name Augmented multiplier info */
+	/*@{*/
+	iArrayT fContactDOFtags; /**< contact DOF tags and DOF's */
+	iArrayT fFloatingDOF;    /**< 1 if multiplier is attacted to node that has KBC's */
+	dArrayT fLastDOF;        /**< multiplier history */ 
+	/*@}*/
 };
 
 #endif /* _AUGLAG_WALL_T_H_ */
