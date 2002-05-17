a91ad3e5061a13d8f69961d40b73e3ad408af12b
dzeigle <dzeigle@75a0ec24-64db-4a18-816e-f4d92fc68425>
2002-05-17 22:45:01 +0000

    can now handle large input values

--- src/C1functions/GreenwoodWilliamson.cpp
+++ src/C1functions/GreenwoodWilliamson.cpp
@@ -1,4 +1,4 @@
-/* $Id: GreenwoodWilliamson.cpp,v 1.11 2002-05-16 15:53:08 dzeigle Exp $ */
+/* $Id: GreenwoodWilliamson.cpp,v 1.12 2002-05-17 22:45:01 dzeigle Exp $ */
 
 #include "GreenwoodWilliamson.h"
 #include <math.h>
@@ -13,6 +13,7 @@
 
 /* constants */
 const double PI = 2.0*acos(0.0);
+const double EXTOL = 35.0;
 
 /*
 * constructors
@@ -51,8 +52,6 @@ double GreenwoodWilliamson::Function(double x) const
 {
 	double value=0.0;
 
-	x /= fS;
-	
 	if (fP==1.0)
 	{
 		if (fS==0)
@@ -95,8 +94,6 @@ double GreenwoodWilliamson::DFunction(double x) const
 {
 	double value=0.0;
 	
-	x /= fS;
-
 	if (fP==1.0)
 	{
 		cout << "*** ERROR! Greenwood and Williamson area gradient unavailable.";
@@ -139,17 +136,44 @@ double GreenwoodWilliamson::DFunction(double x) const
 			for (int i=0; i<4; i++)	// clear entries
 				term[i] = 0.0;
 			
-			expo = 0.5*(fM-x)*(fM-x)/(fS*fS);
-			m5 = f5.Function(expo);
-			m7 = f7.Function(expo);
 			gn = h.Function(-0.25);
 			gp = h.Function(0.25);
-
-			term[0] = -2.0*sqrt(2.0)*fS*gp*m5;
-			term[1] = 3.0*(fM-x)*gn*m7;
-			term[2] = exp(-expo)*sqrt(PI*fS*(fM-x))*(term[0]+term[1]);
+			expo = 0.5*(fM-x)*(fM-x)/(fS*fS);
+			
+			if (expo > EXTOL)		// geometric cancellation of 
+							// Kummer function and exponential function
+			{
+				double a[2], b[2], ga[2], gb[2];
+				double app[2];
+				
+				a[0] = 5.0/4.0;
+				a[1] = 7.0/4.0;
+				b[0] = 0.5;
+				b[1] = 1.5;
+				ga[0] = h.Function(a[0]);
+				ga[1] = h.Function(a[1]);
+				gb[0] = h.Function(b[0]);
+				gb[1] = h.Function(b[1]);
+				
+				app[0] = gb[0]*pow(expo,a[0]-b[0])/ga[0];
+				app[1] = gb[1]*pow(expo,a[1]-b[1])/ga[1];
+				
+				term[0] = -2.0*sqrt(2.0)*fS*gp*app[0];
+				term[1] = 3.0*(fM-x)*gn*app[1];
+				term[2] = sqrt(PI*fS*(fM-x))*(term[0]+term[1]);
+			}
+			else
+			{
+				m5 = f5.Function(expo);
+				m7 = f7.Function(expo);
+			
+	
+				term[0] = -2.0*sqrt(2.0)*fS*gp*m5;
+				term[1] = 3.0*(fM-x)*gn*m7;
+				term[2] = exp(-expo)*sqrt(PI*fS*(fM-x))*(term[0]+term[1]);
+			}
+			
 			term[3] = 2.0*pow(2.0,0.25)*sqrt(fM-x)*gp*gn;
-		
 			if (term[3]==0)
 			{
 				cout << "**Error! - Zero denominator in GreenwoodWilliamson. **\n";
@@ -174,8 +198,6 @@ double GreenwoodWilliamson::DDFunction(double x) const
 {
 	double value = 0.0;
 
-	x /= fS;
-	
 	if (fP==1.0)
 	{
 		cout << "*** ERROR! Greenwood and Williamson area gradient unavailable.";
@@ -217,7 +239,7 @@ double GreenwoodWilliamson::DDFunction(double x) const
 		else if (x==fM)
 		{
 			Gamma g;
-		
+			
 			value = -3.0*sqrt(fS*PI)/(pow(2.0,5.0/4.0)*g.Function(0.25));
 		}
 		else if (x<fM)
@@ -229,20 +251,49 @@ double GreenwoodWilliamson::DDFunction(double x) const
 			for (int i=0; i<4; i++)	// clear entries
 				term[i] = 0.0;
 			
-			sqmux = fM*fM-fS*fS-2.0*fM*x+x*x;
-			expo = 0.5*(fM-x)*(fM-x)/(fS*fS);
-			m5 = f5.Function(expo);
-			m7 = f7.Function(expo);
-			m9 = f9.Function(expo);
-			m11 = f11.Function(expo);
+			sqmux = (fM-x)*(fM-x)-fS*fS;
 			gn = h.Function(-0.25);
 			gp = h.Function(0.25);
+			expo = 0.5*(fM-x)*(fM-x)/(fS*fS);
+			
+			if (expo > EXTOL)
+			{
+				double a[4], b[4], ga[4], gb[4], app[4];
+				
+				a[0] = 5.0/4.0;
+				a[1] = 7.0/4.0;
+				a[2] = 9.0/4.0;
+				a[3] = 11.0/4.0;
+				b[0] = 0.5;
+				b[1] = 1.5;
+				b[2] = 1.5;
+				b[3] = 2.5;
+				
+				for (int i=0; i<4; i++)
+				{
+					ga[i] = h.Function(a[i]);
+					gb[i] = h.Function(b[i]);
+					app[i] = gb[i]*pow(expo,a[i]-b[i])/ga[i];
+				}
+
+				term[0] = -(fM-x)*(-20.0*fS*gp*app[2]+7.0*sqrt(2.0)*(fM-x)*gn*app[3]);
+				term[1] = 8.0*fS*(x-fM)*gp*app[0]+6.0*sqrt(2.0)*sqmux*gn*app[1];
+				term[2] = sqrt(PI*(fM-x))*(term[1]+term[0]);				
+			}
+			else
+			{
+				m5 = f5.Function(expo);
+				m7 = f7.Function(expo);
+				m9 = f9.Function(expo);
+				m11 = f11.Function(expo);
+			
 		
-			term[0] = -(fM-x)*(-20.0*fS*gp*m9+7.0*sqrt(2.0)*(fM-x)*gn*m11);
-			term[1] = 8.0*fS*(x-fM)*gp*m5+6.0*sqrt(2.0)*sqmux*gn*m7;
-			term[2] = exp(-expo)*fS*sqrt(PI*(fM-x))*(term[1]+term[0]);
+				term[0] = -(fM-x)*(-20.0*fS*gp*m9+7.0*sqrt(2.0)*(fM-x)*gn*m11);
+				term[1] = 8.0*fS*(x-fM)*gp*m5+6.0*sqrt(2.0)*sqmux*gn*m7;
+				term[2] = exp(-expo)*sqrt(PI*(fM-x))*(term[1]+term[0]);
+			}
+			
 			term[3] = pow(2.0,11.0/4.0)*pow(fS,1.5)*sqrt(fM-x)*gn*gp;
-		
 			if (term[3]==0)
 			{
 				cout << "**Error! - Zero denominator in GreenwoodWilliamson. **\n";
@@ -260,6 +311,7 @@ double GreenwoodWilliamson::DDFunction(double x) const
 	
 	return (-value);
 }
+
 
 
 
@@ -281,7 +333,7 @@ dArrayT& GreenwoodWilliamson::MapFunction(const dArrayT& in, dArrayT& out) const
 	
 	for (int i = 0; i < in.Length(); i++)
 	{
-		r = (*pl++)/fS;
+		r = *pl++;
 		
 		if (fP==1)
 		{
@@ -340,8 +392,8 @@ dArrayT& GreenwoodWilliamson::MapDFunction(const dArrayT& in, dArrayT& out) cons
 		}
 		else if (fP==1.5)
 		{
-			r = (*pl++)/fS;
-			
+			r = *pl++;
+		
 			if (r>fM)
 			{
 				double term[3], k1, k3, k5, coef, expo;
@@ -360,7 +412,7 @@ dArrayT& GreenwoodWilliamson::MapDFunction(const dArrayT& in, dArrayT& out) cons
 				term[1] = 2.0*(fM*fM+2.0*fS*fS-2*fM*r+r*r)*k1+term[0];
 				term[2] = exp(-expo)*(fM-r)*term[1];
 		
-				value = coef*term[2];
+				value = coef*term[2];		
 			}
 			else if (r==fM)
 			{
@@ -377,24 +429,51 @@ dArrayT& GreenwoodWilliamson::MapDFunction(const dArrayT& in, dArrayT& out) cons
 				for (int i=0; i<4; i++)	// clear entries
 					term[i] = 0.0;
 			
-				expo = 0.5*(fM-r)*(fM-r)/(fS*fS);
-				m5 = f5.Function(expo);
-				m7 = f7.Function(expo);
 				gn = h.Function(-0.25);
 				gp = h.Function(0.25);
-		
-				term[0] = -2.0*sqrt(2.0)*fS*gp*m5;
-				term[1] = 3.0*(fM-r)*gn*m7;
-				term[2] = exp(-expo)*sqrt(PI)*sqrt(fS*(fM-r))*(term[0]+term[1]);
+				expo = 0.5*(fM-r)*(fM-r)/(fS*fS);
+			
+				if (expo > EXTOL)		// geometric cancellation of 
+										// Kummer function and exponential function
+				{
+					double a[2], b[2], ga[2], gb[2];
+					double app[2];
+				
+					a[0] = 5.0/4.0;
+					a[1] = 7.0/4.0;
+					b[0] = 0.5;
+					b[1] = 1.5;
+					ga[0] = h.Function(a[0]);
+					ga[1] = h.Function(a[1]);
+					gb[0] = h.Function(b[0]);
+					gb[1] = h.Function(b[1]);
+				
+					app[0] = gb[0]*pow(expo,a[0]-b[0])/ga[0];
+					app[1] = gb[1]*pow(expo,a[1]-b[1])/ga[1];
+				
+					term[0] = -2.0*sqrt(2.0)*fS*gp*app[0];
+					term[1] = 3.0*(fM-r)*gn*app[1];
+					term[2] = sqrt(PI*fS*(fM-r))*(term[0]+term[1]);
+				}
+				else
+				{
+					m5 = f5.Function(expo);
+					m7 = f7.Function(expo);
+			
+	
+					term[0] = -2.0*sqrt(2.0)*fS*gp*m5;
+					term[1] = 3.0*(fM-r)*gn*m7;
+					term[2] = exp(-expo)*sqrt(PI*fS*(fM-r))*(term[0]+term[1]);
+				}
+			
 				term[3] = 2.0*pow(2.0,0.25)*sqrt(fM-r)*gp*gn;
-		
 				if (term[3]==0)
 				{
 					cout << "**Error! - Zero denominator in GreenwoodWilliamson. **\n";
 					throw eBadInputValue;
 				}
 		
-				value = term[2]/term[3];
+				value = term[2]/term[3];		
 			}
 		}
 		else
@@ -426,19 +505,19 @@ dArrayT& GreenwoodWilliamson::MapDDFunction(const dArrayT& in, dArrayT& out) con
 		}
 		else if (fP==1.5)
 		{
-			r = (*pl++)/fS;
+			r = *pl++;
 			
 			if (r>fM)
 			{
-				double term[8], coef, expo, k1, k3, k5, k7, k9, kn1, kn3, sqmur;
+				double term[8], coef, expo, k1, k3, k5, k7, k9, kn1, kn3, sqmux;
 				ModBessel f1(0.25), f3(0.75), f5(1.25), f7(1.75), f9(2.25);
 				ModBessel fn1(-0.25), fn3(-0.75);
 		
 				for (int i=0; i<8; i++)	// clear entries
 					term[i] = 0.0;
-				
+			
 				coef = 1.0/(16.0*sqrt(PI*(r-fM))*fS*(r-fM));
-				sqmur = fM*fM+2.0*fS*fS-2.0*fM*r+r*r;
+				sqmux = fM*fM+2.0*fS*fS-2.0*fM*r+r*r;
 				expo = (fM-r)*(fM-r)/((2*fS)*(2*fS));
 				k1 = f1.Function(expo);
 				k3 = f3.Function(expo);
@@ -448,13 +527,13 @@ dArrayT& GreenwoodWilliamson::MapDDFunction(const dArrayT& in, dArrayT& out) con
 				kn1 = fn1.Function(expo);
 				kn3 = fn3.Function(expo);
 		
-				term[0] =- (fM-r)*(fM-r)*(kn1+k1+k7+k9)/(fS*fS);
+				term[0] = -(fM-r)*(fM-r)*(kn1+k1+k7+k9)/(fS*fS);
 				term[1] = -16.0*k1+8.0*(k3+k5);
-				term[2] = 2.0*sqmur*(kn3+k5)/(fS*fS);
+				term[2] = 2.0*sqmux*(kn3+k5)/(fS*fS);
 				term[3] = -0.5*(fM-r)*(fM-r)*(r-fM)*(term[1]+term[2]+term[0]);
-				term[4] = -(fM-r)*(fM-r)*(2.0*sqmur*k1-k3-k5)/(fS*fS);
-				term[5] = 2.0*(r-fM)*(2.0*sqmur*k1-(fM-r)*(fM-r)*(k3+k5));
-				term[6] = (fM-r)*(2*sqmur*k1-(fM-r)*(fM-r)*(k3+k5));
+				term[4] = -(fM-r)*(fM-r)*(r-fM)*(2.0*sqmux*k1-(fM-r)*(fM-r)*(k3+k5))/(fS*fS);
+				term[5] = 2.0*(r-fM)*(2.0*sqmux*k1-(fM-r)*(fM-r)*(k3+k5));
+				term[6] = (fM-r)*(2*sqmux*k1-(fM-r)*(fM-r)*(k3+k5));
 				term[7] = exp(-expo)*(term[6]+term[5]+term[4]+term[3]);
 		
 				value = coef*term[7];
@@ -462,32 +541,61 @@ dArrayT& GreenwoodWilliamson::MapDDFunction(const dArrayT& in, dArrayT& out) con
 			else if (r==fM)
 			{
 				Gamma g;
-		
+			
 				value = -3.0*sqrt(fS*PI)/(pow(2.0,5.0/4.0)*g.Function(0.25));
 			}
 			else if (r<fM)
 			{
-				double term[4], expo, m5, m7, m9, m11,sqmur, gn, gp;
+				double term[4], expo, m5, m7, m9, m11,sqmux, gn, gp;
 				Kummer f5(5.0/4.0,0.5), f7(7.0/4.0,1.5), f9(9.0/4.0,1.5), f11(11.0/4.0,2.5);
 				Gamma h;
 		
 				for (int i=0; i<4; i++)	// clear entries
 					term[i] = 0.0;
 			
-				sqmur = fM*fM-fS*fS-2.0*fM*r+r*r;
-				expo = 0.5*(fM-r)*(fM-r)/(fS*fS);
-				m5 = f5.Function(expo);
-				m7 = f7.Function(expo);
-				m9 = f9.Function(expo);
-				m11 = f11.Function(expo);
+				sqmux = (fM-r)*(fM-r)-fS*fS;
 				gn = h.Function(-0.25);
 				gp = h.Function(0.25);
+				expo = 0.5*(fM-r)*(fM-r)/(fS*fS);
+			
+				if (expo > EXTOL)
+				{
+					double a[4], b[4], ga[4], gb[4], app[4];
+				
+					a[0] = 5.0/4.0;
+					a[1] = 7.0/4.0;
+					a[2] = 9.0/4.0;
+					a[3] = 11.0/4.0;
+					b[0] = 0.5;
+					b[1] = 1.5;
+					b[2] = 1.5;
+					b[3] = 2.5;
+				
+					for (int i=0; i<4; i++)
+					{
+						ga[i] = h.Function(a[i]);
+						gb[i] = h.Function(b[i]);
+						app[i] = gb[i]*pow(expo,a[i]-b[i])/ga[i];
+					}
+				
+					term[0] = -(fM-r)*(-20.0*fS*gp*app[2]+7.0*sqrt(2.0)*(fM-r)*gn*app[3]);
+					term[1] = 8.0*fS*(r-fM)*gp*app[0]+6.0*sqrt(2.0)*sqmux*gn*app[1];
+					term[2] = sqrt(PI*(fM-r))*(term[1]+term[0]);
+				}
+				else
+				{
+					m5 = f5.Function(expo);
+					m7 = f7.Function(expo);
+					m9 = f9.Function(expo);
+					m11 = f11.Function(expo);
+			
 		
-				term[0] = -(fM-r)*(-20.0*fS*gp*m9+7.0*sqrt(2.0)*(fM-r)*gn*m11);
-				term[1] = 8.0*fS*(r-fM)*gp*m5+6.0*sqrt(2.0)*sqmur*gn*m7;
-				term[2] = exp(-expo)*fS*sqrt(PI*(fM-r))*(term[1]+term[0]);
+					term[0] = -(fM-r)*(-20.0*fS*gp*m9+7.0*sqrt(2.0)*(fM-r)*gn*m11);
+					term[1] = 8.0*fS*(r-fM)*gp*m5+6.0*sqrt(2.0)*sqmux*gn*m7;
+					term[2] = exp(-expo)*sqrt(PI*(fM-r))*(term[1]+term[0]);
+				}
+			
 				term[3] = pow(2.0,11.0/4.0)*pow(fS,1.5)*sqrt(fM-r)*gn*gp;
-		
 				if (term[3]==0)
 				{
 					cout << "**Error! - Zero denominator in GreenwoodWilliamson. **\n";
