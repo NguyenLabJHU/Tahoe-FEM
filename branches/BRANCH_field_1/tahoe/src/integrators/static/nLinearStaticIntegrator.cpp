c6c241a5705d2a79bb71808492df2e159a908f18
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-06-05 09:23:21 +0000

    added corrector

--- branches/BRANCH_field_1/tahoe/src/integrators/static/nLinearStaticIntegrator.cpp
+++ branches/BRANCH_field_1/tahoe/src/integrators/static/nLinearStaticIntegrator.cpp
@@ -1,8 +1,9 @@
-/* $Id: nLinearStaticIntegrator.cpp,v 1.1.4.1 2002-04-24 01:29:22 paklein Exp $ */
+/* $Id: nLinearStaticIntegrator.cpp,v 1.1.4.2 2002-06-05 09:23:21 paklein Exp $ */
 /* created: paklein (10/14/1996) */
 
 #include "nLinearStaticIntegrator.h"
 #include "BasicFieldT.h"
+#include "dArrayT.h"
 
 /* constructor */
 nLinearStaticIntegrator::nLinearStaticIntegrator(void) { };
@@ -12,4 +13,25 @@ void nLinearStaticIntegrator::Predictor(BasicFieldT& field)
 {
 	/* clear all displacements */
 	field[0] = 0.0;
+}
+
+/* correctors - map ACTIVE */
+void nLinearStaticIntegrator::Corrector(BasicFieldT& field, const dArrayT& update, 
+		int eq_start, int num_eq)
+{
+	const iArray2DT& eqnos = field.Equations();
+
+	/* add update - assumes that fEqnos maps directly into dva */
+	int    *peq = eqnos.Pointer();
+	double *pd  = field[0].Pointer();
+	for (int i = 0; i < eqnos.Length(); i++)
+	{
+		int eq = *peq++ - eq_start;
+	
+		/* active dof */
+		if (eq > -1 && eq < num_eq) *pd = update[eq];
+
+		/* next */
+		pd++;
+	}
 }
