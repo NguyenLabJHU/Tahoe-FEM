0058a0799bfd3aae4f9c664c72bb555834a19c01
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-07-18 17:45:23 +0000

    removed unneeded stuff and added references to the associated continuum and particle groups

--- tahoe/src/elements/bridging_scale/BridgingScaleT.h
+++ tahoe/src/elements/bridging_scale/BridgingScaleT.h
@@ -1,20 +1,15 @@
+/* $Id: BridgingScaleT.h,v 1.3 2002-07-18 17:45:23 paklein Exp $ */
 #ifndef _BRIDGING_SCALE_T_H_
 #define _BRIDGING_SCALE_T_H_
 
-/* base class */
-#include "ElementBaseT.h"
-
 /* direct members */
-#include "LocalArrayT.h"
-#include "GeometryT.h"
+#include "ElasticT.h"
+#include "RaggedArray2DT.h"
 
 namespace Tahoe {
 
 /* forward declarations */
-class MaterialListT;
-class ShapeFunctionT;
-class Traction_CardT;
-class StringT;
+class RodT;
 
 /** base class for elements using shape functions */
 class BridgingScaleT: public ElementBaseT
@@ -22,22 +17,15 @@ class BridgingScaleT: public ElementBaseT
 public:
 
 	/** constructor */
-	BridgingScaleT(const ElementSupportT& support, const FieldT& field);
+	BridgingScaleT(const ElementSupportT& support, const FieldT& field,
+		const RodT& particle,
+		const ElasticT& solid);
 
 	/** destructor */
 	virtual ~BridgingScaleT(void);
-		
-	/** number of element integration points */
-	int NumIP(void) const { return fNumIP;} ;
-	
-	/** reference to element shape functions */
+
+	/* accessors */
 	const ShapeFunctionT& ShapeFunction(void) const;
-
-	/** interpolate the nodal field values to the current integration point */
-    void IP_Interpolate(const LocalArrayT& nodal_u, dArrayT& ip_u) const;
-
-	/** interpolate the nodal field values to the specified integration point */
-    void IP_Interpolate(const LocalArrayT& nodal_u, dArrayT& ip_u, int ip) const;
 
 	/** element coordinates.
 	 * \return initial nodal coordinates of current element: [nen] x [nsd] */
@@ -57,9 +45,6 @@ public:
 	virtual void Equations(AutoArrayT<const iArray2DT*>& eq_1,
 		AutoArrayT<const RaggedArray2DT<int>*>& eq_2);
 
-	/* form of tangent matrix - symmetric by default */
-	virtual GlobalT::SystemTypeT TangentType(void) const;
-
 	/* initialize/finalize time increment */
 	virtual void InitStep(void);
 	virtual void CloseStep(void);
@@ -71,45 +56,20 @@ public:
 	/** send output */
 	virtual void WriteOutput(IOBaseT::OutputModeT mode);
 
-	/** return the geometry code */
-	GeometryT::CodeT GeometryCode(void) const;
-
-	/** reference to the materials list */
-	const MaterialListT& MaterialsList(void) const;
-	
-	/** mass types */
-	enum MassTypeT {kNoMass = 0, /**< do not compute mass matrix */
-            kConsistentMass = 1, /**< variationally consistent mass matrix */
-                kLumpedMass = 2  /**< diagonally lumped mass */ };
-
 protected:
-
-	/** stream extraction operator */
-	friend istream& operator>>(istream& in, BridgingScaleT::MassTypeT& type);
 
 	/** allocate and initialize local arrays */
 	virtual void SetLocalArrays(void);
-
-	/** allocate and initialize shape function objects */
-	virtual void SetShape(void) = 0;
 
 	/** form the residual force vector. computes contribution from natural
 	 * boundary conditions */
 	virtual void RHSDriver(void);
 
-	/** compute shape functions and derivatives */
-	virtual void SetGlobalShape(void);
-
 	/** write element group parameters to out */
 	virtual void PrintControlData(ostream& out) const;
 	
 	/* element data */
-	virtual void ReadMaterialData(ifstreamT& in);	
-	virtual void WriteMaterialData(ostream& out) const;
 	virtual void EchoOutputCodes(ifstreamT& in, ostream& out) = 0;
-
-	/** construct a new material list and return a pointer */
-	virtual MaterialListT* NewMaterialList(int size) const = 0;
 
 	/** write all current element information to the stream. used to generate
 	 * debugging information after runtime errors */
@@ -131,12 +91,6 @@ private:
 		const iArrayT& n_codes, ArrayT<StringT>& n_labels, 
 		const iArrayT& e_codes, ArrayT<StringT>& e_labels) const = 0;
 
-	/** return the default number of element nodes.
-	 * \note needed because ExodusII does not store \a any information about
-	 * empty element groups, which causes trouble for parallel execution
-	 * when a partition contains no elements from a group. */
-	virtual int DefaultNumElemNodes(void) const;
-
 	/* bridging scale-related computational functions */
 	
 	/* computes error caused by projecting solution onto FEM basis space */
@@ -147,23 +101,18 @@ private:
 
 protected:
 
-	/* materials */
-	MaterialListT* fMaterialList;  /**< list of materials */
+	/** "element" group calculating particle solution */
+	const RodT& fParticle;
 	
+	/** continuum group solving displacements */
+	const ElasticT& fSolid; 
+
+	/** list of particles per element: [n_cell] x [n_part_i] */
+	RaggedArray2DT<int> fParticlesInCell;
+
 	/* output control */
 	iArrayT	fNodalOutputCodes;
 	iArrayT	fElementOutputCodes;
-	  	
-	/* body force vector */
-	const ScheduleT* fBodySchedule; /**< body force schedule */
-	dArrayT fBody; /**< body force vector   */
-
-	/* traction data */
-	ArrayT<Traction_CardT> fTractionList;
-	int fTractionBCSet;
-
-	/** shape functions */
-	ShapeFunctionT* fShapes;
 	
 	/* arrays with local ordering */
 	LocalArrayT	fLocInitCoords;	/**< initial coords with local ordering */
@@ -176,33 +125,16 @@ protected:
 
 private:
 
-	/** number of integration points */
-	int	fNumIP;
-
 	/** output ID */
 	int fOutputID;
-
-	/* control data */
-	GeometryT::CodeT fGeometryCode;
 };
 
 /* inlines */
 
-/* return the geometry code */
-inline GeometryT::CodeT BridgingScaleT::GeometryCode(void) const
-{ return fGeometryCode; }
-
 /* accessors */
 inline const ShapeFunctionT& BridgingScaleT::ShapeFunction(void) const
 {
-#if __option(extended_errorcheck)
-	if (!fShapes)
-	{
-		cout << "\n BridgingScaleT::ShapeFunction: no shape functions" << endl;
-		throw eGeneralFail;
-	}
-#endif
-	return *fShapes;
+	return fSolid.ShapeFunction();
 }
 
 inline const LocalArrayT& BridgingScaleT::InitialCoordinates() const
@@ -213,12 +145,6 @@ inline const LocalArrayT& BridgingScaleT::InitialCoordinates() const
 inline const LocalArrayT& BridgingScaleT::Displacements() const
 {
 	return fLocDisp;
-}
-
-inline const MaterialListT& BridgingScaleT::MaterialsList(void) const
-{
-	if (!fMaterialList) throw eGeneralFail;
-	return *fMaterialList;
 }
 
 } // namespace Tahoe 
