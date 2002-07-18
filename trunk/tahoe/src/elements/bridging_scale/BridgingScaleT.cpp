1080b3e987102370b154620457170156267a1c54
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-07-18 17:45:23 +0000

    removed unneeded stuff and added references to the associated continuum and particle groups

--- trunk/tahoe/src/elements/bridging_scale/BridgingScaleT.cpp
+++ trunk/tahoe/src/elements/bridging_scale/BridgingScaleT.cpp
@@ -1,76 +1,36 @@
+/* $Id: BridgingScaleT.cpp,v 1.3 2002-07-18 17:45:23 paklein Exp $ */
 #include "BridgingScaleT.h"
+#include "ShapeFunctionT.h"
+#include "RodT.h"
 
 #include <iostream.h>
 #include <iomanip.h>
 
 #include "fstreamT.h"
-//#include "FEManagerT.h"
-#include "ModelManagerT.h"
-#include "StructuralMaterialT.h"
-#include "ShapeFunctionT.h"
-#include "DomainIntegrationT.h"
-#include "eControllerT.h"
-#include "Traction_CardT.h"
 #include "iAutoArrayT.h"
 #include "OutputSetT.h"
-#include "ScheduleT.h"
-
-//TEMP: all this for general traction BC implementation?
-#include "VariArrayT.h"
-#include "nVariArray2DT.h"
-#include "VariLocalArrayT.h"
-
-/* services */
-#include "EdgeFinderT.h"
-#include "GraphT.h"
-
-/* materials lists */
-#include "MaterialListT.h"
-#include "Material2DT.h"
-
-/* constructor */
 
 using namespace Tahoe;
 
+/* constructor */
 BridgingScaleT::BridgingScaleT(const ElementSupportT& support, 
-	const FieldT& field):
+	const FieldT& field,
+	const RodT& particle,
+	const ElasticT& solid):
 	ElementBaseT(support, field),
-	fMaterialList(NULL),
-	fBodySchedule(NULL),
-	fBody(NumDOF()),
-	fTractionBCSet(0),
-	fShapes(NULL),
+	fParticle(particle),
+	fSolid(solid),
 	fLocInitCoords(LocalArrayT::kInitCoords),
 	fLocDisp(LocalArrayT::kDisp),
 	fDOFvec(NumDOF())
 {
-	ifstreamT&  in = ElementSupport().Input();
-	ostream&    out = ElementSupport().Output();
-		
-	/* control parameters */
-	in >> fGeometryCode; //TEMP - should actually come from the geometry database
-	in >> fNumIP;
+
 }
 
 /* destructor */
 BridgingScaleT::~BridgingScaleT(void)
 {	
-	delete fShapes;
-	delete fMaterialList;
-}
 
-/* accessors */
-/* interpolate the nodal field values to the current integration point */
-void BridgingScaleT::IP_Interpolate(const LocalArrayT& nodal_u, dArrayT& ip_u) const
-{
-    /* computed by shape functions */
-    ShapeFunction().InterpolateU(nodal_u, ip_u);
-}
-
-void BridgingScaleT::IP_Interpolate(const LocalArrayT& nodal_u, dArrayT& ip_u, int ip) const
-{
-    /* computed by shape functions */
-    ShapeFunction().InterpolateU(nodal_u, ip_u, ip);
 }
 
 /* allocates space and reads connectivity data */
@@ -79,14 +39,9 @@ void BridgingScaleT::Initialize(void)
 	/* inherited */
 	ElementBaseT::Initialize();
 	
-	/* allocate work space */
-	fNEEvec.Allocate(NumElementNodes()*NumDOF());
-
-	/* initialize local arrays */
-	SetLocalArrays();
-
-	/* construct shape functions */
-	SetShape();
+	// stages of initialization
+	// (1) sort all particles into elements
+	// ...
 
 	/* streams */
 	ifstreamT& in = ElementSupport().Input();
@@ -94,21 +49,6 @@ void BridgingScaleT::Initialize(void)
 
 	/* output print specifications */
 	EchoOutputCodes(in, out);
-
-	/* echo material properties */
-	ReadMaterialData(in);	
-	WriteMaterialData(out);
-
-	/* get form of tangent */
-	GlobalT::SystemTypeT type = TangentType();
-	
-	/* set form of element stiffness matrix */
-	if (type == GlobalT::kSymmetric)
-		fLHS.SetFormat(ElementMatrixT::kSymmetricUpper);
-	else if (type == GlobalT::kNonSymmetric)
-		fLHS.SetFormat(ElementMatrixT::kNonSymmetric);
-	else if (type == GlobalT::kDiagonal)
-		fLHS.SetFormat(ElementMatrixT::kDiagonal);
 }
 
 void BridgingScaleT::Equations(AutoArrayT<const iArray2DT*>& eq_1,
@@ -116,26 +56,6 @@ void BridgingScaleT::Equations(AutoArrayT<const iArray2DT*>& eq_1,
 {
 	/* inherited */
 	ElementBaseT::Equations(eq_1, eq_2);
-
-	/* mark traction BC data as old */
-	fTractionBCSet = 0;
-}
-
-/* form of tangent matrix */
-GlobalT::SystemTypeT BridgingScaleT::TangentType(void) const
-{
-	/* initialize to lowest precedence */
-	GlobalT::SystemTypeT type = GlobalT::kDiagonal;
-
-	for (int i = 0; i < fMaterialList->Length(); i++)
-	{
-		GlobalT::SystemTypeT e_type = (*fMaterialList)[i]->TangentType();
-	
-		/* using type precedence */
-		type = (e_type > type) ? e_type : type;
-	}
-	
-	return type;
 }
 
 /* initialize/finalize step */
@@ -143,9 +63,6 @@ void BridgingScaleT::InitStep(void)
 {
 	/* inherited */
 	ElementBaseT::InitStep();
-
-	/* set material variables */
-	fMaterialList->InitStep();
 }
 
 /* initialize/finalize step */
@@ -153,30 +70,6 @@ void BridgingScaleT::CloseStep(void)
 {
 	/* inherited */
 	ElementBaseT::CloseStep();
-
-	/* set material variables */
-	fMaterialList->CloseStep();
-
-	/* update element level internal variables */
-	if (fMaterialList->HasHistoryMaterials())
-	{
-		Top();
-		while (NextElement())
-		{
-		        /* compute bridging scale related displacements */
-		        ComputeError();
-			ComputeFineScaleU();
-
-			ElementCardT& element = CurrentElement();
-			if (element.IsAllocated())
-			{
-				ContinuumMaterialT* pmat = (*fMaterialList)[element.MaterialNumber()];
-
-				/* material update function */
-				pmat->UpdateHistory();
-			}
-		}
-	}
 }
 
 /* resets to the last converged solution */
@@ -184,28 +77,12 @@ void BridgingScaleT::ResetStep(void)
 {
 	/* inherited */
 	ElementBaseT::ResetStep();
-
-	/* update material internal variables */
-	if (fMaterialList->HasHistoryMaterials())
-	{
-		Top();
-		while (NextElement())
-		{
-			ElementCardT& element = CurrentElement();		
-			if (element.IsAllocated())
-			{
-				ContinuumMaterialT* pmat = (*fMaterialList)[element.MaterialNumber()];
-
-				/* material reset function */
-				pmat->ResetHistory();
-			}
-		}
-	}
 }
 
 /* writing output */
 void BridgingScaleT::RegisterOutput(void)
 {
+#if 0
 //NOTE: could loop over each output mode and register
 //      it with the output separately. for now just register
 //      "kAtInc"
@@ -234,6 +111,7 @@ void BridgingScaleT::RegisterOutput(void)
 		
 	/* register and get output ID */
 	fOutputID = ElementSupport().RegisterOutput(output_set);
+#endif
 }
 
 //NOTE - this function is/was identical to CSEBaseT::WriteOutput
@@ -267,34 +145,6 @@ void BridgingScaleT::WriteOutput(IOBaseT::OutputModeT mode)
 * Protected
 ***********************************************************************/
 
-namespace Tahoe {
-
-/* stream extraction operator */
-istream& operator>>(istream& in, BridgingScaleT::MassTypeT& type)
-{
-	int i_type;
-	in >> i_type;
-	switch (i_type)
-	{
-		case BridgingScaleT::kNoMass:
-			type = BridgingScaleT::kNoMass;
-			break;
-		case BridgingScaleT::kConsistentMass:
-			type = BridgingScaleT::kConsistentMass;
-			break;
-		case BridgingScaleT::kLumpedMass:
-			type = BridgingScaleT::kLumpedMass;
-			break;
-		default:
-			cout << "\n BridgingScaleT::MassTypeT: unknown type: "
-			<< i_type<< endl;
-			throw eBadInputValue;	
-	}
-	return in;
-}
-
-}
-
 /* initialize local arrays */
 void BridgingScaleT::SetLocalArrays(void)
 {
@@ -313,62 +163,14 @@ void BridgingScaleT::RHSDriver(void)
 	// should call derived class function here	
 }
 
-/* form global shape function derivatives */
-void BridgingScaleT::SetGlobalShape(void)
-{
-	/* fetch (initial) coordinates */
-	SetLocalX(fLocInitCoords);
-	
-	/* compute shape function derivatives */
-	fShapes->SetDerivatives();
-}
-
 /* print element group data */
 void BridgingScaleT::PrintControlData(ostream& out) const
 {
 	/* inherited */
 	ElementBaseT::PrintControlData(out);
 
-	out << " Element geometry code . . . . . . . . . . . . . = " << fGeometryCode << '\n';
-	out << "    eq." << GeometryT::kPoint         << ", point\n";
-	out << "    eq." << GeometryT::kLine          << ", line\n";
-	out << "    eq." << GeometryT::kQuadrilateral << ", quadrilateral\n";
-	out << "    eq." << GeometryT::kTriangle	  << ", triangle\n";
-	out << "    eq." << GeometryT::kHexahedron	  << ", hexahedron\n";
-	out << "    eq." << GeometryT::kTetrahedron   << ", tetrahedron\n";
-	out << " Number of integration points. . . . . . . . . . = " << fNumIP    << '\n';
-}
-
-void BridgingScaleT::ReadMaterialData(ifstreamT& in)
-{
-	/* construct material list */
-	int size;
-	in >> size;
-	fMaterialList = NewMaterialList(size);
-	if (!fMaterialList) throw eOutOfMemory;
-
-	/* read */
-	fMaterialList->ReadMaterialData(in);
-	
-	/* check range */
-	for (int i = 0; i < fBlockData.Length(); i++)
-		if (fBlockData[i].MaterialID() < 0 ||
-		    fBlockData[i].MaterialID() >= size)
-		{
-			cout << "\n BridgingScaleT::ReadMaterialData: material number "
-			     << fBlockData[i].MaterialID() + 1 << '\n';
-			cout<<    "     for element block " << i + 1 << " is out of range" << endl;
-			throw eBadInputValue;
-		}
-}
-
-/* use in conjunction with ReadMaterialData */
-void BridgingScaleT::WriteMaterialData(ostream& out) const
-{
-	fMaterialList->WriteMaterialData(out);
-
-	/* flush buffer */
-	out.flush();
+	out << " Particle group number . . . . . . . . . . . . . = " << ElementSupport().ElementGroupNumber(&fParticle) + 1 << '\n';
+	out << " Continuum group number. . . . . . . . . . . . . = " << ElementSupport().ElementGroupNumber(&fSolid) + 1 << '\n';
 }
 
 /* write all current element information to the stream */
@@ -393,33 +195,6 @@ void BridgingScaleT::CurrElementInfo(ostream& out) const
 /***********************************************************************
 * Private
 ***********************************************************************/
-
-/* return the default number of element nodes */
-int BridgingScaleT::DefaultNumElemNodes(void) const
-{
-	switch (fGeometryCode)
-	{
-		case GeometryT::kLine:
-			return 2;
-		case GeometryT::kQuadrilateral:
-			return 4;
-		case GeometryT::kTriangle:
-			return 3;
-		case GeometryT::kHexahedron:
-			return 8;
-		case GeometryT::kTetrahedron:
-			return 4;
-		case GeometryT::kPentahedron:
-			return 6;
-		default:
-			cout << "\n BridgingScaleT::DefaultNumElemNodes: unknown geometry code: "
-			     << fGeometryCode << endl;
-			return 0;
-	}
-}
-//NOTE: needed because ExodusII does not store ANY information about
-//      empty element groups, which causes trouble for parallel execution
-//      when a partition contains no element from a group.
 
 void BridgingScaleT::ComputeError(void)
 {
