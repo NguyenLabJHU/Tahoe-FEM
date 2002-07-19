bbefd75da649251d3ea57645e8859b0b84c6e11b
recampb <recampb@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-07-19 17:28:25 +0000

    continuing trial run

--- trunk/contrib/vtk/java/test.java
+++ trunk/contrib/vtk/java/test.java
@@ -1,21 +1,16 @@
-// $Id: test.java,v 1.1 2002-07-17 23:28:42 paklein Exp $
+// $Id: test.java,v 1.2 2002-07-19 17:28:25 recampb Exp $
 import java.io.*;
 
 public class test {
 
-	// looks like an unnamed static function
-	static {
-		System.loadLibrary("testClass");
+  // looks like an unnamed static function
+  static {
+	  System.loadLibrary("testClass");
 	}
-		
-	// "native" function to construct C++ object
-	test() {
-		initCppSide();
-	}
-	
-	public native void Print();
-	public native void initCppSide();
-	
-	// pointer to the C++ object
-	private long cpp_ptr;	
+  
+  public native void Print();
+  
+  public static void main(String[] args){
+    new test().Print();
+  }
 }
--- trunk/contrib/vtk/java/testImpl.cpp
+++ trunk/contrib/vtk/java/testImpl.cpp
@@ -1,20 +1,17 @@
-// $Id: testImpl.cpp,v 1.2 2002-07-17 23:53:28 paklein Exp $
+// $Id: testImpl.cpp,v 1.3 2002-07-19 17:28:25 recampb Exp $
 //#include <StubPreamble.h>
 // not needed for 1.3.1
 
 #include "test.h"
 #include "testClass.h"
 
-JNIEXPORT void JNICALL Java_test_initCppSide(JNIEnv *, jobject javaObj)
-{
-	testClass* tc = new testClass(5);
-	//unhand(javaObj)->cpp_ptr = (long) tc;
-	javaObj->cpp_ptr = (long) tc;
-}
 
 JNIEXPORT void JNICALL Java_test_Print(JNIEnv *, jobject javaObj)
 {
   //testClass* tc = (testClass*) unhand(javaObj)->cpp_ptr;
-	testClass* tc = (testClass*) javaObj->cpp_ptr;
-	tc->Print(cout);
+  //testClass* tc = (testClass*) javaObj->cpp_ptr;
+  //	tc->Print(cout);
+  cout << "Hello world" << endl;
+  return;
+
 }
