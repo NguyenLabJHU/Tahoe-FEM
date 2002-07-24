01c4e0d8c8b25a1db02aee6472c4cc656e2b0f6a
recampb <recampb@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-07-24 19:01:28 +0000

    Testing with simple GUI

--- contrib/vtk/java/test.java
+++ contrib/vtk/java/test.java
@@ -1,15 +1,115 @@
-// $Id: test.java,v 1.3 2002-07-20 01:59:11 paklein Exp $
+// $Id: test.java,v 1.4 2002-07-24 19:01:28 recampb Exp $
 import java.io.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.tree.*;
 
-public class test {
+public class test extends JPanel implements ActionListener {
 
   // looks like an unnamed static function
   static {
 	  System.loadLibrary("testClass");
+	  //System.loadLibrary("toolbox");
 	}
   
   public native void InitCpp();  
   public native void Print();
-  
-	long cpp_obj;
-}
+  public native void SetMinSc(int x);
+
+ 
+  long cpp_obj;
+  long console;
+  protected JButton testButton, b2;
+  protected JTextField minScalarTF;
+
+  public test(){
+
+    InitCpp();
+    Print();
+    JTabbedPane tabbedPane = new JTabbedPane();
+
+    JPanel rootPanel = new JPanel(false);
+    JPanel framePanel = new JPanel(false);
+    JPanel bodyPanel = new JPanel(false);
+    JPanel bodyVarPanel = new JPanel(false);
+    tabbedPane.addTab("Root Commands", rootPanel);
+    tabbedPane.addTab("Frame Commands", framePanel);
+    tabbedPane.addTab("Body Commands", bodyPanel);
+    tabbedPane.addTab("Body Variables", bodyVarPanel);
+    bodyVarPanel.setLayout(new GridLayout(2,2,20,5));
+
+    minScalarTF = new JTextField(Long.toString(cpp_obj), 8);
+    JLabel minScalarL = new JLabel("min_Scalar_Range");
+    JButton bodyVarOKButton = new JButton("OK");
+    JButton bodyVarCancelButton = new JButton("Cancel");
+    bodyVarOKButton.addActionListener(this);
+    bodyVarOKButton.setActionCommand("BodyVarOK");
+
+    bodyVarPanel.add(minScalarL);
+    bodyVarPanel.add(minScalarTF);
+    bodyVarPanel.add(bodyVarOKButton);
+    bodyVarPanel.add(bodyVarCancelButton);
+
+
+    setLayout(new GridLayout(1, 2)); 
+    add(tabbedPane);
+ 
+    testButton = new JButton ("Exit");
+    testButton.addActionListener(this);
+    testButton.setActionCommand("Exit");
+    rootPanel.add(testButton);
+
+    b2 = new JButton ("Print value");
+    b2.addActionListener(this);
+    b2.setActionCommand("Print value");
+    rootPanel.add(b2);
+    
+
+    DynamicTree treePanel = new DynamicTree();
+    populateTree(treePanel);
+
+    //setLayout(new BorderLayout());
+    //treePanel.setPreferredSize(new Dimension(300,150));
+    add(treePanel);
+  }
+
+  public void actionPerformed(ActionEvent e){
+    if (e.getActionCommand().equals("Exit")){
+      System.exit(0);  
+    }
+
+    else if (e.getActionCommand().equals("Print value")){
+      Print();
+
+    }
+    
+    else if (e.getActionCommand().equals("BodyVarOK")){
+      SetMinSc(Integer.parseInt(minScalarTF.getText()));
+      
+    }
+    
+  }
+
+
+    public void populateTree(DynamicTree treePanel) {
+        String p1Name = new String("Parent 1");
+        String p2Name = new String("Parent 2");
+        String c1Name = new String("Child 1");
+        String c2Name = new String("Child 2");
+
+        DefaultMutableTreeNode p1, p2;
+
+        p1 = treePanel.addObject(null, p1Name);
+        p2 = treePanel.addObject(null, p2Name);
+
+        treePanel.addObject(p1, c1Name);
+        treePanel.addObject(p1, c2Name);
+
+        treePanel.addObject(p2, c1Name);
+        treePanel.addObject(p2, c2Name);
+    }
+
+
+
+} // class test
--- contrib/vtk/java/testClass.cpp
+++ contrib/vtk/java/testClass.cpp
@@ -1,4 +1,4 @@
-/* $Id: testClass.cpp,v 1.2 2002-07-20 01:59:11 paklein Exp $ */
+/* $Id: testClass.cpp,v 1.3 2002-07-24 19:01:28 recampb Exp $ */
 #include "testClass.h"
 
 testClass::testClass(int a): a_(a)
@@ -8,3 +8,9 @@ void testClass::Print(ostream& out)
 {
 	out << "\n testClass::Print: field value is " << a_ << endl;
 }
+
+void testClass::SetA(int x)
+{
+a_ = x;
+}
+  
