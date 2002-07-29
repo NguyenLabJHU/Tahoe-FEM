805654216f1250bf8094e05eb629cb9f950997a5
saubry <saubry@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-07-29 19:12:42 +0000

    New version of rotation in 3D.

--- trunk/toolbox/src/misc/Rotate3DT.cpp
+++ trunk/toolbox/src/misc/Rotate3DT.cpp
@@ -1,8 +1,6 @@
 /* This class provides the functionality to do 3D coordinate */
 /* transformations.                                          */
 
-/* ....TO BE CHECKED (SA 07/25/02)....*/
-
 #include "Rotate3DT.h"
 #include <math.h>
 #include "Constants.h"
@@ -18,32 +16,39 @@ const double Pi = acos(-1.0);
 /*
 * constructor
 */
-Rotate3DT::Rotate3DT(void): fAngleDeg1(0.0), fAngleDeg2(0.0),fAngle1(0.0), fAngle2(0.0),
+Rotate3DT::Rotate3DT(void): fAngleDeg(0.0), fDir(kMatrixDim),fAngle(0.0), 
 			    fQ(kMatrixDim), fVec(kMatrixDim)
 {
 
 }
 
-Rotate3DT::Rotate3DT(double angle1,double angle2): fAngleDeg1(angle1), fAngleDeg2(angle2),
-						   fQ(kMatrixDim),fVec(kMatrixDim)
+Rotate3DT::Rotate3DT(dArrayT u,double angle): fAngleDeg(angle), fDir(kMatrixDim), 
+					      fAngle(0.0),fQ(kMatrixDim),fVec(kMatrixDim)
 {
-	SetAngle(fAngleDeg1,fAngleDeg2);
+  SetAngle(u,fAngleDeg);
 }
 
-void Rotate3DT::SetAngle(double angle1,double angle2)
+void Rotate3DT::SetAngle(dArrayT u,double angle)
 {
-	fAngle1 = angle1*Pi/180.0;
-	fAngle2 = angle2*Pi/180.0;
+  fAngle = angle*Pi/180.0; // angle in radians
+  for (int i=0; i<kMatrixDim; i++) fDir[i] = u[i];
 
-	fQ(0,0) = cos(angle1)*sin(angle2);
-	fQ(1,0) = sin(angle1)*sin(angle2);
-	fQ(2,0) = cos(angle2);
 
-	fQ(0,1) = cos(angle1)*cos(angle2);
-	fQ(1,1) = sin(angle1)*cos(angle2);
-	fQ(2,1) = -sin(angle2);
-
-	fQ(0,2) = fQ(1,0)*fQ(2,1)-fQ(2,0)*fQ(1,1);
-	fQ(1,2) =-(fQ(0,0)*fQ(2,1)-fQ(2,0)*fQ(0,1));
-	fQ(2,2) = fQ(0,0)*fQ(1,1)-fQ(1,0)*fQ(0,1);
+  
+  for (int i=0; i<kMatrixDim; i++)
+    {
+      for (int j=0; j<kMatrixDim; j++)
+	fQ(i,j) = (1.-fAngle)*fDir[i]*fDir[j];
+      fQ(i,i) += cos(fAngle);
+    }
+  
+  
+  fQ(1,0) += sin(fAngle)*fDir[2];
+  fQ(2,0) -= sin(fAngle)*fDir[1];
+  
+  fQ(0,1) -= sin(fAngle)*fDir[2];
+  fQ(2,1) += sin(fAngle)*fDir[0];
+  
+  fQ(0,2) += sin(fAngle)*fDir[1];
+  fQ(1,2) -= sin(fAngle)*fDir[0];
 }
--- trunk/toolbox/src/misc/Rotate3DT.h
+++ trunk/toolbox/src/misc/Rotate3DT.h
@@ -1,16 +1,13 @@
 /* This class provides the functionality to do 3D coordinate */
 /* transformations. Similar to Rotate2D but less routines*/
 
+/* Rotation in 3D defined by a direction  and an angle */
 
-
-/* ....TO BE CHECKED (SA 07/25/02)....*/
- 
 #ifndef _ROTATE3D_T_H_
 #define _ROTATE3D_T_H_
 
 /* direct members */
 #include "dMatrixT.h"
-#include "dSymMatrixT.h"
 #include "dArrayT.h"
 
 
@@ -22,13 +19,13 @@ public:
 
 	/* constructor - angle(degrees) */
 	Rotate3DT(void);
-	Rotate3DT(double angle1,double angle2);
+	Rotate3DT(dArrayT u,double angle);
 
 	/* set the angle field and associated work variables - angle
 	 * passed in degrees */
-	void SetAngle(double angle1,double angle2);
-	double Angle1(void) const; /* angle1 returned in degrees */
-	double Angle2(void) const; /* angle2 returned in degrees */
+	void SetAngle(dArrayT u,double angle);
+	double Angle(void) const; /* angle returned in degrees */
+	dArrayT Direction(void) const;
 	
 	/* transformation tensor */
 	const dMatrixT& Q(void) const;
@@ -41,8 +38,9 @@ public:
 	
 private:
 
-	double	fAngleDeg1,fAngleDeg2; /* angles in degrees */
-	double	fAngle1,fAngle2; /* angles in radians */
+	dArrayT fDir;
+	double	fAngleDeg; /* angle in degrees */
+	double	fAngle; /* angles in radians */
 
 	dMatrixT	fQ; 
 	dArrayT		fVec;
@@ -55,8 +53,8 @@ private:
 inline const dMatrixT& Rotate3DT::Q(void) const { return (fQ); }
 
 /* angle returned in degrees */
-inline double Rotate3DT::Angle1(void) const { return(fAngleDeg1); }
-inline double Rotate3DT::Angle2(void) const { return(fAngleDeg2); }
+inline double Rotate3DT::Angle(void) const { return(fAngleDeg); }
+inline dArrayT Rotate3DT::Direction(void) const { return(fDir); }
 
 /* vectors */
 inline const dArrayT& Rotate3DT::RotateVectorIn(const dArrayT& vector)
