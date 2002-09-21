ae3db624e542511dde05950ef7b9636c653b937d
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-09-21 09:09:59 +0000

    (1) B matricies are now calculated by the elements instead of by     ShapeFunctionT. (2) ElasticT no longer provides a default implementation for FormKd     and FormStiffness. These are now pure virtual. (3) ElasticT::Set_B sets up the "standard" B matrix for solids. (4) SmallStrainT now has an implementation of FormKd and FormStiffness. (5) SmallStrainT can do B-bar.
    NOTE: the solids benchmarks don't pass yet.

--- branches/BRANCH_B_1/tahoe/src/elements/continuum/solid/SmallStrainT.cpp
+++ branches/BRANCH_B_1/tahoe/src/elements/continuum/solid/SmallStrainT.cpp
@@ -1,4 +1,4 @@
-/* $Id: SmallStrainT.cpp,v 1.6 2002-07-17 00:00:49 paklein Exp $ */
+/* $Id: SmallStrainT.cpp,v 1.6.2.1 2002-09-21 09:09:59 paklein Exp $ */
 
 #include "SmallStrainT.h"
 #include "ShapeFunctionT.h"
@@ -93,33 +93,96 @@ void SmallStrainT::SetLocalArrays(void)
 	ElasticT::SetLocalArrays();
 
 	/* using B-bar */
-	if (fStrainDispOpt == ShapeFunctionT::kMeanDilBbar)
-		fLocDispTranspose.Allocate(fLocDisp.Length());
+	if (fStrainDispOpt == kMeanDilBbar) {
+		fLocDispTranspose.Dimension(fLocDisp.Length());
+		fMeanDilatation.Dimension(NumSD(), NumElementNodes());
+	}
 }
 
-/* increment current element */
-void SmallStrainT::SetGlobalShape(void)
+/* calculate the internal force contribution ("-k*d") */
+void SmallStrainT::FormKd(double constK)
 {
-	/* inherited */
-	ElasticT::SetGlobalShape();
+	const double* Det    = fShapes->IPDets();
+	const double* Weight = fShapes->IPWeights();
+	
+	/* collect incremental heat */
+	bool need_heat = fElementHeat.Length() == fShapes->NumIP();
+	
+	fShapes->TopIP();
+	while (fShapes->NextIP())
+	{
+		/* strain displacement matrix */
+		if (fStrainDispOpt == kMeanDilBbar)
+			Set_B_bar(fShapes->Derivatives_U(), fMeanDilatation, fB);
+		else
+			Set_B(fShapes->Derivatives_U(), fB);
 
+		/* B^T * Cauchy stress */
+		fB.MultTx(fCurrMaterial->s_ij(), fNEEvec);
+		
+		/* accumulate */
+		fRHS.AddScaled(constK*(*Weight++)*(*Det++), fNEEvec);
+		
+		/* incremental heat generation */
+		if (need_heat) 
+			fElementHeat[fShapes->CurrIP()] += fCurrMaterial->IncrementalHeat();
+	}	
+}
+
+/* form the element stiffness matrix */
+void SmallStrainT::FormStiffness(double constK)
+{
+	/* matrix format */
+	dMatrixT::SymmetryFlagT format =
+		(fLHS.Format() == ElementMatrixT::kNonSymmetric) ?
+		dMatrixT::kWhole :
+		dMatrixT::kUpperOnly;
+
+	/* integrate element stiffness */
+	const double* Det    = fShapes->IPDets();
+	const double* Weight = fShapes->IPWeights();
+	
+	fShapes->TopIP();
+	while ( fShapes->NextIP() )
+	{
+		double scale = constK*(*Det++)*(*Weight++);
+	
+		/* strain displacement matrix */
+		if (fStrainDispOpt == kMeanDilBbar)
+			Set_B_bar(fShapes->Derivatives_U(), fMeanDilatation, fB);
+		else
+			Set_B(fShapes->Derivatives_U(), fB);
+
+		/* get D matrix */
+		fD.SetToScaled(scale, fCurrMaterial->c_ijkl());
+							
+		/* multiply b(transpose) * db, taking account of symmetry, */
+		/* and accumulate in elstif */
+		fLHS.MultQTBQ(fB, fD, format, dMatrixT::kAccumulate);	
+	}
+}
+
+/* compute the measures of strain/deformation over the element */
+void SmallStrainT::SetDeformation(void)
+{
 	/* material information */
 	int material_number = CurrentElement().MaterialNumber();
 	const ArrayT<bool>& needs = fMaterialNeeds[material_number];
 	
 	/* using B-bar */
-	if (fStrainDispOpt == ShapeFunctionT::kMeanDilBbar)
+	if (fStrainDispOpt == kMeanDilBbar)
 	{
 		/* compute mean dilatation */
-		fShapes->SetMeanDilatation();
+		SetMeanDilatation(fMeanDilatation);
 
 		/* loop over integration points */
 		fShapes->TopIP();
 		while (fShapes->NextIP())
 		{
-			/* get B-bar */
-			fShapes->B(fB);
-		
+			/* set B-bar */
+			int ip = fShapes->CurrIP();
+			Set_B_bar(fShapes->Derivatives_U(ip), fMeanDilatation, fB);
+	
 			/* deformation gradient */
 			if (needs[fNeedsOffset + kstrain])
 			{
@@ -127,7 +190,7 @@ void SmallStrainT::SetGlobalShape(void)
 				fLocDisp.ReturnTranspose(fLocDispTranspose);
 
 				/* compute strain using B-bar */
-				dSymMatrixT& strain = fStrain_List[fShapes->CurrIP()];
+				dSymMatrixT& strain = fStrain_List[ip];
 				fB.Multx(fLocDispTranspose, strain);
 				strain.ScaleOffDiagonal(0.5);
 			}
@@ -139,7 +202,7 @@ void SmallStrainT::SetGlobalShape(void)
 				fLocLastDisp.ReturnTranspose(fLocDispTranspose);
 
 				/* compute strain using B-bar */
-				dSymMatrixT& strain = fStrain_last_List[fShapes->CurrIP()];
+				dSymMatrixT& strain = fStrain_last_List[ip];
 				fB.Multx(fLocDispTranspose, strain);
 				strain.ScaleOffDiagonal(0.5);
 			}
@@ -169,6 +232,121 @@ void SmallStrainT::SetGlobalShape(void)
 				/* symmetric part */
 				 fStrain_last_List[i].Symmetrize(fGradU);
 			}
+		}
+	}
+}
+
+/***********************************************************************
+ * Private
+ ***********************************************************************/
+
+/* compute mean dilatation, Hughes (4.5.23) */
+void SmallStrainT::SetMeanDilatation(dArray2DT& mean_dilatation) const
+{
+	int nip = NumIP();
+	const double* det = fShapes->IPDets();
+	const double*   w = fShapes->IPWeights();
+
+	/* volume */
+	double vol = 0.0;
+	for (int i = 0; i < nip; i++)
+		vol += w[i]*det[i];
+
+	/* initialize */
+	mean_dilatation = 0.0;			
+
+	/* integrate */
+	for (int i = 0; i < nip; i++)
+		mean_dilatation.AddScaled(w[i]*det[i]/vol, fShapes->Derivatives_U(i));
+}
+
+/* set B-bar as given by Hughes (4.5.11-16) */
+void SmallStrainT::Set_B_bar(const dArray2DT& DNa, const dArray2DT& mean_dilatation, 
+	dMatrixT& B)
+{
+#if __option(extended_errorcheck)
+	if (B.Rows() != dSymMatrixT::NumValues(DNa.MajorDim()) ||
+	    B.Cols() != DNa.Length() ||
+	    mean_dilatation.MinorDim() != DNa.MinorDim() ||
+	    mean_dilatation.MajorDim() != DNa.MajorDim())
+	    throw eSizeMismatch;
+#endif
+
+	int nnd = DNa.MinorDim();
+	double* pB = B.Pointer();
+
+	/* 1D */
+	if (DNa.MajorDim() == 1)
+	{
+		cout << "\n SmallStrainT::Set_B_bar: not implemented yet for 1D B-bar" << endl;
+		throw eGeneralFail;
+	}
+	/* 2D */
+	else if (DNa.MajorDim() == 2)
+	{
+		double* pNax = DNa(0);
+		double* pNay = DNa(1);
+			
+		double* pBmx = mean_dilatation(0);
+		double* pBmy = mean_dilatation(1);
+			
+		for (int i = 0; i < nnd; i++)
+		{
+			double factx = ((*pBmx++) - (*pNax))/3.0;
+			double facty = ((*pBmy++) - (*pNay))/3.0;
+			
+			/* Hughes (4.5.11-16) */
+			*pB++ = *pNax + factx;
+			*pB++ = factx;
+			*pB++ = *pNay;
+	
+			*pB++ = facty;
+			*pB++ = *pNay + facty;
+			*pB++ = *pNax;
+				
+			pNax++; pNay++;
+		}
+	}
+	/* 3D */
+	else		
+	{
+		double* pNax = DNa(0);
+		double* pNay = DNa(1);
+		double* pNaz = DNa(2);
+
+		double* pBmx = mean_dilatation(0);
+		double* pBmy = mean_dilatation(1);
+		double* pBmz = mean_dilatation(2);
+			
+		for (int i = 0; i < nnd; i++)
+		{
+			double factx = ((*pBmx++) - (*pNax))/3.0;
+			double facty = ((*pBmy++) - (*pNay))/3.0;
+			double factz = ((*pBmz++) - (*pNaz))/3.0;
+
+			/* Hughes (4.5.11-16) */
+			*pB++ = *pNax + factx;
+			*pB++ = factx;
+			*pB++ = factx;
+			*pB++ = 0.0;
+			*pB++ = *pNaz;
+			*pB++ = *pNay;
+
+			*pB++ = facty;
+			*pB++ = *pNay + facty;
+			*pB++ = facty;
+			*pB++ = *pNaz;
+			*pB++ = 0.0;
+			*pB++ = *pNax;
+	
+			*pB++ = factz;
+			*pB++ = factz;
+			*pB++ = *pNaz + factz;
+			*pB++ = *pNay;
+			*pB++ = *pNax;
+			*pB++ = 0.0;
+				
+			pNax++; pNay++; pNaz++;
 		}
 	}
 }
--- branches/BRANCH_B_1/tahoe/src/elements/continuum/solid/TotalLagrangianT.h
+++ branches/BRANCH_B_1/tahoe/src/elements/continuum/solid/TotalLagrangianT.h
@@ -1,4 +1,4 @@
-/* $Id: TotalLagrangianT.h,v 1.6 2002-07-05 22:28:03 paklein Exp $ */
+/* $Id: TotalLagrangianT.h,v 1.6.2.1 2002-09-21 09:09:59 paklein Exp $ */
 /* created: paklein (07/03/1996) */
 
 #ifndef _TOTAL_LAGRANGRIAN_T_H_
@@ -10,57 +10,39 @@
 /* direct members */
 #include "dMatrixT.h"
 
-
 namespace Tahoe {
 
+/** total Lagrangian, finite strain element */
 class TotalLagrangianT: public FiniteStrainT
 {
 public:
 
-	/* constructors */
+	/** constructors */
 	TotalLagrangianT(const ElementSupportT& support, const FieldT& field);
 
-	/* data initialization */
+	/** data initialization */
 	virtual void Initialize(void);
 		
 protected:
 
-	/* form the element stiffness matrix */
+	/** form the element stiffness matrix */
 	virtual void FormStiffness(double constK);
-	
-//DEV - Rayleigh damping should be added to the constitutive level
-#if 0
-	/* compute the effective acceleration and velocities based
-	 * on the algorithmic flags formXx and the given constants
-	 * constXx.
-	 *
-	 *		acc_eff  = constMa acc  + constCv a vel
-	 *      disp_eff = constKd disp
-	 *
-	 * where a and b are the Rayleigh damping coefficients.
-	 *
-	 *        ***The effective displacement does not include
-	 *           velocity since the internal force is a nonlinear
-	 *           function of the displacements
-*/
-	virtual void ComputeEffectiveDVA(int formBody,
-		int formMa, double constMa, int formCv, double constCv,
-		int formKd, double constKd);
-#endif
 
-	/* calculate the internal force contribution ("-k*d") */
+	/** calculate the internal force contribution ("-k*d") */
 	virtual void FormKd(double constK);
 
 protected:
 
-	/* workspace */
-	dMatrixT fStressMat;   // space for a stress tensor
-	dMatrixT fStressStiff; // compact stress stiffness contribution
-	dMatrixT fGradNa;      // shape function gradients matrix
+	/** \name workspace */
+	/*@{*/
+	dMatrixT fStressMat;   /**< space for a stress tensor */
+	dMatrixT fStressStiff; /**< compact stress stiffness contribution */
+	dMatrixT fGradNa;      /**< shape function gradients matrix */
 	
 	dArrayT   fTemp2;
 	dMatrixT  fTempMat1, fTempMat2;
 	dArray2DT fDNa_x;
+	/*@}*/
 };
 
 } // namespace Tahoe 
