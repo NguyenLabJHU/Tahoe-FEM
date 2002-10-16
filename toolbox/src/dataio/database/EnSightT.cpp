1f172efee67127d710a915b7492bc1d4de719bbd
sawimme <sawimme@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-10-16 17:41:02 +0000

    WriteConnectivity: change *pc + j to be *(pc + j)

--- toolbox/src/dataio/database/EnSightT.cpp
+++ toolbox/src/dataio/database/EnSightT.cpp
@@ -1,4 +1,4 @@
-/* $Id: EnSightT.cpp,v 1.10 2002-07-23 11:42:54 sawimme Exp $ */
+/* $Id: EnSightT.cpp,v 1.11 2002-10-16 17:41:02 sawimme Exp $ */
 /* created: sawimme (05/13/1999) */
 
 #include "EnSightT.h"
@@ -148,7 +148,7 @@ void EnSightT::WriteConnectivity (ostream& fgeo, int numelemnodes, const iArray2
       for (int ic = 0; ic < connects.MajorDim(); ic++)
 	{
 	  for (int j=0; j < numelemnodes; j++)
-	    fgeo << setw (iwidth) << *pc + j;
+	    fgeo << setw (iwidth) << *(pc + j);
 	  fgeo << '\n';
 
 	  pc += connects.MinorDim();
