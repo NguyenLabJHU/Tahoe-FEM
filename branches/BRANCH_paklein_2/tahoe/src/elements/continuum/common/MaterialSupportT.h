ea6739d5e37dcc43b2b542d019e16f9dd2ddff29
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-10-26 16:24:21 +0000

    sketching out materials support interface. Removing all references to ContinuumElementT.

--- branches/BRANCH_paklein_2/tahoe/src/elements/continuum/common/MaterialSupportT.h
+++ branches/BRANCH_paklein_2/tahoe/src/elements/continuum/common/MaterialSupportT.h
@@ -1,43 +1,129 @@
-/* $Id: MaterialSupportT.h,v 1.2 2002-07-02 19:56:04 cjkimme Exp $ */
-/* created: paklein (06/10/1997)                                          */
-
-#ifndef _SS_HOOKEAN_MAT_H_
-#define _SS_HOOKEAN_MAT_H_
-
-/* base classes */
-#include "SSStructMatT.h"
-#include "HookeanMatT.h"
-
+/* $Id: MaterialSupportT.h,v 1.2.8.1 2002-10-26 16:24:21 paklein Exp $ */
+#ifndef _MATERIAL_SUPPORT_T_H_
+#define _MATERIAL_SUPPORT_T_H_
 
 namespace Tahoe {
 
-class SSHookeanMatT: public SSStructMatT, public HookeanMatT
+/* forward declarations */
+class ContinuumElementT;
+class ElementCardT;
+
+/** support for the Tahoe materials classes. */
+class MaterialSupportT
 {
 public:
 
-	/* constructor */
-	SSHookeanMatT(ifstreamT& in, const SmallStrainT& element);
+	/** constructor */
+	MaterialSupportT(int nsd, int ndof, int nip);
 
-	/* initialization */
-	virtual void Initialize(void);
+	/** destructor */
+	~MaterialSupportT(void);
 
-	/* spatial description */
-	virtual const dMatrixT& c_ijkl(void); // spatial tangent moduli
-	virtual const dSymMatrixT& s_ij(void); // Cauchy stress
+	/** \name dimensions */
+	/*@{*/
+	/** number of spatial dimensions */
+	int NumSD(void) const { return fNumSD; };
+	
+	/** number of degrees of freedom (per node) */
+	int NumDOF(void) const { return fNumDOF; };
 
-	/* material description */
-	virtual const dMatrixT& C_IJKL(void); // material tangent moduli
-	virtual const dSymMatrixT& S_IJ(void); // PK2 stress
-		// overloaded to avoid additional virtual function call
+	/** stress evaluation points per element */
+	int NumIP(void) const { return fNumIP; };
+	/*@}*/
 
-	/* returns the strain energy density for the specified strain */
-	virtual double StrainEnergyDensity(void);
+	/** \name run time status */
+	/*@{*/
+	/** current stress evaluation point within the element. If
+	 * no source for the current point is set using 
+	 * MaterialSupportT::SetCurrIP, will return -1. */
+	int CurrIP(void) const;
 
-protected:
+	/** the iteration number for the current time increment. If
+	 * no source for the iteration number is set using 
+	 * MaterialSupportT::SetIterationNumber, will return -1. */
+	int IterationNumber(void) const;
 
-	/* return values */
-	dSymMatrixT fStress;
+	/** set the source for the current stress evaluation point */
+	void SetCurrIP(const int& curr_ip);
+
+	/** set the source for the iteration number */
+	void SetIterationNumber(const int& iter);
+	/*@}*/
+	
+	/** \name host code information */
+	/*@{*/
+	/** return a pointer to the host element. Returns NULL if no
+	 * no element information in available. The ContinuumElementT
+	 * pointer is set using MaterialSupportT::SetContinuumElement. */
+	const ContinuumElementT* ContinuumElement(void) const;
+
+	int NumElements(void) const;
+	int CurrElementNumber(void) const;
+	ElementCardT& ElementCard(int card) const;
+	ElementCardT& CurrentElement(void) const;
+	/*@}*/
+	
+	/** set the element group pointer */
+	void SetContinuumElement(ContinuumElementT* p);
+
+  private:
+  
+  	/** \name dimensions */
+  	/*@{*/
+	/** number of degrees of spatial dimensions */
+	int fNumSD;
+
+	/** number of degrees of freedom */
+	int fNumDOF;
+	
+	/** number of integration points */
+	int fNumIP;
+  	/*@}*/
+  	
+  	/** pointer to run time information */
+  	/*@{*/
+	const int* fCurrIP;
+	const int* fIterationNumber;
+  	/*@}*/
+  
+  	/** pointer to the continuum element */
+  	ContinuumElementT* fContinuumElement;
+
 };
 
-} // namespace Tahoe 
+/* inlines functions */
+inline const ContinuumElementT* MaterialSupportT::ContinuumElement(void) const
+{
+	return fContinuumElement;
+}
+
+inline void MaterialSupportT::SetContinuumElement(ContinuumElementT* p)
+{
+	fContinuumElement = p;
+}
+
+/* run time status */
+inline int MaterialSupportT::CurrIP(void) const
+{
+	if (fCurrIP) return *fCurrIP;
+	else return -1;
+}
+
+inline int MaterialSupportT::IterationNumber(void) const
+{
+	if (fIterationNumber) return *fIterationNumber;
+	else return -1;
+}
+
+inline void MaterialSupportT::SetCurrIP(const int& curr_ip)
+{
+	fCurrIP = &curr_ip;
+}
+
+inline void MaterialSupportT::SetIterationNumber(const int& iter)
+{
+	fIterationNumber = &iter;
+}
+
+} /* namespace Tahoe */
 #endif /* _SS_HOOKEAN_MAT_H_ */
