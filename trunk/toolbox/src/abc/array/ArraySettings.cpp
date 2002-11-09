3f1840364a16446da92d44a31e77028bf875eabf
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-11-09 01:54:40 +0000

    array behavior for arrays of RaggedArray2DT<int>* 's

--- trunk/toolbox/src/abc/array/ArraySettings.cpp
+++ trunk/toolbox/src/abc/array/ArraySettings.cpp
@@ -1,13 +1,14 @@
-/* $Id: ArraySettings.cpp,v 1.6 2002-07-05 17:15:48 paklein Exp $ */
+/* $Id: ArraySettings.cpp,v 1.7 2002-11-09 01:54:40 paklein Exp $ */
 /* created: paklein (01/23/2001) */
-
 #include "ArrayT.h"
+#include "RaggedArray2DT.h"
 
 /* NOTE: IBM's Visual Age C++ compiler xlC requires template
  *       specializations to be declared in the same namespace
  *       as the template. Other platforms don't seem to care. */
 
 namespace Tahoe {
+
 /* built-in types */
 const bool ArrayT<int>::fByteCopy = true;
 const bool ArrayT<char>::fByteCopy = true;
@@ -26,5 +27,7 @@ const bool ArrayT<double*>::fByteCopy = true;
 /* arrays of arrays */
 const bool ArrayT<ArrayT<int>*>::fByteCopy = true;
 const bool ArrayT<ArrayT<double>*>::fByteCopy = true;
+const bool ArrayT<const RaggedArray2DT<int>*>::fByteCopy = true;
+
 } /* namespace Tahoe */
 
