24c1388b0efd50a39335420f77440babb42e8ca4
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-11-28 01:07:10 +0000

    added cut-off term to Lennard-Jones potential

--- tahoe/src/elements/particle/potentials/LennardJonesPairT.cpp
+++ tahoe/src/elements/particle/potentials/LennardJonesPairT.cpp
@@ -1,21 +1,40 @@
-/* $Id: LennardJonesPairT.cpp,v 1.2 2002-11-26 01:55:37 paklein Exp $ */
+/* $Id: LennardJonesPairT.cpp,v 1.3 2002-11-28 01:07:10 paklein Exp $ */
 #include "LennardJonesPairT.h"
+#include "toolboxConstants.h"
 
 using namespace Tahoe;
 
 /* initialize static parameters */
 double LennardJonesPairT::s_eps = 1.0;
 double LennardJonesPairT::s_sigma = 1.0;
-double LennardJonesPairT::s_cut = 1.0;
+double LennardJonesPairT::s_alpha =-1.0;
+double LennardJonesPairT::s_phi_rc = 0.0;
+double LennardJonesPairT::s_dphi_rc = 0.0;
 
 /* constructor */
-LennardJonesPairT::LennardJonesPairT(double mass, double eps, double sigma, double cut_off):
+LennardJonesPairT::LennardJonesPairT(double mass, double eps, double sigma, double alpha):
 	f_eps(eps),
 	f_sigma(sigma),
-	f_cut(cut_off)
+	f_alpha(alpha),
+	f_dphi_rc(0.0),
+	f_phi_rc(0.0)
 {
-	SetRange(f_cut);
+	SetRange(f_sigma*f_alpha);
 	SetMass(mass);
+	
+	/* evaluate unmodified force at the cut-off */
+	if (f_alpha > kSmall) {
+		s_eps = f_eps;
+		s_sigma = f_sigma;
+		s_alpha = -1.0;
+		s_phi_rc = 0.0;
+		s_dphi_rc = 0.0;
+		
+		f_phi_rc = Energy(f_sigma*f_alpha, NULL, NULL);
+		f_dphi_rc = Force(f_sigma*f_alpha, NULL, NULL);
+	}
+	else
+		f_alpha = 0.0;
 }
 
 /* return a pointer to the energy function */
@@ -24,7 +43,9 @@ PairPropertyT::EnergyFunction LennardJonesPairT::getEnergyFunction(void)
 	/* copy my data to static */
 	s_eps = f_eps;
 	s_sigma = f_sigma;
-	s_cut = f_cut;
+	s_alpha = f_alpha;
+	s_phi_rc = f_phi_rc;
+	s_dphi_rc = f_dphi_rc;
 
 	/* return function pointer */
 	return LennardJonesPairT::Energy;
@@ -35,7 +56,9 @@ PairPropertyT::ForceFunction LennardJonesPairT::getForceFunction(void)
 	/* copy my data to static */
 	s_eps = f_eps;
 	s_sigma = f_sigma;
-	s_cut = f_cut;
+	s_alpha = f_alpha;
+	s_phi_rc = f_phi_rc;
+	s_dphi_rc = f_dphi_rc;
 
 	/* return function pointer */
 	return LennardJonesPairT::Force;
@@ -46,7 +69,9 @@ PairPropertyT::StiffnessFunction LennardJonesPairT::getStiffnessFunction(void)
 	/* copy my data to static */
 	s_eps = f_eps;
 	s_sigma = f_sigma;
-	s_cut = f_cut;
+	s_alpha = f_alpha;
+	s_phi_rc = f_phi_rc;
+	s_dphi_rc = f_dphi_rc;
 
 	/* return function pointer */
 	return LennardJonesPairT::Stiffness;
@@ -61,12 +86,18 @@ double LennardJonesPairT::Energy(double r_ab, double* data_a, double* data_b)
 #pragma unused(data_a)
 #pragma unused(data_b)
 
-	double r = s_sigma/r_ab;
+	double r_c = s_sigma*s_alpha;
+	if (s_alpha > kSmall && r_ab > r_c)
+		return 0.0;
+	else
+	{
+		double r = s_sigma/r_ab;
 	
-	double r_6 = r*r*r*r*r*r;
-	double r_12 = r_6*r_6;
+		double r_6 = r*r*r*r*r*r;
+		double r_12 = r_6*r_6;
 	
-	return s_eps*(0.5*r_12 - r_6);
+		return 4.0*s_eps*(r_12 - r_6) - s_phi_rc - (r_ab - r_c)*s_dphi_rc;
+	}
 }
 
 double LennardJonesPairT::Force(double r_ab, double* data_a, double* data_b)
@@ -74,13 +105,19 @@ double LennardJonesPairT::Force(double r_ab, double* data_a, double* data_b)
 #pragma unused(data_a)
 #pragma unused(data_b)
 
-	double r = s_sigma/r_ab;
+	double r_c = s_sigma*s_alpha;
+	if (s_alpha > kSmall && r_ab > r_c)
+		return 0.0;
+	else
+	{
+		double r = s_sigma/r_ab;
 	
-	double r_6 = r*r*r*r*r*r;
-	double r_7 = r_6*r;
-	double r_13 = r_6*r_7;
+		double r_6 = r*r*r*r*r*r;
+		double r_7 = r_6*r;
+		double r_13 = r_6*r_7;
 	
-	return 6.0*s_eps*(-r_13 + r_7);
+		return 4.0*s_eps*(-12.0*r_13 + 6.0*r_7) - s_dphi_rc;
+	}
 }
 
 double LennardJonesPairT::Stiffness(double r_ab, double* data_a, double* data_b)
@@ -88,11 +125,17 @@ double LennardJonesPairT::Stiffness(double r_ab, double* data_a, double* data_b)
 #pragma unused(data_a)
 #pragma unused(data_b)
 
-	double r = s_sigma/r_ab;
+	double r_c = s_sigma*s_alpha;
+	if (s_alpha > kSmall && r_ab > r_c)
+		return 0.0;
+	else
+	{
+		double r = s_sigma/r_ab;
 
-	double r_6 = r*r*r*r*r*r;
-	double r_8 = r_6*r*r;
-	double r_14 = r_6*r_8;
+		double r_6 = r*r*r*r*r*r;
+		double r_8 = r_6*r*r;
+		double r_14 = r_6*r_8;
 	
-	return s_eps*(78.0*r_14 - 42.0*r_8);
+		return 4.0*s_eps*(156.0*r_14 - 42.0*r_8);
+	}
 }
