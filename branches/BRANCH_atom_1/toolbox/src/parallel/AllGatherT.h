bc2181c91d6432dc297c0df329aec83c2e054b51
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-12-10 17:01:17 +0000

    corrected bug which was not seeing the total gathered size. Changed array types from ArrayT to nArrayT so that it's easier to write the data for debugging using operator<<.

--- branches/BRANCH_atom_1/toolbox/src/parallel/AllGatherT.h
+++ branches/BRANCH_atom_1/toolbox/src/parallel/AllGatherT.h
@@ -1,4 +1,4 @@
-/* $Id: AllGatherT.h,v 1.1 2002-12-05 08:25:19 paklein Exp $ */
+/* $Id: AllGatherT.h,v 1.1.2.1 2002-12-10 17:01:17 paklein Exp $ */
 #ifndef _GATHER_T_H_
 #define _GATHER_T_H_
 
@@ -37,6 +37,9 @@ public:
 	 * is the same. */
 	bool Equal(void) const { return fEqual; };
 
+	/** the array of counts from every processor */
+	const iArrayT& Counts(void) const { return fCounts; };
+
 	/** \name perform the gather 
 	 * Gather does not need to be called with the same data type used
 	 * in call to AllGatherT::Initialize.
@@ -45,8 +48,8 @@ public:
 	 *        least of length AllGatherT:Total.
 	 */
 	/*@{*/
-	void AllGather(const ArrayT<double>& my_data, ArrayT<double>& gather);
-	void AllGather(const ArrayT<int>& my_data, ArrayT<int>& gather);
+	void AllGather(const nArrayT<double>& my_data, nArrayT<double>& gather);
+	void AllGather(const nArrayT<int>& my_data, nArrayT<int>& gather);
 	/*@}*/
 		
 private:
