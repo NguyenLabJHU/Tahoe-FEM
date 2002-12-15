434075c9c5e2f2c9b811a67a9963def8ba2370e4
paklein <paklein@75a0ec24-64db-4a18-816e-f4d92fc68425>
2002-12-15 01:10:24 +0000

    fStack needs to be in namesapce Tahoe for AIX

--- src/elements/multiscale/FEA/matrix/FEA_Access.cpp
+++ src/elements/multiscale/FEA/matrix/FEA_Access.cpp
@@ -1,9 +1,9 @@
-/* $Id: FEA_Access.cpp,v 1.6 2002-12-02 07:11:03 paklein Exp $ */
+/* $Id: FEA_Access.cpp,v 1.7 2002-12-15 01:10:24 paklein Exp $ */
 /** This file contains global parameters for the FEA classes */
 //DEVELOPMENT
 #include "FEA.h"
 
-#if defined (__DEC__) || defined (__SUN__) || defined(__MWERKS__)
+#if defined (__DEC__) || defined (__SUN__) || defined(__MWERKS__) || defined(__AIX__)
 
 /* declare "global" within the Tahoe namespace */
 namespace Tahoe {
