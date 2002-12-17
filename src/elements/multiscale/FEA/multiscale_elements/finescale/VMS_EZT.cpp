b38936c4616d5a1feb5083054f1b23e42c886ea9
creigh <creigh@75a0ec24-64db-4a18-816e-f4d92fc68425>
2002-12-17 00:20:44 +0000

    Files modified, terms easier, converges.

--- src/elements/multiscale/FEA/multiscale_elements/finescale/VMS_EZT.cpp
+++ src/elements/multiscale/FEA/multiscale_elements/finescale/VMS_EZT.cpp
@@ -29,17 +29,16 @@ void VMS_EZT::Construct (FEA_ShapeFunctionT &Shapes,VMF_MaterialT *EZ_Matl, VMS_
 	n_sd_x_n_sd = n_sd * n_sd;
 	n_sd_x_n_en = n_sd * n_en;
  
-	neg_1by10 = -1.0/10.0;
+	neg_1by10 = -0.1;
+	//neg_1by10 = -0.0005;  // good for grad(ub) ~ grad(u)
 
 	Data_Pro.Construct ( Shapes.dNdx );
   
   Form_A_S_Lists	(np1,n);
-  Form_T4_List		();
   Form_B_List			();
 
 	//B.Print("B Matricies");
 	//A.Print("A Matricies");
-	//T4.Print("T4 Matricies");
 
 	Integral.Construct (Shapes.j, Shapes.W); 
 }
@@ -51,16 +50,8 @@ void VMS_EZT::Construct (FEA_ShapeFunctionT &Shapes,VMF_MaterialT *EZ_Matl, VMS_
 
 void VMS_EZT::Form_LHS_Ka_Kb ( dMatrixT &Ka, dMatrixT &Kb )
 {
- /* del(grad_w)	*/ 	Ka -= Integral.of( B[kB_1hat], B[kB_IA_tau_2bar] );  	
-										Kb -= Integral.of( B[kB_1hat], B[kB_IA_tau_2bar] ); 
-
- /* del(G2) 	  */	Ka += Integral.of( B[kB_1hat], neg_1by10, B[kB_IB1_2bar] ); 						
-										Kb += Integral.of( B[kB_1hat], neg_1by10, B[kB_IB2_2bar] ); 						
-										Ka += Integral.of( B[kB_1hat], neg_1by10, B[kB_IB3_2bar] ); 						
-										Kb += Integral.of( B[kB_1hat], neg_1by10, B[kB_IB4_2bar] ); 						
-
- /* del(j)      */  Ka += Integral.of( B[kB_1hat], T4[kUI], B[kB_1hat] );
- 										Kb += Integral.of( B[kB_1hat], T4[kUI], B[kB_1hat] );
+ 	Ka = Integral.of( B[kB_1hat], 						B[kB_1hat] ); 
+	Kb = Integral.of( B[kB_1hat], neg_1by10, 	B[kB_1hat] );
 }
 
 //---------------------------------------------------------------------
@@ -82,26 +73,8 @@ void VMS_EZT::Form_RHS_F_int ( dArrayT &F_int) // Untested
 
 void VMS_EZT::Form_B_List (void)
 {
-
-		B.Construct (kNUM_B_TERMS, n_ip, n_sd_x_n_sd, n_sd_x_n_en);  // B = B(9,24)	
-
-    // Generic B 
-		
-	 	Data_Pro.grad_u     ( B[kB_1hat], FEA::kNonSymmetric 	); 
-
-    // del ( grad_w )
-	
-	 	Data_Pro.A_grad_u_T ( A[kG2], B[kB_IA_tau_2bar] ); 
-
-		// del ( G2 )
-		
-	 	Data_Pro.A_grad_u 	( A[k1_minus_grad_ua], 	B[kB_IB1_2bar] ); 
-	 	Data_Pro.A_grad_u 	( A[kgrad_ua], 					B[kB_IB2_2bar] ); 
-	 	Data_Pro.A_grad_u 	( A[kgrad_ub], 					B[kB_IB3_2bar] ); 
-	 	Data_Pro.A_grad_u 	( A[k1_minus_grad_ub], 	B[kB_IB4_2bar] ); 
-		B[kB_IB2_2bar] *= -1.0;
-		B[kB_IB3_2bar] *= -1.0;
-		
+	B.Construct (kNUM_B_TERMS, n_ip, n_sd_x_n_sd, n_sd_x_n_en);  // B = B(9,24)	
+ 	Data_Pro.grad_u ( B[kB_1hat], FEA::kNonSymmetric 	); 
 }
 						
 //##################################################################################
@@ -119,27 +92,9 @@ void VMS_EZT::Form_A_S_Lists (VMS_VariableT &npt,VMS_VariableT &n)
 	A[kgrad_ua] 	= npt.Get (	VMS::kgrad_ua		);
 	A[kgrad_ub] 	= npt.Get (	VMS::kgrad_ub		);
 
-	A[k1_minus_grad_ua].Identity(); 
-	A[k1_minus_grad_ua]  -= A[kgrad_ua];
-	A[k1_minus_grad_ub].Identity(); 
-	A[k1_minus_grad_ub]  -= A[kgrad_ub];
-
 	A[kG2]  = A[kgrad_ub];
 	A[kG2] *= neg_1by10; 
-	A[kG2] += A[kgrad_ua];
-
-}
-
-//##################################################################################
-//################ 4th Order Tensor Terms (i.e. T4[i] ) ########################
-//##################################################################################
-
-void VMS_EZT::Form_T4_List (void)  // These matricies are all 9x9 (not 3x3 like A)
-{
-	
-	T4.Construct ( kNUM_T4_TERMS, n_ip, n_sd_x_n_sd, n_sd_x_n_sd );
-
-  Data_Pro.A_o_1 	( A[kG2], T4[kUI] );
+	A[kG2] += A[kgrad_ua];  // G2 = grad(ua) - (1/10)grad(ub)  (i.e. Strong Form)
 
 }
 
