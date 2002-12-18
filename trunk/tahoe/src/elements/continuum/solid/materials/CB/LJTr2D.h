654cd87b37cc32969057337d93982e5c8ed19307
cjkimme <cjkimme@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2002-12-18 22:49:59 +0000

    Deriving LJTr2D from CBLatticeT so that it can work with more than nearest-neighbor interactions. All the energy, stress, and moduli routines have been replaced to use CBLatticeT's bookkeeping. It still passes the benchmark.

--- trunk/tahoe/src/elements/continuum/solid/materials/CB/LJTr2D.h
+++ trunk/tahoe/src/elements/continuum/solid/materials/CB/LJTr2D.h
@@ -1,10 +1,11 @@
-/* $Id: LJTr2D.h,v 1.5 2002-12-17 18:53:54 cjkimme Exp $ */
+/* $Id: LJTr2D.h,v 1.6 2002-12-18 22:49:59 cjkimme Exp $ */
 /* created: paklein (07/01/1996) */
 #ifndef _LJTR2D_H_
 #define _LJTR2D_H_
 
 /* base class */
 #include "NL_E_Mat2DT.h"
+#include "CBLatticeT.h"
 
 /* direct members */
 #include "dArray2DT.h"
@@ -12,13 +13,15 @@
 namespace Tahoe {
 
 /** plane stress hexagonal lattice with LJ potential */
-class LJTr2D: public NL_E_Mat2DT
+  class LJTr2D: public NL_E_Mat2DT, protected CBLatticeT
 {
 public:
 
 	/* constructor */
 	LJTr2D(ifstreamT& in, const FDMatSupportT& support);
 	
+	virtual void Initialize(void);
+
 	/* print parameters */
 	virtual void Print(ostream& out) const;
 	virtual void PrintName(ostream& out) const;
@@ -34,7 +37,13 @@ protected:
 	/* strain energy density */
 	virtual double ComputeEnergyDensity(const dSymMatrixT& E);
 
+	virtual void LoadBondTable(void);
+
 private:
+
+	double Ulj(double r) const;
+	
+	double dUlj(double r) const;
 
 	/* second derivative of the Lennard-Jones 6/12 potential */
 	double ddU(double l) const;
@@ -42,10 +51,7 @@ private:
 private:
 
 	/* LJ scaling constant */
-	double fScale;
-	
-	/* bond vectors (undeformed) */
-	dArray2DT fBondVectors;
+	double feps;
 				
 };
 
