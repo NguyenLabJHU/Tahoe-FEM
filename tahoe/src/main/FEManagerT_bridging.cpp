5d775f02871306a3c3a4d3174decbcb05ef3665b
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-02-10 02:22:04 +0000

    code to take a bridging scale calculation through the initialization phase

--- tahoe/src/main/FEManagerT_bridging.cpp
+++ tahoe/src/main/FEManagerT_bridging.cpp
@@ -1,65 +1,160 @@
-/* $Id: FEManagerT_bridging.cpp,v 1.1.2.1 2003-02-06 02:39:08 paklein Exp $ */
+/* $Id: FEManagerT_bridging.cpp,v 1.1.2.2 2003-02-10 02:22:04 paklein Exp $ */
 #include "FEManagerT_bridging.h"
 #include "ModelManagerT.h"
+#include "NodeManagerT.h"
+#include "BridgingScaleT.h"
+#include "KBC_PrescribedT.h"
+#include "KBC_CardT.h"
 
 /* constructor */
-FEManagerT_bridging::FEManagerT_bridging(ifstreamT& input, ofstreamT& output, CommunicatorT& comm):
-	FEManagerT(input, output, comm)
+FEManagerT_bridging::FEManagerT_bridging(ifstreamT& input, ofstreamT& output, CommunicatorT& comm,
+	ifstreamT& bridging_input):
+	FEManagerT(input, output, comm),
+	fBridgingIn(bridging_input),
+	fBridgingScale(NULL),
+	fSolutionDriver(NULL)
 {
 
 }
 
-/* initialize nodes that follow the field computed by this instance */
-void SetFollowers(const iArrayT& nodes, const StringT& field, NodeManagerT& node_manager)
+/* initialize the ghost node information */
+void FEManagerT_bridging::InitGhostNodes(void)
 {
-	// map incoming nodes into elements
-	
-	// store local nodes and shape function weights to do the interpolation
-
-}
-
-/* initialize data for the driving field */
-void SetExactSolution(const iArrayT& nodes, const StringT& field,  NodeManagerT& node_manager)
-{
-	// map incoming nodes into elements
-	
-	// mark affected nodes as prescribed
-	
-	// reset the group equations numbers
-}
-
-/*************************************************************************
- * Protected
- *************************************************************************/
-
-/* initialize members */
-void FEManagerT_bridging::ReadParameters(InitCodeT init)
-{
-	/* inherited */
-	FEManagerT::ReadParameters(init);
+	const char caller[] = "FEManagerT_bridging::InitGhostNodes";
 
 	/* collect ghost nodes */
 	ArrayT<StringT> id_list;
-	fModelManager->NodeSetList(fMainIn, id_list);
+	fModelManager->NodeSetList(fBridgingIn, id_list);
 	fModelManager->ManyNodeSets(id_list, fGhostNodes);
-}
 
-/* write parameters to main out */
-void FEManagerT_bridging::WriteParameters(void) const
-{
-	/* inherited */
-	FEManagerT::WriteParameters();
+	/* assume atomistic field is "displacement" */
+	StringT field = "displacement";
+	FieldT* the_field = fNodeManager->Field(field);
+	if (!the_field) ExceptionT::GeneralFail(caller, "could not resolve field \"%s\"", field.Pointer());
+
+	/* create controller to driver solution */
+	if (!fSolutionDriver) {
+	
+		/* construct new contoller */
+		fSolutionDriver = new KBC_PrescribedT(*fNodeManager);
+	
+		/* add to field */
+		the_field->AddKBCController(fSolutionDriver);
+	}
+
+	/* generate KBC cards - all degrees of freedom */
+	int ndof = the_field->NumDOF();
+	ArrayT<KBC_CardT>& KBC_cards = fSolutionDriver->KBC_Cards();
+	KBC_cards.Dimension(fGhostNodes.Length()*ndof);
+	int dex = 0;
+	for (int j = 0; j < ndof; j++)
+		for (int i = 0; i < fDrivenCellNodes.Length(); i++)
+			KBC_cards[dex++].SetValues(fGhostNodes[i], j, KBC_CardT::kDsp, 0, 0.0);
+	
+	/* reset the group equations numbers */
+	SetEquationSystem(the_field->Group());
 
 	/* echo ghost nodes */
-	fMainOut << "\n Ghost nodes:\n";
-	fMainOut << fGhostNodes.wrap(10) << '\n';
+	if (fPrintInput) {
+		fMainOut << "\n Ghost nodes:\n";
+		fMainOut << fGhostNodes.wrap(10) << '\n';
+	}
+
+	/* mark nodes as ghost */
+	fNonGhostNodes.Dimension(fModelManager->NumNodes() - fGhostNodes.Length());
+	iArrayT is_ghost(fModelManager->NumNodes());
+	is_ghost = 0;
+	for (int i = 0; i < fGhostNodes.Length(); i++)
+		is_ghost[fGhostNodes[i]] = 1;
+
+	/* check for uniqueness */
+	int ng = is_ghost.Count(1);
+	if (ng != fGhostNodes.Length())
+		ExceptionT::GeneralFail(caller, "list of ghost nodes contains %d duplicates",
+			fGhostNodes.Length() - ng);
+
+	/* collect non-ghost nodes */
+	dex = 0;
+	for (int i = 0; i < is_ghost.Length(); i++)
+		if (is_ghost[i] == 0)
+			fNonGhostNodes[dex++] = i;
 }
 
-/* construct node manager */
-void FEManagerT_bridging::SetNodeManager(void)
+/* initialize nodes that follow the field computed by this instance */
+void FEManagerT_bridging::InitInterpolation(const iArrayT& nodes, const StringT& field, NodeManagerT& node_manager)
 {
-	/* inherited */
-	FEManagerT::SetNodeManager();
+#pragma unused(field)
+
+	/* map nodes into cells (using reference coordinates) */
+	const dArray2DT& init_coords = node_manager.InitialCoordinates();
+	BridgingScale().MaptoCells(nodes, &init_coords, NULL, fFollowerCellData);
+}
+
+/* initialize data for the driving field */
+void FEManagerT_bridging::InitProjection(const iArrayT& nodes, const StringT& field,  NodeManagerT& node_manager)
+{
+	const char caller[] = "FEManagerT_bridging::SetExactSolution";
+
+	/* map nodes into cells (using reference coordinates) */
+	const dArray2DT& init_coords = node_manager.InitialCoordinates();
+	BridgingScale().MaptoCells(nodes, &init_coords, NULL, fDrivenCellData);
+
+	/* get the associated field */
+	FieldT* the_field = fNodeManager->Field(field);
+	if (!the_field) ExceptionT::GeneralFail(caller, "could not resolve field \"%s\"", field.Pointer());
+
+	/* create controller to driver solution */
+	if (!fSolutionDriver) {
 	
-	//declare ghost nodes with node manager
+		/* construct new contoller */
+		fSolutionDriver = new KBC_PrescribedT(*fNodeManager);
+	
+		/* add to field */
+		the_field->AddKBCController(fSolutionDriver);
+	}
+
+	/* collect nodes in non-empty cells */
+	fDrivenCellData.CollectCellNodes(fDrivenCellNodes);
+
+	/* generate KBC cards - all degrees of freedom */
+	int ndof = the_field->NumDOF();
+	ArrayT<KBC_CardT>& KBC_cards = fSolutionDriver->KBC_Cards();
+	KBC_cards.Dimension(fDrivenCellNodes.Length()*ndof);
+	int dex = 0;
+	for (int j = 0; j < ndof; j++)
+		for (int i = 0; i < fDrivenCellNodes.Length(); i++)
+			KBC_cards[dex++].SetValues(fDrivenCellNodes[i], j, KBC_CardT::kDsp, 0, 0.0);
+	
+	/* reset the group equations numbers */
+	SetEquationSystem(the_field->Group());
+}
+
+/*************************************************************************
+ * Private
+ *************************************************************************/
+
+/* the bridging scale element group */
+BridgingScaleT& FEManagerT_bridging::BridgingScale(void) const
+{
+	/* find bridging scale group */
+	if (!fBridgingScale) {
+	
+		/* search through element groups */
+		for (int i = 0; !fBridgingScale && i < fElementGroups.Length(); i++)
+		{
+			/* try cast */
+			ElementBaseT* element_base = fElementGroups[i];
+			
+			/* need non-const pointer to this */
+			FEManagerT_bridging* fe = (FEManagerT_bridging*) this;
+			fe->fBridgingScale = dynamic_cast<BridgingScaleT*>(element_base);
+		}
+		
+		/* not found */
+		if (!fBridgingScale)
+			ExceptionT::GeneralFail("FEManagerT_bridging::BridgingScale",
+				"did not find BridgingScaleT element group");
+	}
+	
+	return *fBridgingScale;
 }
--- tahoe/src/main/FEManagerT_bridging.h
+++ tahoe/src/main/FEManagerT_bridging.h
@@ -1,51 +1,96 @@
-/* $Id: FEManagerT_bridging.h,v 1.1.2.1 2003-02-06 02:39:08 paklein Exp $ */
+/* $Id: FEManagerT_bridging.h,v 1.1.2.2 2003-02-10 02:22:04 paklein Exp $ */
 #ifndef _FE_MANAGER_BRIDGING_H_
 #define _FE_MANAGER_BRIDGING_H_
 
 /* base class */
 #include "FEManagerT.h"
 
+/* direct members */
+#include "PointInCellDataT.h"
+
 namespace Tahoe {
+
+/* forward declarations */
+class BridgingScaleT;
+class KBC_PrescribedT;
 
 class FEManagerT_bridging: public FEManagerT
 {
 public:
 
 	/** constructor */
-	FEManagerT_bridging(ifstreamT& input, ofstreamT& output, CommunicatorT& comm);
+	FEManagerT_bridging(ifstreamT& input, ofstreamT& output, CommunicatorT& comm,
+		ifstreamT& bridging_input);
+
+	/** \name ghost nodes 
+	 * The ghost node database must be initialized by calling
+	 * FEManagerT_bridging::InitGhostNodes before accessing the lists.*/
+	/*@{*/
+	/** initialize the ghost node information */
+	void InitGhostNodes(void);
 
 	/** return list of ghost nodes */
 	const iArrayT& GhostNodes(void) const { return fGhostNodes; };
 
 	/** return list of ghost nodes */
 	const iArrayT& NonGhostNodes(void) const { return fNonGhostNodes; };
+	/*@}*/
 
-	/** initialize nodes that follow the field computed by this instance */
-	void SetFollowers(const iArrayT& nodes, const StringT& field,
+	/** initialize interpolation data. Initialize data structures needed to interpolate
+	 * field values to the given list of points. Requires that this FEManagerT has
+	 * a BridgingScaleT in its element list. */
+	void InitInterpolation(const iArrayT& nodes, const StringT& field,
 		NodeManagerT& node_manager);
 
-	/** initialize data for the driving field */
-	void SetExactSolution(const iArrayT& nodes, const StringT& field,
+	/** initialize projection data. Initialize data structures needed to project
+	 * field values to the given list of points. Requires that this FEManagerT has
+	 * a BridgingScaleT in its element list. */
+	void InitProjection(const iArrayT& nodes, const StringT& field,
 		NodeManagerT& node_manager);
-
-protected:
-
-	/** read parameters. Collect ghost nodes */
-	virtual void ReadParameters(InitCodeT init);
-
-	/** write parameters to main out */
-	virtual void WriteParameters(void) const;
-
-	/** construct node manager */
-	virtual void SetNodeManager(void);
 
 private:
 
+	/** map coordinates into elements. Temporarily limited to elements
+	 * within a single element block */
+	void MaptoCells(const iArrayT& nodes, const dArray2DT& coords, iArrayT& cell_num,
+		dArray2DT& cell_coords) const;
+
+	/** the bridging scale element group */
+	BridgingScaleT& BridgingScale(void) const;
+
+private:
+
+	/** input parameters for bridging parameters */
+	ifstreamT& fBridgingIn;
+
+	/** \name ghost node information */
+	/*@{*/
 	/** list of my ghost nodes */
 	iArrayT fGhostNodes;
 
 	/** list of my non-ghost nodes */
 	iArrayT fNonGhostNodes;
+	/*@}*/
+	
+	/** \name follower node information */
+	/*@{*/
+	BridgingScaleT* fBridgingScale;
+	
+	/** map data of follower points into the mesh */
+	PointInCellDataT fFollowerCellData;
+	/*@}*/
+	
+	/** \name the driven solution */
+	/*@{*/
+	/** the KBC_ControllerT applying the external solution */
+	KBC_PrescribedT* fSolutionDriver;
+	
+	/** list of nodes in elements being driven */
+	iArrayT fDrivenCellNodes;
+	
+	/** map data of driver points into the mesh */
+	PointInCellDataT fDrivenCellData;
+	/*@}*/
 };
 
 } /* namespace Tahoe */
