c96cab17937d8b998a4a05f2e205f5fbd1074917
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-03-02 18:55:11 +0000

    (1) added documentation (2) all contact elements now send num contact and max depth to base class

--- tahoe/src/elements/contact/ContactT.h
+++ tahoe/src/elements/contact/ContactT.h
@@ -1,6 +1,5 @@
-/* $Id: ContactT.h,v 1.7 2003-02-08 16:00:25 paklein Exp $ */
+/* $Id: ContactT.h,v 1.8 2003-03-02 18:55:11 paklein Exp $ */
 /* created: paklein (12/11/1997) */
-
 #ifndef _CONTACT_T_H_
 #define _CONTACT_T_H_
 
@@ -13,9 +12,9 @@
 #include "dArray2DT.h"
 #include "nVariArray2DT.h"
 
-
 namespace Tahoe {
 
+/** base class for contact elements */
 class ContactT: public ElementBaseT
 {
 public:
@@ -34,6 +33,9 @@ public:
 
 	/** initialization after constructor */
 	virtual void Initialize(void);
+
+	/** initialize current time increment. Reset the contact tracking data. */
+	virtual void InitStep(void);
 
 	/** solution calls */
 	virtual void AddNodalForce(const FieldT& field, int node, dArrayT& force); //not implemented
@@ -80,7 +82,7 @@ protected:
 	virtual void SetWorkSpace(void);
 	/*@}*/
 
-	/* generate contact element data - return true if configuration has
+	/** generate contact element data. Returns true if configuration has
 	 * changed since the last call */
 	bool SetContactConfiguration(void);
 
@@ -117,40 +119,60 @@ protected:
 	void StrikersFromSideSets(ifstreamT& in, ostream& out);
 	/*@}*/
 
+	/** set the tracking data */
+	void SetTrackingData(int num_contact, double max_depth);
+
 protected:
 
-	/* control parameters */
+	/** control parameters */
 	int fNumFacetNodes;
 
-	/* tags on contact surfaces */
-	ArrayT<iArray2DT> fSurfaces; // lists of facets
+	/** tags on contact surfaces */
+	ArrayT<iArray2DT> fSurfaces;
 
 	/* database info */
 	iArrayT	  fStrikerTags; // should be variable
 	dArray2DT fStrikerCoords; // should be variable
 		// only used for search grid
 
-	/* by-striker data */
+	/** \name by-striker data */
+	/*@{*/
 	// could also map the strikers onto the variable sized data
 	// for the active strikers.
-	iArrayT fActiveMap; // map to active strikers
-	AutoArrayT<int> fActiveStrikers; // global numbers of active strikers
-	AutoArrayT<int> fHitSurface;     // contact surface for each active striker
-	AutoArrayT<int> fHitFacets;      // facet of contact surface
+	iArrayT fActiveMap;              /**< map to active strikers                  */
+	AutoArrayT<int> fActiveStrikers; /**< global numbers of active strikers       */
+	AutoArrayT<int> fHitSurface;     /**< contact surface for each active striker */
+	AutoArrayT<int> fHitFacets;      /**< facet of contact surface                */
+	/*@}*/
 
-	/* link surfaces in ConnectsU - for graph */
+	/** link surfaces in ConnectsU needed for graph */
 	iArray2DT fSurfaceLinks;
 	
 private:
 
-	/* dynamic work space managers for element arrays */
+	/** \name dynamic work space managers for element arrays */
+	/*@{*/
 	nVariArray2DT<int> fConnectivities_man;		
 	nVariArray2DT<int> fEqnos_man;
+	/*@}*/
+
+	/** \name tracking data. Parameters should be set using ContactT::SetTrackingData
+	 * and are written during ContactT::WriteOutput. */
+	/*@{*/
+	int    fnum_contact;
+	double fh_max;
+	/*@}*/
 	
 //NOTE: need to generate overall list of facets (all same "geometry"?)
 //      to return as ConnectsX for domain decompositition, and what to
 //      due with strikers? For now do not return decomposition connects
 };
+
+inline void ContactT::SetTrackingData(int num_contact, double max_depth)
+{
+	fnum_contact = num_contact;
+	fh_max = max_depth;
+}
 
 } // namespace Tahoe 
 #endif /* _CONTACT_T_H_ */
