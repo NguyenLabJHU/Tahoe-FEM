9dc71820bc0809dd498190c4c6244f2c4136342e
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-03-04 07:35:45 +0000

    for non multi-field simulations, the fields were not initialized to 0.0 after being dimensioned. Meaning that any access to the fields before the call to InitialCondition accessed uninitialized values. For multi-field simulations, the fields were already being set to 0.0 after allocation.

--- tahoe/src/nodes/NodeManagerT.cpp
+++ tahoe/src/nodes/NodeManagerT.cpp
@@ -1,4 +1,4 @@
-/* $Id: NodeManagerT.cpp,v 1.21 2003-01-29 07:35:21 paklein Exp $ */
+/* $Id: NodeManagerT.cpp,v 1.22 2003-03-04 07:35:45 paklein Exp $ */
 /* created: paklein (05/23/1996) */
 #include "NodeManagerT.h"
 
@@ -1367,6 +1367,8 @@ void NodeManagerT::EchoFields(ifstreamT& in, ostream& out)
 		
 		/* dimension */
 		field->Dimension(NumNodes(), false);
+		for (int j = 0; j <= field->Order(); j++)
+			(*field)[j] = 0.0;
 		
 		/* clear all equation numbers */
 		field->Equations() = FieldT::kInit;
