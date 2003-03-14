f12c6ef470ecb472ca9cba0396b7028d7348afb0
cjkimme <cjkimme@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-03-14 00:45:49 +0000

    Initial check-in of class RandomNumberT.

---
+++ trunk/toolbox/src/abc/other/RandomNumberT.cpp
@@ -0,0 +1,121 @@
+/* $Id: RandomNumberT.cpp,v 1.1 2003-03-14 00:45:49 cjkimme Exp $ */
+/* created: paklein (08/01/1996) */
+#include "RandomNumberT.h"
+#include "ifstreamT.h"
+#include <math.h>
+#include "dArrayT.h"
+
+using namespace Tahoe;
+
+/* line length */
+const int kLineLength = 254;
+
+RandomNumberT::RandomNumberT(ifstreamT in)
+{
+  ExceptionT::GeneralFail("RandomNumberT::RandomNumberT","not implemented yet");
+}
+
+RandomNumberT::RandomNumberT(DistributionT type)
+{
+  randomType = type;
+  switch(randomType)
+    {
+    case kUniform:
+      {
+	randFunc = &RandomNumberT::UniformRandom;
+	uniformType = randomType;
+	break;
+      }
+    case kGaussian:
+      {
+	randFunc = &RandomNumberT::GaussianRandom;
+	uniformType = kUniform;
+	uniformFunc = &RandomNumberT::UniformRandom;
+	break;
+      }
+    case kParadynUniform:
+      {
+	randFunc = &RandomNumberT::ParadynUniformRandom;
+	uniformType = randomType;
+	break;
+      }
+    case kParadynGaussian:
+      {
+	randFunc = &RandomNumberT::GaussianRandom;
+	uniformType = kParadynUniform;
+	uniformFunc = &RandomNumberT::ParadynUniformRandom;
+	break;
+      }
+    }
+}
+
+//double RandomNumberT::Rand(void)
+//{
+//  return (this->*randFunc)();
+//}
+
+/* a more high-falutin' uniform random number generator */
+/* obviously, it's not implemented yet */
+double RandomNumberT::UniformRandom(void)
+{
+  return 0.;
+}
+
+/* Return a Gaussian random number with zero mean and unit variance */
+double RandomNumberT::GaussianRandom(void)
+{  
+  return sqrt(-2.0*log((this->*uniformFunc)()))*cos(6.283185308*(this->*uniformFunc)());
+}
+
+/* "minimal standard" random generator from Numerical Recipes. It's
+ * the one used by Paradyn, too.
+ */
+double RandomNumberT::ParadynUniformRandom(void)
+{
+
+  long k = fseed/12773;
+  fseed = fa*(fseed-k*12773)-2836*k;
+  if (fseed < 0)
+    fseed += frm;
+
+  return 1./frm*fseed;
+
+}
+
+/* set the parameters */
+void RandomNumberT::sRand(int seed, int a, int rm)
+{
+
+  if (seed == 0) 
+    ExceptionT::GeneralFail("RandomNumberT::sRand","0 provided as seed");
+  fseed = seed;
+  fa = a;
+  frm = rm;
+  switch(uniformType)
+    {
+    case kUniform:
+      {
+	break ;
+      }
+    case kParadynUniform:
+      {
+	break ;
+      }
+    }
+
+  return ;
+
+}
+
+/* fill an array with random numbers */
+dArrayT& RandomNumberT::RandomArray(dArrayT& fillArray)
+{
+
+  double *f = fillArray.Pointer();
+
+  for (int i = 0; i < fillArray.Length(); i++)
+    *f++ = Rand();
+
+  return fillArray;
+
+}
---
+++ trunk/toolbox/src/abc/other/RandomNumberT.h
@@ -0,0 +1,69 @@
+/* $Id: RandomNumberT.h,v 1.1 2003-03-14 00:45:49 cjkimme Exp $ */
+/* created: paklein (08/01/1996) */
+#ifndef _RANDOM_NUMBER_T_H_
+#define _RANDOM_NUMBER_T_H_
+
+/* Environmental */
+//#include "Environment.h"
+
+//#include "ios_fwd_decl.h"
+
+namespace Tahoe {
+
+/* forward declarations */
+class dArrayT;
+class ifstreamT;
+
+class RandomNumberT
+{
+public:
+  enum DistributionT 
+  {
+    kUniform = 0,
+    kGaussian = 1,
+    kParadynUniform = 2,
+    kParadynGaussian = 3
+  };
+
+	/** \name constructors */
+	/*@{*/
+	RandomNumberT(DistributionT type = kUniform);
+	/*@}*/
+
+	/*@{*/
+	RandomNumberT(ifstreamT in);
+	/*@}*/
+	
+ public:
+	void sRand(int seed, int a = 16807, int rm = 2147483647);
+
+	double Rand(void);
+
+	dArrayT& RandomArray(dArrayT& fillArray);
+
+        // Need some way to write seed to restart. 
+
+ private:
+
+	double UniformRandom(void);
+
+	double GaussianRandom(void);
+
+	double ParadynUniformRandom(void);
+
+	long fseed, fa, frm; 
+	DistributionT randomType, uniformType;
+
+	double (RandomNumberT::*randFunc)(void);
+	double (RandomNumberT::*uniformFunc)(void);
+
+};
+
+/* Is this fast or slow? */
+inline double RandomNumberT::Rand(void)
+{
+  return (this->*randFunc)();
+}
+
+}//namespace Tahoe
+#endif /* _RANDOM_NUMBER_T_H_ */
