b556af0cf58ed8b30af1b54c3980e217913ef1cc
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-03-15 03:26:32 +0000

    corrected DEC name mangling

--- toolbox/src/common/fortran_names.h
+++ toolbox/src/common/fortran_names.h
@@ -1,4 +1,4 @@
-/* $Id: fortran_names.h,v 1.3 2003-03-14 23:44:21 paklein Exp $ */
+/* $Id: fortran_names.h,v 1.4 2003-03-15 03:26:32 paklein Exp $ */
 #ifndef _FORTRAN_NAMES_H_
 #define _FORTRAN_NAMES_H_
 /*
@@ -31,12 +31,8 @@
 #endif
 
 /* DEC - single trailing underscore */
-#elif defined(__ALPHA__)
-#ifdef __STDC__
+#elif defined(__DEC__)
 #define FORTRAN_NAME(n_)	n_ ## _
-#else
-#define FORTRAN_NAME(n_)	n_/**/_
-#endif
 
 /* AIX - no mangling */
 #elif defined(__AIX__)
