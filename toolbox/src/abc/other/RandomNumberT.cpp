55848470b0b1e545af1b4e24670037d6facb8ffb
cjkimme <cjkimme@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-04-10 17:13:27 +0000

    Uniform and Gaussian Random numbers match paradyn's. Also implemented another uniform random number generator. Added accessor for random seed.

--- toolbox/src/abc/other/RandomNumberT.cpp
+++ toolbox/src/abc/other/RandomNumberT.cpp
@@ -1,4 +1,4 @@
-/* $Id: RandomNumberT.cpp,v 1.2 2003-03-14 16:31:06 paklein Exp $ */
+/* $Id: RandomNumberT.cpp,v 1.3 2003-04-10 17:13:27 cjkimme Exp $ */
 #include "RandomNumberT.h"
 #include "ifstreamT.h"
 #include <math.h>
@@ -8,101 +8,109 @@ using namespace Tahoe;
 
 /* line length */
 const int kLineLength = 254;
+const double TWOPI = 6.283185307179586476925286;
 
 RandomNumberT::RandomNumberT(ifstreamT& in)
 {
-  ExceptionT::GeneralFail("RandomNumberT::RandomNumberT","not implemented yet");
+#pragma unused(in)
+	ExceptionT::GeneralFail("RandomNumberT::RandomNumberT","not implemented yet");
 }
 
 RandomNumberT::RandomNumberT(DistributionT type)
 {
-  randomType = type;
-  switch(randomType)
+	randomType = type;
+	switch(randomType)
     {
-    case kUniform:
-      {
-	randFunc = &RandomNumberT::UniformRandom;
-	uniformType = randomType;
-	break;
-      }
-    case kGaussian:
-      {
-	randFunc = &RandomNumberT::GaussianRandom;
-	uniformType = kUniform;
-	uniformFunc = &RandomNumberT::UniformRandom;
-	break;
-      }
-    case kParadynUniform:
-      {
-	randFunc = &RandomNumberT::ParadynUniformRandom;
-	uniformType = randomType;
-	break;
-      }
-    case kParadynGaussian:
-      {
-	randFunc = &RandomNumberT::GaussianRandom;
-	uniformType = kParadynUniform;
-	uniformFunc = &RandomNumberT::ParadynUniformRandom;
-	break;
-      }
-    }
+    	case kUniform:
+    	{
+			randFunc = &RandomNumberT::UniformRandom;
+			uniformType = randomType;
+			break;
+		}
+		case kGaussian: //It's paradyn's till I put in another one
+		{
+			randFunc = &RandomNumberT::GaussianRandom;
+			uniformType = kUniform;
+			uniformFunc = &RandomNumberT::UniformRandom;
+			break;
+		}
+    	case kParadynUniform:
+		{
+			randFunc = &RandomNumberT::ParadynUniformRandom;
+			uniformType = randomType;
+			break;
+		}
+		case kParadynGaussian:
+		{
+			randFunc = &RandomNumberT::GaussianRandom;
+			uniformType = kParadynUniform;
+			uniformFunc = &RandomNumberT::ParadynUniformRandom;
+			break;
+		}
+	}
 }
 
+/* Implemeneted in .h file for now */
 //double RandomNumberT::Rand(void)
 //{
 //  return (this->*randFunc)();
 //}
 
-/* a more high-falutin' uniform random number generator */
-/* obviously, it's not implemented yet */
+/* a slightly more high-falutin' uniform random number generator */
 double RandomNumberT::UniformRandom(void)
 {
-  return 0.;
+	long k = fseed/12773;
+	fseed = fa*(fseed-k*12773)-2836*k;
+	if (fseed < 0)
+		fseed += frm;
+
+	return 1./frm*fseed;
 }
 
 /* Return a Gaussian random number with zero mean and unit variance */
 double RandomNumberT::GaussianRandom(void)
 {  
-  return sqrt(-2.0*log((this->*uniformFunc)()))*cos(6.283185308*(this->*uniformFunc)());
+	double a1 = (this->*uniformFunc)();
+	double a2 = (this->*uniformFunc)();
+	return sqrt(-2.0*std::log(a1))*std::cos(TWOPI*a2);
 }
 
-/* "minimal standard" random generator from Numerical Recipes. It's
- * the one used by Paradyn, too.
- */
+/* random number generator from Paradyn */
 double RandomNumberT::ParadynUniformRandom(void)
 {
+	fseed = fa*fseed % frm;	
+	dseed = std::fmod(da*dseed,drm);
+	fseed = dseed;
 
-  long k = fseed/12773;
-  fseed = fa*(fseed-k*12773)-2836*k;
-  if (fseed < 0)
-    fseed += frm;
-
-  return 1./frm*fseed;
+	return dseed/drm;
 
 }
 
 /* set the parameters */
-void RandomNumberT::sRand(int seed, int a, int rm)
+void RandomNumberT::sRand(long seed, long a, long rm)
 {
+	if (seed == 0) 
+		ExceptionT::GeneralFail("RandomNumberT::sRand","0 provided as seed");
+    fseed = seed;
+	fa = a;
+	frm = rm;
+	dseed = double(seed);
+	da = double(a);
+	drm = double(rm);
 
-  if (seed == 0) 
-    ExceptionT::GeneralFail("RandomNumberT::sRand","0 provided as seed");
-  fseed = seed;
-  fa = a;
-  frm = rm;
-  switch(uniformType)
-    {
-    case kUniform:
-      {
-	break ;
-      }
-    case kParadynUniform:
-      {
-	break ;
-      }
-    }
+	switch(uniformType)
+	{
+		case kUniform:
+		{
+			break ;
+		}
+		case kParadynUniform:
+		{
+			break ;
+		}
+	}
 
-  return ;
+	return ;
 
 }
 
@@ -110,11 +118,12 @@ void RandomNumberT::sRand(int seed, int a, int rm)
 dArrayT& RandomNumberT::RandomArray(dArrayT& fillArray)
 {
 
-  double *f = fillArray.Pointer();
+	double* f = fillArray.Pointer();
 
-  for (int i = 0; i < fillArray.Length(); i++)
-    *f++ = Rand();
+	for (int i = 0; i < fillArray.Length(); i++)
+		*f++ = Rand();
 
-  return fillArray;
+	return fillArray;
 
 }
+
