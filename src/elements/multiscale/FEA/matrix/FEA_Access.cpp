57d11865c13bc02e7e265beea02ca51219cd2ccb
paklein <paklein@75a0ec24-64db-4a18-816e-f4d92fc68425>
2003-04-11 23:05:54 +0000

    corrections for GNU on SUN

--- src/elements/multiscale/FEA/matrix/FEA_Access.cpp
+++ src/elements/multiscale/FEA/matrix/FEA_Access.cpp
@@ -1,8 +1,8 @@
-/* $Id: FEA_Access.cpp,v 1.8 2003-02-03 04:40:24 paklein Exp $ */
+/* $Id: FEA_Access.cpp,v 1.9 2003-04-11 23:05:54 paklein Exp $ */
 /** This file contains global parameters for the FEA classes */
 #include "FEA.h"
 
-#if defined (__DEC__) || defined (__SUN__) || defined(__MWERKS__) || defined(__AIX__)
+#if defined (__DEC__) || (defined (__SUN__) && !defined(__GNU__)) || defined(__MWERKS__) || defined(__AIX__)
 
 /* declare "global" within the Tahoe namespace */
 namespace Tahoe {
