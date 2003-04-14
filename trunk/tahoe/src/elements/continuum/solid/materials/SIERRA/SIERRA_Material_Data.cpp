050fa0bdf88970991a2a7443107dc28421489919
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-04-14 23:00:10 +0000

    added dimensioning for input variable "rot_strain_increment"

--- trunk/tahoe/src/elements/continuum/solid/materials/SIERRA/SIERRA_Material_Data.cpp
+++ trunk/tahoe/src/elements/continuum/solid/materials/SIERRA/SIERRA_Material_Data.cpp
@@ -1,4 +1,4 @@
-/* $Id: SIERRA_Material_Data.cpp,v 1.3 2003-03-09 21:58:50 paklein Exp $ */
+/* $Id: SIERRA_Material_Data.cpp,v 1.4 2003-04-14 23:00:10 paklein Exp $ */
 #include "SIERRA_Material_Data.h"
 
 /* static data */
@@ -24,6 +24,8 @@ void SIERRA_Material_Data::AddInputVariable(const StringT& input_var)
 {
 	int size = -1;
 	if (input_var == "rot_strain_inc")
+		size = 6;
+	else if (input_var == "rot_strain_increment")
 		size = 6;
 	//others?
 	
