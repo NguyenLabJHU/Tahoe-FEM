ee025b628f9abb876324a7f926f0a6faa24fbf74
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-04-26 19:10:35 +0000

    filled in interface for handling sublists of parameters

--- toolbox/src/param_tree/ParameterInterfaceT.cpp
+++ toolbox/src/param_tree/ParameterInterfaceT.cpp
@@ -1,4 +1,4 @@
-/* $Id: ParameterInterfaceT.cpp,v 1.1 2003-04-22 18:32:16 paklein Exp $ */
+/* $Id: ParameterInterfaceT.cpp,v 1.2 2003-04-26 19:10:35 paklein Exp $ */
 #include "ParameterInterfaceT.h"
 #include "ParameterListT.h"
 
@@ -14,10 +14,24 @@ void ParameterInterfaceT::SetParameters(const ParameterListT& list)
 }
 
 /* build complete parameter list description */
-void ParameterInterfaceT::DefineParameters(ParameterListT& list) 
+void ParameterInterfaceT::DefineParameters(ParameterListT& list) const
 {
 	const char caller[] = "ParameterInterfaceT::DefineParameters";
 	if (list.Name() != Name())
 		ExceptionT::GeneralFail(caller, "list name \"%s\" must be \"%s\"",
 			list.Name().Pointer(), Name().Pointer());
+}
+
+/* return the list of sub-list names */
+void ParameterInterfaceT::SubListNames(ArrayT<StringT>& list, ArrayT<ParameterListT::OccurrenceT>& occur) const
+{
+	list.Dimension(0);
+	occur.Dimension(0);
+}
+	
+/* return a pointer to the ParameterInterfaceT */
+ParameterInterfaceT* ParameterInterfaceT::SubList(const StringT& list_name)
+{
+#pragma unused(list_name)
+	return NULL;
 }
--- toolbox/src/param_tree/ParameterInterfaceT.h
+++ toolbox/src/param_tree/ParameterInterfaceT.h
@@ -1,11 +1,13 @@
-/* $Id: ParameterInterfaceT.h,v 1.1 2003-04-22 18:32:16 paklein Exp $ */
+/* $Id: ParameterInterfaceT.h,v 1.2 2003-04-26 19:10:35 paklein Exp $ */
 #ifndef _PARAMETER_INTERFACE_T_H_
 #define _PARAMETER_INTERFACE_T_H_
+
+/* direct members */
+#include "ParameterListT.h";
 
 namespace Tahoe {
 
 /* forward declarations */
-class ParameterListT;
 class StringT;
 
 /** abstract interface for classes which define and use parameters */
@@ -19,11 +21,25 @@ public:
 	/** identifier */
 	virtual const StringT& Name(void) const = 0;
 
+	/** \name parameters */
+	/*@{*/
 	/** accept completed parameter list */
 	virtual void SetParameters(const ParameterListT& list);
 	
 	/** build complete parameter list description */
-	virtual void DefineParameters(ParameterListT& list);
+	virtual void DefineParameters(ParameterListT& list) const;
+	/*@}*/
+
+	/** \name sub-lists */
+	/*@{*/
+	/** return the list of sub-list names */
+	virtual void SubListNames(ArrayT<StringT>& list, ArrayT<ParameterListT::OccurrenceT>& occur) const;
+	
+	/** a pointer to the ParameterInterfaceT of the given sublist
+	 * or NULL if the name is invalid. The objects returned must remain
+	 * valid as long as this. */
+	virtual ParameterInterfaceT* SubList(const StringT& list_name);
+	/*@}*/
 };
 
 } /* namespace Tahoe */
