1bab4168b99bc9e26cd62379b26b4a8db712ba4d
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-05-04 22:49:50 +0000

    merging changes from BRANCH_parameter_1: added example program that demonstrates how to use the new parameter tree classes to define and validate parameters, and how to contruct an object given a valid list of parameters.

---
+++ trunk/contrib/parameter_example/src/storm_shelter.cpp
@@ -0,0 +1,39 @@
+/* $Id: storm_shelter.cpp,v 1.2 2003-05-04 22:49:50 paklein Exp $ */
+#include "storm_shelter.h"
+
+storm_shelter::storm_shelter(void):
+	basement("storm_shelter"),
+	auxiliary_power_(false),
+	first_aid_kit_(true),
+	stored_water_(0.0)
+{
+
+}
+
+void storm_shelter::DefineParameters(ParameterListT& list) const
+{
+	/* inherited */
+	basement::DefineParameters(list);
+
+	ParameterT auxiliary_power(auxiliary_power_, "auxiliary_power");
+	auxiliary_power.SetDefault(true);
+	list.AddParameter(auxiliary_power);
+
+	ParameterT first_aid_kit(first_aid_kit_, "first_aid_kit");
+	first_aid_kit.SetDefault(true);
+	list.AddParameter(first_aid_kit);
+
+	ParameterT stored_water(stored_water_, "stored_water");
+	stored_water.SetDefault(0.0);
+	list.AddParameter(stored_water);
+}
+
+void storm_shelter::TakeParameterList(const ParameterListT& list)
+{
+	/* inherited */
+	basement::TakeParameterList(list);
+
+	list.GetParameter("auxiliary_power", auxiliary_power_);
+	list.GetParameter("first_aid_kit", first_aid_kit_);
+	list.GetParameter("stored_water", stored_water_);
+}
