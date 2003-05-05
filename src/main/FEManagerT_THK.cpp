61d4ee3e60042e264b0c9a9d5327e924bd0bf7ce
paklein <paklein@75a0ec24-64db-4a18-816e-f4d92fc68425>
2003-05-05 00:14:00 +0000

    added missing namespace Tahoe declaration

--- src/main/FEManagerT_THK.cpp
+++ src/main/FEManagerT_THK.cpp
@@ -1,6 +1,8 @@
-/* $Id: FEManagerT_THK.cpp,v 1.3 2003-04-07 06:13:05 paklein Exp $ */
+/* $Id: FEManagerT_THK.cpp,v 1.4 2003-05-05 00:14:00 paklein Exp $ */
 #include "FEManagerT_THK.h"
 #ifdef BRIDGING_ELEMENT
+
+using namespace Tahoe;
 
 /* constructor */
 FEManagerT_THK::FEManagerT_THK(ifstreamT& input, ofstreamT& output, CommunicatorT& comm,
