4d523466f484788dcadd406471adef9e65590bdb
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-05-12 16:35:55 +0000

    added methods to access the complete internal forces generated by the element group (code copied from BRANCH_THK_1).

--- tahoe/src/elements/ElementBaseT.h
+++ tahoe/src/elements/ElementBaseT.h
@@ -1,4 +1,4 @@
-/* $Id: ElementBaseT.h,v 1.26 2003-02-21 22:31:28 cjkimme Exp $ */
+/* $Id: ElementBaseT.h,v 1.26.8.1 2003-05-12 16:35:55 paklein Exp $ */
 /* created: paklein (05/24/1996) */
 
 #ifndef _ELEMENTBASE_T_H_
@@ -267,6 +267,14 @@ public:
 
 	/** moved from protected to public by HSP on 7-19-02 */
 	void NodesUsed(ArrayT<int>& nodes_used) const;
+
+	/** contribution to the nodal residual forces. Return the contribution of this element
+	 * group to the residual for the given solver group. 
+	 * \note ElementBaseT::InternalForce is not implemented and throws ExceptionT::kGeneralFail. 
+	 *       Subclasses need to implemented this method if it is required. This may turn out to
+	 *       be a more general approach for collecting the total residual for the given solver
+	 *       group and could replace the current approach implemented through ElementBaseT::FormRHS. */
+	virtual const dArray2DT& InternalForce(int group);
 
 protected: /* for derived classes only */
 
