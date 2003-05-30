1ba3881bd1b88e0a3a170bb68ed346d4c0824dd9
cjkimme <cjkimme@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-05-30 23:18:58 +0000

    Updating SetOfNodesKBCT to also control nodesets defined by the nodes' being in a specified volume of space. Input parameters have changed because of this addition, and although the input parameters allow the user to specify that the nodeset is reconfigured every specified number of timesteps, this feature is not yet implemented.

--- trunk/tahoe/src/nodes/KBC_controllers/SetOfNodesKBCT.cpp
+++ trunk/tahoe/src/nodes/KBC_controllers/SetOfNodesKBCT.cpp
@@ -1,4 +1,4 @@
-/* $Id: SetOfNodesKBCT.cpp,v 1.1 2003-05-28 17:44:52 cjkimme Exp $ */
+/* $Id: SetOfNodesKBCT.cpp,v 1.2 2003-05-30 23:18:58 cjkimme Exp $ */
 #include "SetOfNodesKBCT.h"
 #include "NodeManagerT.h"
 #include "FEManagerT.h"
@@ -6,6 +6,7 @@
 #include "ifstreamT.h"
 #include "iArrayT.h"
 #include "dArrayT.h"
+#include "AutoArrayT.h"
 
 using namespace Tahoe;
 
@@ -32,56 +33,158 @@ void SetOfNodesKBCT::WriteParameters(ostream& out) const
 
 void SetOfNodesKBCT::Initialize(ifstreamT& in)
 {
-	int numBCs;
-	in >> numBCs;
+	int readRegion; // 0 for set, 1 for Region
+	in >> readRegion;
 	
-	/* Read in data first */
-	iArrayT iMin(numBCs), iMax(numBCs), dof(numBCs), fScheduleNum(numBCs);
-	dArrayT fScale(numBCs);
-	ArrayT<KBC_CardT::CodeT> code(numBCs);
-	int numCards = 0;
-	for (int i = 0; i < numBCs; i++)
-	{
-//	int SetOrRegion; 
-		in >> iMin[i] >> iMax[i]; 
-		if (iMin[i] > iMax[i])
-			ExceptionT::BadInputValue("SetOfNodesKBCT::Initialize","Bad node ID range specification for set %d\n",i);
-		iMin[i]--;
-		iMax[i]--;
-	 
-		in >> dof[i];
-		dof[i]--;
-	
-		in >> code[i];
-	
-		in >> fScheduleNum[i] >> fScale[i]; 
+	if (!readRegion)  // Initialize a range of nodes
+	{		
+		int iMin, iMax; // Range of Nodes to control
 		
-		if (code[i] != KBC_CardT::kFix)
+		in >> iMin >> iMax; 
+		if (iMin > iMax)
+			ExceptionT::BadInputValue("SetOfNodesKBCT::Initialize","Bad node ID range specification \n");
+		iMin--;
+		iMax--;
+	
+		int numBCs;
+		in >> numBCs;
+		if (numBCs <= 0)
+			ExceptionT::BadInputValue("SetOfNodesKBCT::Initialize","Expecting numBCs > 0");
+	
+		iArrayT dof(numBCs), fScheduleNum(numBCs);
+		dArrayT fScale(numBCs);
+		ArrayT<KBC_CardT::CodeT> code(numBCs);
+	 
+	 	for (int i = 0; i < numBCs; i++)
+	 	{
+			in >> dof[i];
+			dof[i]--;
+	
+			in >> code[i];
+	
+			in >> fScheduleNum[i] >> fScale[i]; 
+		
+			if (code[i] != KBC_CardT::kFix)
+			{
+				fScheduleNum[i]--;
+				fSchedule = fNodeManager.Schedule(fScheduleNum[i]);	
+				if (!fSchedule) 
+					ExceptionT::BadInputValue("SetOfNodesKBCT::Initialize","Cannot get schedule %d \n",fScheduleNum[i]);
+			}
+			
+		}
+		
+		fKBC_Cards.Dimension((iMax - iMin + 1) * numBCs);
+
+		/* generate BC cards */
+		KBC_CardT* pcard = fKBC_Cards.Pointer();
+		
+		for (int i = 0; i < numBCs; i++)
 		{
-			fScheduleNum[i]--;
-			fSchedule = fNodeManager.Schedule(fScheduleNum[i]);	
-			if (!fSchedule) 
-				ExceptionT::BadInputValue("SetOfNodesKBCT::Initialize","Cannot get schedule %d \n",fScheduleNum[i]);
+			for (int j = 0; j < iMax - iMin + 1; j++, pcard++)
+			{			
+				/* set values */
+				pcard->SetValues(iMin + j, dof[i], code[i], fScheduleNum[i], fScale[i]);
+			}
+	
+		}
+		
+	}
+	else // Initialize regions
+	{		
+		int numSD = fNodeManager.NumSD();
+		dArrayT fxmin(numSD), fxmax(numSD);
+		
+		/* get nodal coordinates */
+		const dArray2DT& coords = fNodeManager.InitialCoordinates();
+
+		in >> fxmin;
+		in >> fxmax;
+		in >> nIncs; 
+		if (nIncs < 0) 
+			ExceptionT::BadInputValue("SetOfNodesKBCT::InitRegion","Bad increment value");
+
+		for (int j = 0; j < numSD; j++)
+			if (fxmin[j] >= fxmax[j])
+				ExceptionT::BadInputValue("SetOfNodesKBCT::InitRegion","Bad bounding box coordinates");
+	
+		if (fxmin.Length() != coords.MinorDim())
+		ExceptionT::GeneralFail("ThermostattedRegionT::NodesInRegion",
+				"Dimension mismatch between coords and bounding box");
+
+		AutoArrayT<int> tmpList;		
+
+		double* xmin = fxmin.Pointer();
+		double* xmax = fxmax.Pointer(); 
+		double* x_j;
+		int ihits = 0;
+		int nnd = coords.MajorDim();
+		for (int j = 0; j < nnd; j++)
+		{
+			bool inBox = true;
+			x_j = coords(j);
+			for (int k = 0; inBox && k < numSD; k++)
+			{
+				inBox = (xmin[k] < x_j[k]) && (x_j[k] < xmax[k]);
+			}
+			if (inBox)
+			{
+				tmpList.Append(j);
+				ihits++;
+			}
 		}
 			
-		numCards += iMax[i] - iMin[i] + 1;
-	}
+		fNodes.Dimension(ihits);
+		tmpList.CopyInto(fNodes);
+		
+		// save a bit of memory just in case
+		tmpList.Free();
+		
+		int numBCs;
+		in >> numBCs;
+		if (numBCs <= 0)
+			ExceptionT::BadInputValue("SetOfNodesKBCT::Initialize","Expecting numBCs > 0");
 	
-	fKBC_Cards.Dimension(numCards);
-
-	/* generate BC cards */
-	KBC_CardT* pcard = fKBC_Cards.Pointer();
+		iArrayT dof(numBCs), fScheduleNum(numBCs);
+		dArrayT fScale(numBCs);
+		ArrayT<KBC_CardT::CodeT> code(numBCs);
+	 
+	 	for (int i = 0; i < numBCs; i++)
+	 	{
+			in >> dof[i];
+			dof[i]--;
 	
-	for (int i = 0; i < numBCs; i++)
-	{
-		for (int j = 0; j < iMax[i] - iMin[i] + 1; j++, pcard++)
-		{			
-			/* set values */
-			pcard->SetValues(iMin[i] + j, dof[i], code[i], fScheduleNum[i], fScale[i]);
+			in >> code[i];
+	
+			in >> fScheduleNum[i] >> fScale[i]; 
+		
+			if (code[i] != KBC_CardT::kFix)
+			{
+				fScheduleNum[i]--;
+				fSchedule = fNodeManager.Schedule(fScheduleNum[i]);	
+				if (!fSchedule) 
+					ExceptionT::BadInputValue("SetOfNodesKBCT::Initialize","Cannot get schedule %d \n",fScheduleNum[i]);
+			}
+			
 		}
 		
-	}
-	
+		fKBC_Cards.Dimension(fNodes.Length()*numBCs);
+
+		/* generate BC cards */
+		KBC_CardT* pcard = fKBC_Cards.Pointer();
+		
+		for (int i = 0; i < numBCs; i++)
+		{
+			// loop over number of KBCs per node set
+		
+			for (int j = 0; j < fNodes.Length(); j++, pcard++)
+			{			
+				/* set values */
+				pcard->SetValues(fNodes[j], dof[i], code[i], fScheduleNum[i], fScale[i]);
+			}
+		
+		}
+	}	
 }
 
 
