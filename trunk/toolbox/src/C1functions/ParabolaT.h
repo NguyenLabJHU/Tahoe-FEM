41b8a2461622132ebd6673b30a88135bb6ec5543
thao <thao@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-08-04 01:27:27 +0000

    generalize ParabolaT class and remove redundant ParabolatPot class.

--- trunk/toolbox/src/C1functions/ParabolaT.h
+++ trunk/toolbox/src/C1functions/ParabolaT.h
@@ -1,4 +1,4 @@
-/* $Id: ParabolaT.h,v 1.4 2003-05-21 16:04:08 thao Exp $ */
+/* $Id: ParabolaT.h,v 1.5 2003-08-04 01:27:27 thao Exp $ */
 /* created: paklein (03/25/1999)                                          */
 
 #ifndef _PARABOLA_T_H_
@@ -15,7 +15,7 @@ class ParabolaT: public C1FunctionT
 public:
 
         /* constructor */
-        ParabolaT(double k);
+        ParabolaT(double k, double B=0.0, double l0=0.0);
 
         /* I/O */
         virtual void Print(ostream& out) const;
@@ -35,13 +35,16 @@ private:
 
         /* potential parameters */
         double fk;
+	double fl0;
+	double fB;
 };
 
 /* inlines */
 
 /* returning values */
-inline double ParabolaT::Function(double x) const { return fk*x*x; }
-inline double ParabolaT::DFunction(double x) const { return fk*x; }
+inline double ParabolaT::Function(double x) const { 
+  return 0.5*fk*(x-fl0)*(x-fl0)-0.5*fk*fB; }
+inline double ParabolaT::DFunction(double x) const { return fk*(x-fl0); }
 inline double ParabolaT::DDFunction(double x) const
 {
 #pragma unused(x)
