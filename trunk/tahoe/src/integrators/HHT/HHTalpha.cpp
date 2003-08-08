1034bdada62cd76da87c47babc685757b46b1839
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-08-08 00:19:23 +0000

    corrected bounds check on alpha

--- trunk/tahoe/src/integrators/HHT/HHTalpha.cpp
+++ trunk/tahoe/src/integrators/HHT/HHTalpha.cpp
@@ -1,6 +1,5 @@
-/* $Id: HHTalpha.cpp,v 1.4 2002-10-20 22:48:08 paklein Exp $ */
+/* $Id: HHTalpha.cpp,v 1.5 2003-08-08 00:19:23 paklein Exp $ */
 /* created: paklein (10/14/1996) */
-
 #include "HHTalpha.h"
 
 #include <iostream.h>
@@ -8,10 +7,9 @@
 #include "fstreamT.h"
 #include "dArrayT.h"
 
-/* constructor */
-
 using namespace Tahoe;
 
+/* constructor */
 HHTalpha::HHTalpha(ifstreamT& in, ostream& out, bool auto2ndorder):
 	fAuto2ndOrder(auto2ndorder),
 	fgamma(0.5),
@@ -30,7 +28,7 @@ HHTalpha::HHTalpha(ifstreamT& in, ostream& out, bool auto2ndorder):
 	out << " alpha . . . . . . . . . . . . . . . . . . . . . = " << falpha << endl;
 
 	/* range checks (incomplete) */
-	if (falpha < 0.0 || falpha > 1.0) throw ExceptionT::kBadInputValue;
+	if (falpha > 0.0 || falpha < -1.0) ExceptionT::BadInputValue("HHTalpha::HHTalpha");
 }
 
 /*************************************************************************
