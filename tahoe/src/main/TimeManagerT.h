9ccc36af21a0816e1ddaeec928694a95a8b7c006
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-08-14 06:02:49 +0000

    (1) derive from ParameterInterfaceT and define some parameters (2) clean-up of unused enum

--- tahoe/src/main/TimeManagerT.h
+++ tahoe/src/main/TimeManagerT.h
@@ -1,4 +1,4 @@
-/* $Id: TimeManagerT.h,v 1.9 2003-05-20 10:37:45 paklein Exp $ */
+/* $Id: TimeManagerT.h,v 1.10 2003-08-14 06:02:49 paklein Exp $ */
 /* created: paklein (05/23/1996) */
 
 #ifndef _TIMEMANAGER_T_H_
@@ -6,6 +6,7 @@
 
 /* base class */
 #include "iConsoleObjectT.h"
+#include "ParameterInterfaceT.h"
 
 /* direct members */
 #include "StringT.h"
@@ -25,7 +26,7 @@ class CoordinatorT;
 class nLinearHHTalpha;
 class NodeManagerT;
 
-class TimeManagerT: public iConsoleObjectT
+class TimeManagerT: public iConsoleObjectT, public ParameterInterfaceT
 {
 	/* time shifters */
 	friend class LinearHHTalpha;
@@ -48,8 +49,11 @@ public:
 	/** stream extraction operator */
 	friend istream& operator>>(istream& in, TimeManagerT::CodeT& code);
 
-	/* constructor */
+	/** constructor */
 	TimeManagerT(FEManagerT& FEM);
+
+	/** initialization */
+	void Initialize(void);
 
 	/* run through the time sequences */
 	void Top(void);
@@ -100,6 +104,15 @@ public:
 
 	/** return a pointer to a integrator of the specified type */
 	IntegratorT* New_Integrator(CodeT type) const;
+
+	/** \name implementation of the ParameterInterfaceT interface */
+	/*@{*/
+	/** describe the parameters needed by the interface */
+	virtual void DefineParameters(ParameterListT& list) const;
+
+	/** accept parameter list */
+	virtual void TakeParameterList(const ParameterListT& list);
+	/*@}*/
 
 private:	
 
