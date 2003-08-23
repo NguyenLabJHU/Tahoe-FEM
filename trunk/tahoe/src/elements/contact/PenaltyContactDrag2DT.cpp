7c5b6afb9d6f64bf663531cccc69dc634de66c86
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-08-23 16:15:35 +0000

    revisions to 2D and 3D contact with drag force formulation. 2D seems OK, 3D is not converging quadratically yet.

--- trunk/tahoe/src/elements/contact/PenaltyContactDrag2DT.cpp
+++ trunk/tahoe/src/elements/contact/PenaltyContactDrag2DT.cpp
@@ -1,10 +1,6 @@
-/* $Id: PenaltyContactDrag2DT.cpp,v 1.1 2003-08-14 05:50:41 paklein Exp $ */
+/* $Id: PenaltyContactDrag2DT.cpp,v 1.2 2003-08-23 16:15:35 paklein Exp $ */
 /* created: paklein (12/11/1997) */
 #include "PenaltyContactDrag2DT.h"
-#include "ContinuumElementT.h"
-#include "ModelManagerT.h"
-#include "ParentDomainT.h"
-#include "InverseMapT.h"
 #include "fstreamT.h"
 #include "eIntegratorT.h"
 
@@ -37,9 +33,15 @@ void PenaltyContactDrag2DT::Initialize(void)
 	   >> fGapTolerance
 	   >> fSlipTolerance;
 
+	/* node blocks containing striker nodes */
+	int num_blocks = -1;
+	in >> num_blocks;
+	ArrayT<StringT> element_id(num_blocks);
+	for (int i = 0; i < element_id.Length(); i++)
+		in >> element_id[i];
+
 	/* compute associated nodal area (using all element blocks) */
-	const ArrayT<StringT>& element_id = ElementSupport().Model().ElementGroupIDs();
-	ComputeNodalArea(element_id, fNodalArea);
+	ComputeNodalArea(element_id, fNodalArea, fStrikerLocNumber);
 }
 
 /***********************************************************************
@@ -50,10 +52,12 @@ void PenaltyContactDrag2DT::Initialize(void)
 void PenaltyContactDrag2DT::PrintControlData(ostream& out) const
 {
 	/* inherited */
-	Contact2DT::PrintControlData(out);
+	PenaltyContact2DT::PrintControlData(out);
 
 	/* regularization */
-	out << " Regularization parameter. . . . . . . . . . . . = " << fK << '\n';	
+	out << " Magnitude of the drag traction. . . . . . . . . = " << fDrag << '\n';	
+	out << " Gap tolerance for drag. . . . . . . . . . . . . = " << fGapTolerance << '\n';	
+	out << " Minimum incremental slip for drag . . . . . . . = " << fSlipTolerance << '\n';	
 }
 
 void PenaltyContactDrag2DT::RHSDriver(void)
@@ -73,8 +77,9 @@ void PenaltyContactDrag2DT::RHSDriver(void)
 	double h_max = 0.0;
 
 	/* loop over active elements */
-	dArrayT tangent(NumSD()), drag(NumDOF());
+	dArrayT tangent(NumSD());
 	iArrayT eqnos;
+	dArrayT tangent_ref(NumSD()), drag(NumDOF()); 
 	int* pelem = fConnectivities[0]->Pointer();
 	int rowlength = fConnectivities[0]->MinorDim();
 	for (int i = 0; i < fConnectivities[0]->MajorDim(); i++, pelem += rowlength)
@@ -139,19 +144,25 @@ void PenaltyContactDrag2DT::RHSDriver(void)
 		bool has_drag = false;
 		if (h < fGapTolerance)
 		{
+			/* reference tangent */
+			tangent_ref.DiffOf(init_coords(pelem[1]), init_coords(pelem[0]));
+			double tangent_ref_mag = tangent_ref.Magnitude();
+
 			/* displacement from the last increment */
 			int striker_node = pelem[2];
 			drag.DiffOf(disp_last(striker_node), disp(striker_node));
 			
 			/* striker is "sliding" */
-			double mag_slip = dArrayT::Dot(drag, tangent)/magtan;
+			double mag_slip = dArrayT::Dot(drag, tangent_ref)/tangent_ref_mag;
 			if (fabs(mag_slip) > fSlipTolerance)
 			{
 				has_drag = true;
 			
 				/* drag force */
-				double f_x = -fDrag*tangent[0]/magtan;
-				double f_y = -fDrag*tangent[1]/magtan;
+				int striker_index = fStrikerLocNumber.Map(striker_node);				
+				double drag_force = fNodalArea[striker_index]*fDrag;
+				double f_x = drag_force*tangent_ref[0]/tangent_ref_mag;
+				double f_y = drag_force*tangent_ref[1]/tangent_ref_mag;
 			
 				/* assemble - equal and opposite force on facet nodes */
 				fRHS[0] += -0.5*f_x;
@@ -176,71 +187,4 @@ void PenaltyContactDrag2DT::RHSDriver(void)
 
 	/* set tracking */
 	SetTrackingData(num_contact, h_max);
-}
-
-/* compute the nodal area associated with each striker node */
-void PenaltyContactDrag2DT::ComputeNodalArea(const ArrayT<StringT>& striker_blocks, 
-	dArrayT& nodal_area)
-{
-	/* initialize nodal area */
-	nodal_area.Dimension(fStrikerTags.Length());
-	nodal_area = 0.0;
-
-	/* get surface faces */
-	GeometryT::CodeT geometry;
-	ArrayT<iArray2DT> surfaces;
-	iArrayT surface_nodes;
-	ElementSupport().Model().SurfaceFacets(striker_blocks, geometry, surfaces, surface_nodes);
-
-	/* no surfaces */
-	if (surfaces.Length() == 0) return;
-
-	/* map to local id of striker nodes */
-	InverseMapT inverse_map;
-	inverse_map.SetOutOfRange(InverseMapT::MinusOne);
-	inverse_map.SetMap(fStrikerTags);
-
-	/* shape functions over the faces */
-	int nip = 1;
-	int nfn = surfaces[0].MinorDim();
-	ParentDomainT surf_shape(geometry, nip, nfn);
-	surf_shape.Initialize();
-
-	/* coordinates over the face */
-	int nsd = NumSD();
-	LocalArrayT ref_coords(LocalArrayT::kInitCoords, nfn, nsd);
-	ElementSupport().RegisterCoordinates(ref_coords);
-	dMatrixT jacobian(nsd, nsd-1);
-
-	/* loop over surfaces */
-	const double* Na = surf_shape.Shape(0);
-	const double* w  = surf_shape.Weight();
-	iArrayT facet_nodes;
-	for (int i = 0; i < surfaces.Length(); i++)
-	{
-		const iArray2DT& surface = surfaces[i];
-
-		/* loop over faces */
-		for (int j = 0; j < surface.MajorDim(); j++)
-		{
-			/* face nodes */
-			surface.RowAlias(j, facet_nodes);
-		
-			/* gather coordinates */
-			ref_coords.SetLocal(facet_nodes);
-		
-			/* coordinate mapping */
-			surf_shape.DomainJacobian(ref_coords, 0, jacobian);
-			double detj = surf_shape.SurfaceJacobian(jacobian);	
-		
-			/* loop over face nodes */
-			for (int k = 0; k < facet_nodes.Length(); k++)
-			{
-				/* striker node index */
-				int index = inverse_map.Map(facet_nodes[k]);
-				if (index != -1)
-					nodal_area[index] += w[0]*detj*Na[k];
-			}
-		}
-	}
 }
