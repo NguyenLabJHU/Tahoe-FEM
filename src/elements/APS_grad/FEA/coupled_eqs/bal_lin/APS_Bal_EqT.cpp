b8421c54bd51fd42370f90e73b07c6287e3958b2
raregue <raregue@75a0ec24-64db-4a18-816e-f4d92fc68425>
2003-09-27 00:04:02 +0000

    tracked down dimensioning errors, but not converging because dof numbering is off

--- src/elements/APS_grad/FEA/coupled_eqs/bal_lin/APS_Bal_EqT.cpp
+++ src/elements/APS_grad/FEA/coupled_eqs/bal_lin/APS_Bal_EqT.cpp
@@ -1,4 +1,4 @@
-// $Id: APS_Bal_EqT.cpp,v 1.11 2003-09-26 23:44:43 raregue Exp $
+// $Id: APS_Bal_EqT.cpp,v 1.12 2003-09-27 00:04:02 raregue Exp $
 #include "APS_Bal_EqT.h" 
 
 using namespace Tahoe;
@@ -70,7 +70,7 @@ void APS_Bal_EqT::Form_RHS_F_int ( dArrayT &F_int, APS_VariableT &npt ) // Untes
 		//V[kgrad_u] = npt.Get ( APS::kgrad_u );
 		B_gradu[kgrad_u] = npt.Get ( APS::kgrad_u );
 		V[kV_Temp2](0)=B_gradu[kgrad_u](0,0);
-		V[kV_Temp2](1)=B_gradu[kgrad_u](1,0);
+		V[kV_Temp2](1)=B_gradu[kgrad_u](0,1);
 		V[kgammap] = npt.Get ( APS::kgammap );
 		//F_int = Integral.of( B[kB], C[kMu], V[kgrad_u] ); 
 		F_int = Integral.of( B_d[kB], C[kMu], V[kV_Temp2] ); 
@@ -87,8 +87,8 @@ void APS_Bal_EqT::Form_B_List (void)
 		B_d.Construct 	( kNUM_B_d_TERMS, n_ip, n_sd, n_en);
 		B_eps.Construct ( kNUM_B_eps_TERMS, n_ip, n_sd, n_sd_x_n_en);
 		int dum=1;
-		B_gradu.Construct ( kNUM_B_gradu_TERMS, n_ip, n_sd, dum);
-		//B_gradu.Construct ( kNUM_B_gradu_TERMS, n_ip, dum, n_sd);		
+		//B_gradu.Construct ( kNUM_B_gradu_TERMS, n_ip, n_sd, dum);
+		B_gradu.Construct ( kNUM_B_gradu_TERMS, n_ip, dum, n_sd);		
 		
 		Data_Pro.APS_B(B_d[kB]);
  		Data_Pro.APS_Ngamma(B_eps[kBgamma]);
@@ -122,9 +122,12 @@ void APS_Bal_EqT::Form_V_S_List (APS_VariableT &npt)
 		V[keps](1) = 0.0;
 		//V[kgrad_u] = npt.Get ( APS::kgrad_u );
 		B_gradu[kgrad_u] = npt.Get ( APS::kgrad_u );
+		V[kV_Temp2](0)=B_gradu[kgrad_u](0,0);
+		V[kV_Temp2](1)=B_gradu[kgrad_u](0,1);
 		//V[knueps].Dot( V[kgrad_u], S[knuepsgradu] );
-		V[knueps].Dot( B_gradu[kgrad_u], VS[kVS_Temp1] );
-		S[knuepsgradu]=VS[kVS_Temp1](0);
+		//V[knueps].Dot( B_gradu[kgrad_u], VS[kVS_Temp1] );
+		V[knueps].Dot( V[kV_Temp2], S[knuepsgradu] );
+		//S[knuepsgradu]=VS[kVS_Temp1](0);
 		V[knueps].Dot( V[keps], S[knuepseps] );
 }
 
