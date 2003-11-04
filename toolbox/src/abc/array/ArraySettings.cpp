dde26be87d505d224933f312c912d95eb156e3a1
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-11-04 01:10:27 +0000

    define macro for different syntaxes used to define static member of templated classes explicitly

--- toolbox/src/abc/array/ArraySettings.cpp
+++ toolbox/src/abc/array/ArraySettings.cpp
@@ -1,27 +1,27 @@
-/* $Id: ArraySettings.cpp,v 1.9 2003-11-03 18:51:13 paklein Exp $ */
+/* $Id: ArraySettings.cpp,v 1.10 2003-11-04 01:10:24 paklein Exp $ */
 /* created: paklein (01/23/2001) */
 #include "ArrayT.h"
 
 namespace Tahoe {
 
 /* built-in types */
-template<> const bool ArrayT<int>::fByteCopy = true;
-template<> const bool ArrayT<char>::fByteCopy = true;
-template<> const bool ArrayT<bool>::fByteCopy = true;
-template<> const bool ArrayT<float>::fByteCopy = true;
-template<> const bool ArrayT<double>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<int>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<char>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<bool>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<float>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<double>::fByteCopy = true;
 
 /* and their pointers */
-template<> const bool ArrayT<int*>::fByteCopy = true;
-template<> const bool ArrayT<char*>::fByteCopy = true;
-template<> const bool ArrayT<bool*>::fByteCopy = true;
-template<> const bool ArrayT<void*>::fByteCopy = true;
-template<> const bool ArrayT<float*>::fByteCopy = true;
-template<> const bool ArrayT<double*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<int*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<char*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<bool*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<void*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<float*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<double*>::fByteCopy = true;
 
 /* arrays of arrays */
-template<> const bool ArrayT<ArrayT<int>*>::fByteCopy = true;
-template<> const bool ArrayT<ArrayT<double>*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<ArrayT<int>*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<ArrayT<double>*>::fByteCopy = true;
 
 } /* namespace Tahoe */
 
@@ -29,7 +29,7 @@ template<> const bool ArrayT<ArrayT<double>*>::fByteCopy = true;
 
 namespace Tahoe {
 
-template<> const bool ArrayT<const RaggedArray2DT<int>*>::fByteCopy = true;
+DEFINE_TEMPLATE_STATIC const bool ArrayT<const RaggedArray2DT<int>*>::fByteCopy = true;
 
 } /* namespace Tahoe */
 
