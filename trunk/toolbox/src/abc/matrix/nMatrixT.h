7b8086d59aef9b9a08232b7328d8d51f860d4923
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2003-11-07 20:08:38 +0000

    verions of MultAB optimized for operations involving 2x2 and 3x3 matricies

--- trunk/toolbox/src/abc/matrix/nMatrixT.h
+++ trunk/toolbox/src/abc/matrix/nMatrixT.h
@@ -1,4 +1,4 @@
-/* $Id: nMatrixT.h,v 1.25 2003-10-27 17:20:46 paklein Exp $ */
+/* $Id: nMatrixT.h,v 1.26 2003-11-07 20:08:38 paklein Exp $ */
 /* created: paklein (05/24/1996) */
 #ifndef _NMATRIX_T_H_
 #define _NMATRIX_T_H_
@@ -766,9 +766,36 @@ void nMatrixT<nTYPE>::MultAB(const nMatrixT& A, const nMatrixT& B, int upper)
 	if (fRows != A.fRows ||
 	    fCols != B.fCols ||
 	  A.fCols != B.fRows) throw ExceptionT::kSizeMismatch;
-#endif		
+#endif
 
-	if (!upper)	/* entire matrix */		
+	/* 2 x 2 specialization */
+	if (fRows == 2 && fCols == 2 && A.fCols == 2) {
+		nTYPE* c = Pointer();
+		nTYPE* a = A.Pointer();
+		nTYPE* b = B.Pointer();
+
+		c[0] = a[0]*b[0] + a[2]*b[1];
+		c[1] = a[1]*b[0] + a[3]*b[1];
+		c[2] = a[0]*b[2] + a[2]*b[3]; 
+		c[3] = a[1]*b[2] + a[3]*b[3];
+	}	
+	/* 3 x 3 specialization */
+	else if (fRows == 3 && fCols == 3 && A.fCols == 3) {
+		nTYPE* c = Pointer();
+		nTYPE* a = A.Pointer();
+		nTYPE* b = B.Pointer();
+
+		c[0] = a[0]*b[0] + a[3]*b[1] + a[6]*b[2];
+		c[1] = a[1]*b[0] + a[4]*b[1] + a[7]*b[2];
+		c[2] = a[2]*b[0] + a[5]*b[1] + a[8]*b[2];
+		c[3] = a[0]*b[3] + a[3]*b[4] + a[6]*b[5];
+		c[4] = a[1]*b[3] + a[4]*b[4] + a[7]*b[5];
+		c[5] = a[2]*b[3] + a[5]*b[4] + a[8]*b[5];
+		c[6] = a[0]*b[6] + a[3]*b[7] + a[6]*b[8];
+		c[7] = a[1]*b[6] + a[4]*b[7] + a[7]*b[8]; 
+		c[8] = a[2]*b[6] + a[5]*b[7] + a[8]*b[8];
+	}
+	else if (!upper)	/* entire matrix */		
 	{		
 		int        dotcount = A.fCols;
 		nTYPE*	c       = Pointer();
