722211ad8de368089b308f39579ba96989d3c81e
rjones <rjones@75a0ec24-64db-4a18-816e-f4d92fc68425>
2003-11-21 23:23:37 +0000

    added a working version and changed the name to be consistent with the other elements in the family

---
+++ src/elements/contact/MultiplierContactElement3DT.h
@@ -0,0 +1,48 @@
+/* $Id: MultiplierContactElement3DT.h,v 1.1 2003-11-21 23:23:37 rjones Exp $ */
+// created by : rjones 2003
+#ifndef _MULTIPLIER_CONTACT_ELEMENT_3D_T_H_
+#define _MULTIPLIER_CONTACT_ELEMENT_3D_T_H_
+
+/* base classes */
+#include "ContactElementT.h"
+
+namespace Tahoe {
+
+class MultiplierContactElement3DT: public ContactElementT
+{
+  public:
+
+	/* constructor */
+	MultiplierContactElement3DT(const ElementSupportT& support, const FieldT& field);
+
+	enum EnforcementParametersT { 
+                                kConsistentTangent = 0 ,
+                                kPenalty ,
+								kGScale,
+								kPScale,
+								kTolP,
+								kMaterialType,
+                                kNumEnfParameters};
+
+	enum StatusT {	kNoP = -1,	
+					kPZero,
+					kPJump,
+					kGapZero};
+
+  protected:
+	/* print element group data */
+	virtual void PrintControlData(ostream& out) const;
+		 	
+	/* set contact status*/
+	virtual void SetContactStatus(void);
+	
+	/* construct the residual force vector, called before LHS */
+	virtual void RHSDriver(void);
+	
+	/* construct the effective mass matrix */
+	virtual void LHSDriver(GlobalT::SystemTypeT sys_type);
+
+};
+
+} // namespace Tahoe 
+#endif /* _MULTIPLIER_CONTACT_ELEMENT_3D_T_H_ */
