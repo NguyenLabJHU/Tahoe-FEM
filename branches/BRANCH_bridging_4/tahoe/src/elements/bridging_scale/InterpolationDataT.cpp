496d6cb0b3b53a7288583e93a1ee394199fa8f90
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-02-25 07:46:27 +0000

    initial check-in of data needed to interpolate data from a set of source points to a set of field points

---
+++ branches/BRANCH_bridging_4/tahoe/src/elements/bridging_scale/InterpolationDataT.cpp
@@ -0,0 +1,53 @@
+/* $Id: InterpolationDataT.cpp,v 1.1.2.1 2004-02-25 07:46:27 paklein Exp $ */
+#include "InterpolationDataT.h"
+
+using namespace Tahoe;
+
+/* transpose the given InterpolationDataT */
+void InterpolationDataT::Transpose(const InverseMapT& map, RaggedArray2DT<int>& neighbors,
+	RaggedArray2DT<double>& neighbor_weights)
+{
+	/* determine unique neighbor numbers */
+	iArrayT neighbors_all(neighbors.Length(), neighbors.Pointer());
+	iArrayT neighbors_used;
+	neighbors_used.Union(neighbors_all);
+	
+	/* map to data for each neighbor */
+	fMap.SetMap(neighbors_used);
+
+	/* counts */
+	iArrayT neigh;
+	iArrayT count(neighbors_used.Length());
+	count = 0;
+	for (int i = 0; i < neighbors.MajorDim(); i++) {
+		neighbors.RowAlias(i, neigh);
+		for (int j = 0; j < neigh.Length(); j++) {
+			int col_map = fMap.Map(neigh[j]);
+			count[col_map]++;
+		}
+	}
+
+	/* dimension arrays */
+	fNeighbors.Configure(count);
+	fNeighborWeights.Configure(count);
+
+	/* get the forward map */
+	iArrayT forward;
+	map.Forward(forward);
+
+	/* transpose the tables */
+	count = 0;
+	dArrayT weight;
+	for (int i = 0; i < neighbors.MajorDim(); i++) {
+		int row = forward[i];
+		neighbors.RowAlias(i, neigh);
+		neighbor_weights.RowAlias(i, weight);
+		for (int j = 0; j < neigh.Length(); j++) {
+			int col_map = fMap.Map(neigh[j]);
+			int& dex = count[col_map];
+			fNeighbors(col_map, dex) = row;
+			fNeighborWeights(col_map, dex) = weight[j];
+			dex++;
+		}
+	}	
+}
