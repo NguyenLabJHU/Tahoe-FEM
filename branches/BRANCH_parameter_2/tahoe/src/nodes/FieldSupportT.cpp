fbb265f3505f20f01f31b6819ec5bdeb886fcc39
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-03-31 16:14:38 +0000

    derive element end field support from a common base class

--- branches/BRANCH_parameter_2/tahoe/src/nodes/FieldSupportT.cpp
+++ branches/BRANCH_parameter_2/tahoe/src/nodes/FieldSupportT.cpp
@@ -1,59 +1,22 @@
-/* $Id: FieldSupportT.cpp,v 1.4.16.2 2004-03-22 18:39:39 paklein Exp $ */
+/* $Id: FieldSupportT.cpp,v 1.4.16.3 2004-03-31 16:14:37 paklein Exp $ */
 #include "FieldSupportT.h"
-#include "FEManagerT.h"
 #include "NodeManagerT.h"
 
 using namespace Tahoe;
 
-/* the model */
-ModelManagerT& FieldSupportT::ModelManager(void) const
+/* constructor */
+FieldSupportT::FieldSupportT(const FEManagerT& fe)
 {
-	/* check pointer */
-	ModelManagerT* model = fFEManager.ModelManager();
-	if (!model)
-		ExceptionT::GeneralFail("FieldSupportT::ModelManager", "not defined");
-	return *model;
-}
-
-void FieldSupportT::AssembleLHS(int group, const ElementMatrixT& elMat, const nArrayT<int>& eqnos) const
-{
-	/* wrapper */
-	fFEManager.AssembleLHS(group, elMat, eqnos);
-}
-
-void FieldSupportT::AssembleRHS(int group, const dArrayT& elRes, const nArrayT<int>& eqnos) const
-{
-	/* wrapper */
-	fFEManager.AssembleRHS(group, elRes, eqnos);
-}
-
-ifstreamT& FieldSupportT::Input(void) const
-{
-	/* wrapper */
-	return fFEManager.Input();
-}
-
-ofstreamT& FieldSupportT::Output(void) const
-{
-	/* wrapper */
-	return fFEManager.Output();
+	SetFEManager(&fe);
 }
 
 /* construct new KBC controller */
-KBC_ControllerT* FieldSupportT::NewKBC_Controller(FieldT& field, int code) const
-{
-	/* node manager */
-	return fNodeManager.NewKBC_Controller(field, code);
+KBC_ControllerT* FieldSupportT::NewKBC_Controller(FieldT& field, int code) const {
+	NodeManagerT& nodes = const_cast<NodeManagerT&>(NodeManager());
+	return nodes.NewKBC_Controller(field, code);
 }
 
-FBC_ControllerT* FieldSupportT::NewFBC_Controller(FieldT& field, int code) const
-{
-	/* node manager */
-	return fNodeManager.NewFBC_Controller(field, code);
-}
-
-/* return a pointer to the specified schedule */
-const ScheduleT* FieldSupportT::Schedule(int num) const
-{
-	return fNodeManager.Schedule(num);
+FBC_ControllerT* FieldSupportT::NewFBC_Controller(FieldT& field, int code) const {
+	NodeManagerT& nodes = const_cast<NodeManagerT&>(NodeManager());
+	return nodes.NewFBC_Controller(field, code);
 }
