a48930fbe39febd16865ec5707060c55906e717d
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-04-14 22:38:33 +0000

    defined class constant for NULL message ID

--- branches/BRANCH_parameter_3/tahoe/src/main/CommManagerT.h
+++ branches/BRANCH_parameter_3/tahoe/src/main/CommManagerT.h
@@ -1,4 +1,4 @@
-/* $Id: CommManagerT.h,v 1.4 2003-11-07 21:22:25 paklein Exp $ */
+/* $Id: CommManagerT.h,v 1.4.20.1 2004-04-14 22:38:33 paklein Exp $ */
 #ifndef _COMM_MANAGER_T_H_
 #define _COMM_MANAGER_T_H_
 
@@ -26,6 +26,9 @@ class MessageT;
 class CommManagerT
 {
 public:
+
+	/** enum */
+	enum CodesT { kNULLMessageID = -99 };
 
 	/** constructor */
 	CommManagerT(CommunicatorT& comm, ModelManagerT& model_manager);
