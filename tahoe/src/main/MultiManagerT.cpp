b1a59f424dea08ee1ad01e21dfedd924bbbd57be
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-06-07 13:56:42 +0000

    added method to constrain bond densities using penalized constraints

--- tahoe/src/main/MultiManagerT.cpp
+++ tahoe/src/main/MultiManagerT.cpp
@@ -1,4 +1,4 @@
-/* $Id: MultiManagerT.cpp,v 1.9.16.8 2004-05-25 23:45:18 paklein Exp $ */
+/* $Id: MultiManagerT.cpp,v 1.9.16.9 2004-06-07 13:56:42 paklein Exp $ */
 #include "MultiManagerT.h"
 
 #ifdef BRIDGING_ELEMENT
@@ -124,6 +124,8 @@ void MultiManagerT::Initialize(InitCodeT)
 			fCoarse->CorrectOverlap_1(particle_pair->Neighbors(), fine_init_coords, fCBTikhonov, fK2);
 		else if (fCorrectOverlap == 2)
 			fCoarse->CorrectOverlap_2(particle_pair->Neighbors(), fine_init_coords, fCBTikhonov, fK2, r, nip);
+		else if (fCorrectOverlap == 22)
+			fCoarse->CorrectOverlap_22(particle_pair->Neighbors(), fine_init_coords, fCBTikhonov, fK2, r, nip);
 		else if (fCorrectOverlap == 3)
 			fCoarse->CorrectOverlap_3(particle_pair->Neighbors(), fine_init_coords, fCBTikhonov, fK2, nip);
 		else if (fCorrectOverlap == 4)
