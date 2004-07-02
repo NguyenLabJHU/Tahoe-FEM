f5e94a081e4d50ad74342659bfab17c652e42550
bsun <bsun@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-07-02 23:18:02 +0000

    Does nothing yet.

--- bravais/src/main.cpp
+++ bravais/src/main.cpp
@@ -1,5 +1,5 @@
 // DEVELOPMENT
-/* $Id: main.cpp,v 1.8 2003-09-08 20:14:51 jzimmer Exp $ */
+/* $Id: main.cpp,v 1.8.2.1 2004-07-02 23:18:02 bsun Exp $ */
 #include <iostream.h>
 #include "ExceptionCodes.h"
 #include "PeriodicTableT.h"
@@ -10,39 +10,39 @@
 #include "MeshAtomT.h"
 
 
-int main()
-{
-  MakeCrystalT MC;
-  try
-    {
-      MC.Run();
-    }
-  
-
-  catch (int ErrorCode) 
-    {
-      cout << "\n\n Exiting due to error . . . ";
-      switch (ErrorCode) 
-	{
-	case eBadInputValue:
-	  cout << " Bad Input Value\n";
-	  break;
-	case eOutOfRange:
-	  cout << " Out of Range\n";
-	  break;
-	case eSizeMismatch:
-	  cout << " Size Mismatch\n";
-	  break;
-	case eOutOfMemory:
-	  cout << " Out of Memory\n";
-	  break;
-	case eDatabaseFail:
-	  cout << " Error with database\n";
-	  break;
-	}
-      cout << "\n\n Game Over\n\n";
-    }
-
-
-  return 0;
-}	
+    int main()
+   {
+      MakeCrystalT MC;
+      try
+      {
+         MC.Run();
+      }
+      
+      
+          catch (int ErrorCode) 
+         {
+            cout << "\n\n Exiting due to error . . . ";
+            switch (ErrorCode) 
+            {
+               case eBadInputValue:
+                  cout << " Bad Input Value\n";
+                  break;
+               case eOutOfRange:
+                  cout << " Out of Range\n";
+                  break;
+               case eSizeMismatch:
+                  cout << " Size Mismatch\n";
+                  break;
+               case eOutOfMemory:
+                  cout << " Out of Memory\n";
+                  break;
+               case eDatabaseFail:
+                  cout << " Error with database\n";
+                  break;
+            }
+            cout << "\n\n Game Over\n\n";
+         }
+   
+   
+      return 0;
+   }	
