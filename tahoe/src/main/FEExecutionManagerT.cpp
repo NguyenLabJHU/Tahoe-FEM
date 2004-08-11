6641081df9be07efcd3c574d767273da8da478c0
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-08-11 01:08:56 +0000

    clean up

--- tahoe/src/main/FEExecutionManagerT.cpp
+++ tahoe/src/main/FEExecutionManagerT.cpp
@@ -1,4 +1,4 @@
-/* $Id: FEExecutionManagerT.cpp,v 1.69.2.6 2004-08-09 20:56:00 paklein Exp $ */
+/* $Id: FEExecutionManagerT.cpp,v 1.69.2.7 2004-08-11 01:08:56 paklein Exp $ */
 /* created: paklein (09/21/1997) */
 #include "FEExecutionManagerT.h"
 
@@ -502,7 +502,7 @@ void FEExecutionManagerT::RunJob_analysis(const StringT& input_file, ostream& st
 #endif
 
 	int phase; // job phase
-	int token; // for run time check sums
+	int token = 1; // for run time check sums
 	
 	FEManagerT* tahoe = NULL;
 	try
@@ -523,9 +523,11 @@ void FEExecutionManagerT::RunJob_analysis(const StringT& input_file, ostream& st
 			pp_format.CloseParameterFile(out);
 			out << endl;
 		}
-		PartitionT partition; // declaration moved out from below
-		StringT model_file;
-		IOBaseT::FileTypeT format;
+//		PartitionT partition; // declaration moved out from below
+//		StringT model_file;
+//		IOBaseT::FileTypeT format;
+		
+#if 0
 		if (valid_list.Name() == "tahoe") // if straight atomistic or continuum calculation do decompose here
 		{
 			// check to see if decomposition is needed -> this works with all but the coupled calcs
@@ -595,6 +597,8 @@ void FEExecutionManagerT::RunJob_analysis(const StringT& input_file, ostream& st
 					partition.SetScope(PartitionT::kLocal);
 				}
 
+// written in FEDecomposeT::Decompose 
+#if 0
 				/* write partial geometry files (if needed) */
 				StringT suffix;
 				suffix.Suffix(model_file);
@@ -616,13 +620,15 @@ void FEExecutionManagerT::RunJob_analysis(const StringT& input_file, ostream& st
 					cout << " " << caller << ": writing partial geometry file: partial_file: "
 						 << partial_file << ": DONE" << endl;
 				}	
+#endif
 			}
 			
 			/* synch and check status */
 			if (fComm.Sum(token) != size) ExceptionT::GeneralFail();
 				
 		} // end if (valid_list.Name() == "tahoe")
-		
+#endif
+
 		/* construction */
 		tahoe = FEManagerT::New(valid_list.Name(), input_file, out, fComm, fCommandLineOptions, FEManagerT::kRun);	// FEManagerT::kRun	??
 		
@@ -650,14 +656,21 @@ void FEExecutionManagerT::RunJob_analysis(const StringT& input_file, ostream& st
 		IOManager_mpi* IOMan = NULL; // declaration moved out from below
 		if (fComm.Size() > 1 && valid_list.Name() == "tahoe") // if straight atomistic or continuum calculation use decompose information from above
 		{
+			/* partition information */
+			const PartitionT* partition = tahoe->Partition();
+		
 			/* external IO */
 			token = 1;
-			//IOManager_mpi* IOMan = NULL;
-			if (partition.DecompType() == PartitionT::kGraph && !CommandLineOption("-split_io"))
+			if (partition && partition->DecompType() == PartitionT::kGraph && !CommandLineOption("-split_io"))
 			{
 				try {
+
+					/* geometry file information */
+					const StringT& model_file = valid_list.GetParameter("geometry_file");
+					IOBaseT::FileTypeT format = IOBaseT::int_to_FileTypeT(valid_list.GetParameter("geometry_format"));
+					
 					/* set-up local IO */
-					IOMan = new IOManager_mpi(input_file, fComm, *(tahoe->OutputManager()), tahoe->Partition(), model_file, format);
+					IOMan = new IOManager_mpi(input_file, fComm, *(tahoe->OutputManager()), *partition, model_file, format);
 					if (!IOMan) throw ExceptionT::kOutOfMemory;
 				
 					/* set external IO */
@@ -1002,268 +1015,3 @@ void FEExecutionManagerT::RunJoin_serial(const StringT& input_file, ostream& sta
 	status <<   "         Join: " << double(t1 - t0)/CLOCKS_PER_SEC << " sec.\n";
 	status <<   "    Stop time: " << ctime(&stoptime);	
 }
-#if 0
-/* testing for distributed execution */
-void FEExecutionManagerT::RunJob_parallel(const StringT& input_file, ostream& status) const
-{
-	const char caller[] = "::RunJob_parallel";
-	
-	/* get rank and size */
-	int rank = fComm.Rank();
-	int size = fComm.Size();
-
-	/* time markers */
-	clock_t t0 = 0, t1 = 0, t2 = 0, t3 = 0;
-
-	/* start day/date info */
-	time_t starttime;
-	time(&starttime);
-
-	/* output stream */
-	StringT out_file;
-	out_file.Root(input_file);
-	out_file.Append(".p", rank);
-	out_file.Append(".out");
-	ofstreamT out;
-	out.open(out_file);
-
-	int token; // for run time check sums
-	try {
-	t0 = clock();
-	
-	/* generate validated parameter list */
-	ParameterListT valid_list;
-	FEManagerT::ParseInput(input_file, valid_list, true, false, false, fCommandLineOptions);
-	if (true) /* write the validated list as formatted text */ {
-		DotLine_FormatterT pp_format;
-		pp_format.SetTabWidth(4);
-		pp_format.InitParameterFile(out);
-		pp_format.WriteParameterList(out, valid_list);
-		pp_format.CloseParameterFile(out);
-		out << endl;
-	}
-		
-	/* extract model file and file format */
-	int i_format = valid_list.GetParameter("geometry_format");
-	IOBaseT::FileTypeT format = IOBaseT::int_to_FileTypeT(i_format);
-	StringT model_file = valid_list.GetParameter("geometry_file");
-
-	/* name translation */
-	StringT path;
-	path.FilePath(input_file);
-	model_file.ToNativePathName();      
-	model_file.Prepend(path);
-
-	/* generate decomposition if needed */
-	token = 1;
-	
-	// generate decompose instance
-	FEDecomposeT decompose;
-	if (decompose.NeedDecomposition(model_file, size) || !CommandLineOption("-split_io"))
-	{
-		/* 'serial' communicator */
-		int rank = fComm.Rank();
-		CommunicatorT comm(fComm, (rank == 0) ? rank : CommunicatorT::kNoColor);
-
-		/* decompose on rank 0 */
-		if (rank == 0) RunDecomp_serial(input_file, status, comm, fComm.Size());
-	
-		/* synch */
-		fComm.Barrier();	
-	}
-
-	/* synch and check status */
-	if (fComm.Sum(token) != size) ExceptionT::GeneralFail();
-
-	/* read partition information */
-	PartitionT partition;
-	StringT part_file;
-	part_file.Root(model_file);
-	part_file.Append(".n", size);
-	part_file.Append(".part", rank);
-	ifstreamT part_in('#', part_file);
-	token = 1;
-	if (!part_in.is_open())
-	{
-		cout << "\n " << caller << ": could not open file: " << part_file << endl;
-		token = 0;
-	}
-	else
-	{
-		part_in >> partition;
-		if (partition.ID() != rank)
-		{
-			cout << "\n " << caller << ": rank and partition ID mismatch" << endl;
-			token = 0;
-		}
-		
-		/* set correct numbering scope */
-		partition.SetScope(PartitionT::kLocal);
-	}
-	
-	/* synch and check status */
-	if (fComm.Sum(token) != size) ExceptionT::GeneralFail();
-		
-	/* write partial geometry files (if needed) */
-	StringT suffix;
-	suffix.Suffix(model_file);
-	StringT partial_file;
-	partial_file.Root(model_file);
-	partial_file.Append(".n", size);
-	partial_file.Append(".p", rank);
-	partial_file.Append(suffix);
-	if (decompose.NeedModelFile(partial_file, format))
-	{
-		/* original model file */
-		ModelManagerT model_ALL(cout);
-		if (!model_ALL.Initialize(format, model_file, true))
-			ExceptionT::GeneralFail(caller, 
-				"error opening file: %s", (const char*) model_file);
-
-		cout << "\n " << caller << ": writing partial geometry file: " << partial_file << endl;
-		decompose.EchoPartialGeometry(partition, model_ALL, partial_file, format);
-		cout << " " << caller << ": writing partial geometry file: partial_file: "
-			 << partial_file << ": DONE" << endl;
-	}
-	
-	/* construct local problem (Initialize() changes the file name) */
-	t1 = clock();
-	token = 1;
-
-	/* construct solver */
-	FEManagerT FEman(input_file, out, fComm, fCommandLineOptions, FEManagerT::kRun);
-	try { 
-		FEman.TakeParameterList(valid_list); 
-	}
-	catch (ExceptionT::CodeT code) {
-		status << "\n \"" << input_file << "\" exit on exception " << code << " during the\n";
-		status << " construction phase. Check the input file for errors." << endl;
-		token = 0;
-	}
-	
-	if (fComm.Sum(token) != size)
-	{
-		/* gather tokens to rank 0 */
-		if (rank == 0)
-		{
-			iArrayT tokens(size);
-			fComm.Gather(token, tokens);
-		
-			status << "\n The following processes exit on exception during construction:\n";
-			for (int i = 0; i < tokens.Length(); i++)
-				if (tokens[i] != 1)
-					status << setw(kIntWidth) << i << '\n';
-			status.flush();
-		}
-		else fComm.Gather(token, 0);
-		
-		ExceptionT::GeneralFail(caller);
-	}
-	
-	/* external IO */
-	token = 1;
-	IOManager_mpi* IOMan = NULL;
-	if (partition.DecompType() == PartitionT::kGraph && !CommandLineOption("-split_io"))
-	{
-		try {
-			/* set-up local IO */
-			IOMan = new IOManager_mpi(input_file, fComm, *(FEman.OutputManager()), FEman.Partition(), model_file, format);
-			if (!IOMan) throw ExceptionT::kOutOfMemory;
-		
-			/* set external IO */
-			FEman.SetExternalIO(IOMan);
-		}
-	
-		catch (ExceptionT::CodeT code)
-		{
-			token = 0;
-			status << "\n \"" << input_file << "\" exit on exception " << code 
-			       << " setting the external IO" << endl;
-		}
-	}
-
-	if (fComm.Sum(token) != size)
-	{
-		/* gather tokens to rank 0 */
-		if (rank == 0)
-		{
-			iArrayT tokens(size);
-			fComm.Gather(token, tokens);			
-			status << "\n The following processes exit on exception during construction:\n";
-			for (int i = 0; i < tokens.Length(); i++)
-				if (tokens[i] != 1)
-					status << setw(kIntWidth) << i << '\n';
-			status.flush();
-		}
-		else 
-			fComm.Gather(token, 0);
-		
-		ExceptionT::GeneralFail(caller);
-	}
-
-	/* solve */
-	t2 = clock();
-	token = 1;
-	try { FEman.Solve(); }
-	catch (ExceptionT::CodeT code)
-	{
-		status << "\n \"" << input_file << "\" exit on exception " << code << " during the\n";
-		status << " solution phase. See \"" << out_file << "\" for a list";
-		status << " of the codes.\n";
-		token = 0;
-	}
-
-	/* free external IO */
-	delete IOMan;
-
-	if (fComm.Sum(token) != size)
-	{
-		/* gather tokens to rank 0 */
-		if (rank == 0)
-		{
-			iArrayT tokens(size);
-			fComm.Gather(token, tokens);
-
-			status << "\n The following processes exit on exception during solution:\n";
-			for (int i = 0; i < tokens.Length(); i++)
-				if (tokens[i] != 1)
-					status << setw(kIntWidth) << i << '\n';
-			status.flush();
-		}
-		else fComm.Gather(token, 0);
-		
-		ExceptionT::GeneralFail(caller);
-	}
-	t3 = clock(); } // end try
-
-	/* job failure */
-	catch (ExceptionT::CodeT code)
-	{
-		/* fix clock values */
-		if (t1 == 0) t1 = clock();
-		if (t2 == 0) t2 = clock();		
-		if (t3 == 0) t3 = clock();		
-	}
-
-	/* stop day/date info */
-	time_t stoptime;
-	time(&stoptime);
-
-	/* output timing */
-	status << "\n     Filename: " << input_file << '\n';
-	status <<   "   Start time: " << ctime(&starttime);
-	status <<   "Decomposition: " << double(t1 - t0)/CLOCKS_PER_SEC << " sec.\n";
-	status <<   " Construction: " << double(t2 - t1)/CLOCKS_PER_SEC << " sec.\n";
-	status <<   "     Solution: " << double(t3 - t2)/CLOCKS_PER_SEC << " sec.\n";
-	status <<   "    Stop time: " << ctime(&stoptime);
-	
-	out << "\n   Start time: " << ctime(&starttime);
-	out <<   "Decomposition: " << double(t1 - t0)/CLOCKS_PER_SEC << " sec.\n";
-	out <<   " Construction: " << double(t2 - t1)/CLOCKS_PER_SEC << " sec.\n";
-	out <<   "     Solution: " << double(t3 - t2)/CLOCKS_PER_SEC << " sec.\n";
-	out <<   "    Stop time: " << ctime(&stoptime);
-
-	status << "\n End Execution\n" << endl;
-	out    << "\n End Execution\n" << endl;
-}
-#endif
