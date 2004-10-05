3db806fdb3e9e15af993de48bf950f9f3ab2ab53
kyonten <kyonten@75a0ec24-64db-4a18-816e-f4d92fc68425>
2004-10-05 15:25:17 +0000

    variables in StressCorrection routine modified, however, the communication of this class with MFGP_AssemblyT needs to be established

--- src/elements/solid/materials/grad_plasticity_MR/GRAD_MRSSKStV.cpp
+++ src/elements/solid/materials/grad_plasticity_MR/GRAD_MRSSKStV.cpp
@@ -105,19 +105,17 @@ const dSymMatrixT& GRAD_MRSSKStV::s_ij(void)
 	int ip = CurrIP();
 	ElementCardT& element = CurrentElement();
 	/*
-	const double& dlam = CurrentDLam();           // passed from the global level
-	const double& lap_dlam = CurrentLap_DLam(); // passed from the global level
+	//interpolate the nodal field values to the current integration point /
+	const double& dlam = IP_Interpolate();
+	// compute the Laplacian of the field at the current integration point/ 
+	const double& lap_dlam = IP_ComputeLaplacian(); 
 	const dSymMatrixT& e_tot = e();          //remove thermal strain ??
 	const dSymMatrixT& e_els = ElasticStrain(e_tot, element, ip);
-	const dSymMatrixT& lap_e_tot = lap_e(); // lap_e??
+	//compute the Laplacian of strain at the current integration point/
+	const dSymMatrixT& lap_e_tot = IP_ComputeLaplacianStrain(); 
 	const dSymMatrixT& lap_e_els = LapElasticStrain(lap_etot, element, ip);
 	*/
-// Note:  This part of the code is incomplete.  Basically, the
-// laplacians of the strain and the plastic multiplier need
-// to be passed from the higher level to the consititutive 
-// routine
 	/* Updated Cauchy stress (return mapping) */
-	// raregue: define variables properly and uncomment next line
 	//fStress = fGRAD_MR->StressCorrection(e_els, lap_e_els, dlam, lap_dlam, element, ip);
 	return fStress;	
 }
