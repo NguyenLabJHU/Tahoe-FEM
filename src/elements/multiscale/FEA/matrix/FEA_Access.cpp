54e090c23730a9dcb977b0c47704ef7cf2f29759
paklein <paklein@75a0ec24-64db-4a18-816e-f4d92fc68425>
2004-11-12 21:04:34 +0000

    __GNU__ replaced with __GNUC__

--- src/elements/multiscale/FEA/matrix/FEA_Access.cpp
+++ src/elements/multiscale/FEA/matrix/FEA_Access.cpp
@@ -1,10 +1,10 @@
-/* $Id: FEA_Access.cpp,v 1.10 2003-05-05 00:58:07 paklein Exp $ */
+/* $Id: FEA_Access.cpp,v 1.11 2004-11-12 21:00:22 paklein Exp $ */
 /** This file contains global parameters for the FEA classes */
 #include "FEA.h"
 
 using namespace Tahoe;
 
-#if defined (__DEC__) || (defined (__SUN__) && !defined(__GNU__)) || defined(__MWERKS__) || defined(__AIX__)
+#if defined (__DEC__) || (defined (__SUN__) && !defined(__GNUC__)) || defined(__MWERKS__) || defined(__AIX__)
 
 /* declare "global" within the Tahoe namespace */
 namespace Tahoe {
