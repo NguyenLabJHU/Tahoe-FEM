bc486f2bf772a43782dd11cccd652d17cd97db27
rjones <rjones@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-12-06 16:30:50 +0000

    make the extract function more flexible, it now uses the suffix of the 3rd arguement to determine, a) which component of the force to output and b) whether a force vs. disp or a force vs. time curve is requested.

--- trunk/contrib/Dakota/extract_1D.cpp
+++ trunk/contrib/Dakota/extract_1D.cpp
@@ -1,4 +1,4 @@
-/* $Id: extract_1D.cpp,v 1.1 2004-11-12 21:09:57 paklein Exp $ */
+/* $Id: extract_1D.cpp,v 1.2 2004-12-06 16:30:50 rjones Exp $ */
 #include "ModelManagerT.h"
 #include "ifstreamT.h"
 #include "ofstreamT.h"
@@ -31,16 +31,26 @@ int main(int argc, char** argv)
 	InputBaseT* pin_fixed_input = IOBaseT::NewInput(pin_fixed_file_type, cout);
 	pin_fixed_input->Open(pin_fixed_file);
 
-	/* locate "F_D_Z" */
+	/* pick component */
+	char dir[3];
+        strcpy(dir,"_Z");
+	if     (strstr(argv[3],"_X"))  strcpy(dir,"_X");
+	else if(strstr(argv[3],"_Y"))  strcpy(dir,"_X"); 
+	else if(strstr(argv[3],"_Z"))  strcpy(dir,"_Z");
+	cout << " dir "  << dir << endl;
+
+	/* locate "F_D_X" */
 	ArrayT<StringT> pin_fixed_nlabels;
 	pin_fixed_input->ReadNodeLabels(pin_fixed_nlabels);
-	int F_D_Z_index = -1;
-	for (int i = 0; F_D_Z_index == -1 && i < pin_fixed_nlabels.Length(); i++)
-		if (pin_fixed_nlabels[i] == "F_D_Z")
-			F_D_Z_index = i;
+	char F_D_X_label[6];
+	strcpy(F_D_X_label,"F_D"); strcat(F_D_X_label,dir);
+	int F_D_X_index = -1;
+	for (int i = 0; F_D_X_index == -1 && i < pin_fixed_nlabels.Length(); i++)
+		if (pin_fixed_nlabels[i] ==  F_D_X_label)
+			F_D_X_index = i;
 
 	//TEMP	
-	cout << "F_D_Z_index = " << F_D_Z_index << endl;
+	cout << " force " <<  F_D_X_label << " index = " << F_D_X_index << endl;
 
 	/* initialize displacement file */
 	const char* disp_file = argv[2];
@@ -48,43 +58,56 @@ int main(int argc, char** argv)
 	InputBaseT* disp_input = IOBaseT::NewInput(disp_file_type, cout);
 	disp_input->Open(disp_file);
 
-	/* locate "D_Z" */
+	/* locate "D_X" */
 	ArrayT<StringT> disp_nlabels;
 	disp_input->ReadNodeLabels(disp_nlabels);
-	int D_Z_index = -1;
-	for (int i = 0; D_Z_index == -1 && i < disp_nlabels.Length(); i++)
-		if (disp_nlabels[i] == "D_Z")
-			D_Z_index = i;
+	char D_X_label[4];
+	strcpy(D_X_label,"D"); strcat(D_X_label,dir);
+	int D_X_index = -1;
+	for (int i = 0; D_X_index == -1 && i < disp_nlabels.Length(); i++)
+		if (disp_nlabels[i] == D_X_label)
+			D_X_index = i;
 
 	//TEMP	
-	cout << "D_Z_index = " << D_Z_index << endl;
+	cout << " displacement " << D_X_label << " index = " << D_X_index << endl;
 
 	/* workspace */
 	dArray2DT pin_fixed_values(pin_fixed_input->NumNodes(), pin_fixed_nlabels.Length());
 	dArray2DT disp_values(disp_input->NumNodes(), disp_nlabels.Length());
 
-	dArrayT f_d_y(pin_fixed_values.MajorDim());
-	dArrayT d_y(disp_values.MajorDim());
+	dArrayT f_d_x(pin_fixed_values.MajorDim());
+	dArrayT d_x(disp_values.MajorDim());
 
 	/* open output file */
 	ofstreamT out(argv[3]);
 	int num_steps = pin_fixed_input->NumTimeSteps();
+	dArrayT time_steps;
+        time_steps.Allocate(num_steps);
+	pin_fixed_input->ReadTimeSteps(time_steps);
 	
 	//TEMP
 	cout << "num_steps = " << num_steps << endl;
+
+	bool time = 0;
+	if(strstr(argv[3],".fvst")) { time = 1;  cout << "f vs t\n";}
+	else if(strstr(argv[3],".fvsd")) cout << "f vs d\n";
 	
 	for (int i = 0; i < num_steps; i++)
 	{
+	//	cout << i << ", time : " << time_steps[i] << "\n";
+		double time =  time_steps[i];
+
 		/* read values */
 		pin_fixed_input->ReadAllNodeVariables(i, pin_fixed_values);
 		disp_input->ReadAllNodeVariables(i, disp_values);
 	
 		/* extract column */
-		pin_fixed_values.ColumnCopy(F_D_Z_index, f_d_y);
-		disp_values.ColumnCopy(D_Z_index, d_y);
+		pin_fixed_values.ColumnCopy(F_D_X_index, f_d_x);
+		disp_values.ColumnCopy(D_X_index, d_x);
 
 		/* output */
-		out << d_y.Max() << ' ' << f_d_y.Sum() << '\n';
+		if (time) out << time  << ' ' << f_d_x.Sum() << '\n';
+	  	else      out << d_x.Max() << ' ' << f_d_x.Sum() << '\n';
 	}
 	out.flush();
 
