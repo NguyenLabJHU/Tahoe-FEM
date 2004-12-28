628d10babe103cbca3ead1fda7641d04e9609511
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-12-28 17:46:01 +0000

    translate subpspaces.f

--- PSPASES/PSPASES.v1.0.3/CPSPASES/pspases_f2c.h
+++ PSPASES/PSPASES.v1.0.3/CPSPASES/pspases_f2c.h
@@ -1,6 +1,7 @@
-/* $Id: pspases_f2c.h,v 1.2 2004-12-11 10:10:51 paklein Exp $ */
+/* $Id: pspases_f2c.h,v 1.3 2004-12-28 17:46:00 paklein Exp $ */
 
 /* data type sizes */
 typedef int integer;
 typedef unsigned int uinteger;
 typedef double doublereal;
+typedef int ftnlen;
