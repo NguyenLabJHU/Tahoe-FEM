90b376d0f187a3334100ab7aefdf01277e69a3e3
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2004-12-30 00:43:39 +0000

    make constant definitions static

--- trunk/PSPASES/PSPASES.v1.0.3/CPSPASES/pspases_f2c.h
+++ trunk/PSPASES/PSPASES.v1.0.3/CPSPASES/pspases_f2c.h
@@ -1,4 +1,4 @@
-/* $Id: pspases_f2c.h,v 1.4 2004-12-30 00:07:21 paklein Exp $ */
+/* $Id: pspases_f2c.h,v 1.5 2004-12-30 00:43:39 paklein Exp $ */
 
 /* data type sizes */
 typedef int integer;
@@ -8,5 +8,5 @@ typedef int ftnlen;
 
 /* boolean */
 typedef int logical;
-const int FALSE_ = 0;
-const int TRUE_  = 1;
+static const int FALSE_ = 0;
+static const int TRUE_  = 1;
