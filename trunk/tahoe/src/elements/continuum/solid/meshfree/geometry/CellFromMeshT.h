de47b58d2dce8912a4496e183d0ea580805a8835
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2005-01-27 17:52:45 +0000

    implemented nodal boundary integration using subdomains defined over the mesh

--- trunk/tahoe/src/elements/continuum/solid/meshfree/geometry/CellFromMeshT.h
+++ trunk/tahoe/src/elements/continuum/solid/meshfree/geometry/CellFromMeshT.h
@@ -1,4 +1,4 @@
-/* $Id: CellFromMeshT.h,v 1.2 2005-01-25 18:22:11 cjkimme Exp $ */
+/* $Id: CellFromMeshT.h,v 1.3 2005-01-27 17:52:45 paklein Exp $ */
 #ifndef _CELL_FROM_MESH_T_H_
 #define _CELL_FROM_MESH_T_H_
 
@@ -10,7 +10,8 @@
 
 namespace Tahoe {
 
-/** base class for particle types */
+/** class which computes the nodal boundary integrals using surfaces of predefined
+ * sub-volumes of a mesh used to define the body geometry. */
 class CellFromMeshT: public CellGeometryT
 {
 public:
@@ -19,9 +20,6 @@ public:
 	CellFromMeshT(const ElementSupportT& support, bool isAxisymmetric);
 	CellFromMeshT(void);
 
-	/** destructor */
-	~CellFromMeshT(void);
-	
 	/** echo element connectivity data. Reads parameters that define
 	 * which nodes belong to this ParticleT group. */
 	virtual void DefineElements(const ArrayT<StringT>& block_ID, const ArrayT<int>& mat_index);
@@ -34,10 +32,9 @@ public:
 									dArrayT& cellVolumes, dArray2DT& cellCentroids, RaggedArray2DT<double>& circumferential_B);
 	
 protected:
-	
-	/** connectivities used to compute B matrices. */
-	iArray2DT fElementConnectivities;
 
+	/** block ID's defining the meshless domain */
+	ArrayT<StringT> fBlockID;
 };
 
 } /* namespace Tahoe */
