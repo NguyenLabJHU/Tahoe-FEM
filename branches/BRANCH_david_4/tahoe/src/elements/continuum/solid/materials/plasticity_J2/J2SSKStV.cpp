c0c60dcb0d88c847afd1e69b9beb79cdf531b8c1
nobody <nobody@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2005-04-08 18:33:32 +0000

    This commit was manufactured by cvs2svn to create branch 'BRANCH_david_4'.

---
+++ branches/BRANCH_david_4/tahoe/src/elements/continuum/solid/materials/plasticity_J2/J2SSKStV.cpp
@@ -0,0 +1,255 @@
+/* $Id: J2SSKStV.cpp,v 1.13 2005-04-06 23:32:45 thao Exp $ */
+/* created: paklein (06/18/1997) */
+#include "J2SSKStV.h"
+#include "SSMatSupportT.h"
+#include "ElementCardT.h"
+#include "StringT.h"
+
+using namespace Tahoe;
+
+/* parameters */
+const double sqrt23 = sqrt(2.0/3.0);
+
+/* element output data */
+const int kNumOutput = 3;
+static const char* Labels[kNumOutput] = {
+	"alpha",  // equivalent plastic strain
+	   "VM",  // Von Mises stress
+	"press"}; // pressure
+
+/* constructor */
+J2SSKStV::J2SSKStV(void):
+	ParameterInterfaceT("small_strain_StVenant_J2"),
+	HookeanMatT(3),
+	fStress(3),
+	fModulus(dSymMatrixT::NumValues(3))
+{
+
+}
+
+/* update internal variables */
+void J2SSKStV::UpdateHistory(void)
+{
+	/* update if plastic */
+	ElementCardT& element = CurrentElement();
+	if (element.IsAllocated()) 
+		Update(element, NumIP());
+}
+
+/* reset internal variables to last converged solution */
+void J2SSKStV::ResetHistory(void)
+{
+	/* reset if plastic */
+	ElementCardT& element = CurrentElement();
+	if (element.IsAllocated()) 
+		Reset(element, NumIP());
+}
+
+/* modulus */
+const dMatrixT& J2SSKStV::c_ijkl(void)
+{
+	/* elastoplastic correction */
+	fModulus.SumOf(HookeanMatT::Modulus(), ModuliCorrection(CurrentElement(), Mu(), NumIP(), CurrIP()));	
+	return fModulus;
+}
+
+/* stress */
+const dSymMatrixT& J2SSKStV::s_ij(void)
+{
+	int ip = CurrIP();
+	ElementCardT& element = CurrentElement();
+	const dSymMatrixT& e_tot = e();
+	const dSymMatrixT& e_els = ElasticStrain(e_tot, element, NumIP(), ip);
+
+	/* elastic stress */
+	HookeanStress(e_els, fStress);
+
+	/* modify Cauchy stress (return mapping) */
+	int iteration = fSSMatSupport->GroupIterationNumber();
+	if (iteration > -1) /* elastic iteration */
+		fStress += StressCorrection(e_els, element, Mu(), NumIP(), ip);
+	return fStress;	
+}
+
+/* returns the strain energy density for the specified strain */
+double J2SSKStV::StrainEnergyDensity(void)
+{
+	return HookeanEnergy(ElasticStrain(e(), CurrentElement(), NumIP(), CurrIP()));		
+}
+
+/* returns the number of variables computed for nodal extrapolation
+* during for element output, ie. internal variables. Returns 0
+* by default. */
+int J2SSKStV::NumOutputVariables(void) const  { return kNumOutput; }
+void J2SSKStV::OutputLabels(ArrayT<StringT>& labels) const
+{
+	/* set size */
+	labels.Dimension(kNumOutput);
+	
+	/* copy labels */
+	for (int i = 0; i < kNumOutput; i++)
+		labels[i] = Labels[i];
+}
+
+const iArrayT& J2SSKStV::InternalDOF(void) const
+{
+  return(fInternalDOF);
+}
+
+const dArrayT& J2SSKStV::InternalStrainVars(void)
+{
+  ElementCardT& element = CurrentElement();
+  if (element.IsAllocated()) {
+    const dArrayT& Internal = Get_Internal(element, NumIP(),  CurrIP());
+    const dSymMatrixT& PlasticStrain =  Get_PlasticStrain(element, NumIP(), CurrIP());
+
+    double* p = fInternalStrainVars.Pointer();
+
+    *p++ = Internal[kalpha];
+
+    *p++ = PlasticStrain[0];
+    *p++ = PlasticStrain[1];
+    *p++ = PlasticStrain[2];
+    *p++ = PlasticStrain[3];
+    *p++ = PlasticStrain[4];
+    *p++ = PlasticStrain[5];
+
+    *p++ = PlasticStrain[0];
+    *p++ = PlasticStrain[1];
+    *p++ = PlasticStrain[2];
+    *p++ = PlasticStrain[3];
+    *p++ = PlasticStrain[4];
+    *p++ = PlasticStrain[5];
+  }
+  else fInternalStrainVars = 0.0;
+  return(fInternalStrainVars);
+}
+
+const dArrayT& J2SSKStV::InternalStressVars(void)
+{
+  ElementCardT& element = CurrentElement();
+  double* p = fInternalStressVars.Pointer();
+  if (element.IsAllocated()) {
+    const dArrayT& Internal = Get_Internal(element, NumIP(), CurrIP());
+    const dSymMatrixT& Beta =  Get_Beta(element, NumIP(), CurrIP());
+
+    *p++ = -K(Internal[kalpha]);
+
+    *p++ = -Beta[0];
+    *p++ = -Beta[1];
+    *p++ = -Beta[2];
+    *p++ = -Beta[3];
+    *p++ = -Beta[4];
+    *p++ = -Beta[5];
+  }
+  else {
+    *p++ = 0.0;
+
+    *p++ = 0.0;
+    *p++ = 0.0;
+    *p++ = 0.0;
+    *p++ = 0.0;
+    *p++ = 0.0;
+    *p++ = 0.0;
+  }
+  const dSymMatrixT& e_tot = e();
+  const dSymMatrixT& e_els = ElasticStrain(e_tot, CurrentElement(), NumIP(), CurrIP());
+  /* elastic stress */
+  HookeanStress(e_els, fStress);
+  *p++ = fStress[0];
+  *p++ = fStress[1];
+  *p++ = fStress[2];
+  *p++ = fStress[3];
+  *p++ = fStress[4];
+  *p++ = fStress[5];
+
+  return(fInternalStressVars);
+}
+
+void J2SSKStV::ComputeOutput(dArrayT& output)
+{
+	/* stress tensor (loads element data and sets fStress) */
+	s_ij();
+
+	/* pressure */
+	output[2] = fStress.Trace()/3.0;
+	
+	/* deviatoric Von Mises stress */
+	fStress.Deviatoric();
+	double J2 = fStress.Invariant2();
+	J2 = (J2 < 0.0) ? 0.0 : J2;
+	output[1] = sqrt(3.0*J2);
+
+	const ElementCardT& element = CurrentElement();
+	if (element.IsAllocated())
+	{
+		/* plastic strain */
+		output[0] = fInternal[kalpha];
+		
+		/* status flags */
+		const iArrayT& flags = element.IntegerData();
+		if (flags[CurrIP()] == kIsPlastic) // output with update
+			output[0] += sqrt23*fInternal[kdgamma];
+	}
+	else
+		output[0] = 0.0;
+}
+
+/* information about subordinate parameter lists */
+void J2SSKStV::DefineSubs(SubListT& sub_list) const
+{
+	/* inherited */
+	SSSolidMatT::DefineSubs(sub_list);
+	IsotropicT::DefineSubs(sub_list);
+	HookeanMatT::DefineSubs(sub_list);
+	J2SSC0HardeningT::DefineSubs(sub_list);
+}
+
+/* return the description of the given inline subordinate parameter list */
+void J2SSKStV::DefineInlineSub(const StringT& name, ParameterListT::ListOrderT& order, 
+	SubListT& sub_lists) const
+{
+	/* inherited */
+	SSSolidMatT::DefineInlineSub(name, order, sub_lists);
+	IsotropicT::DefineInlineSub(name, order, sub_lists);
+	HookeanMatT::DefineInlineSub(name, order, sub_lists);
+	J2SSC0HardeningT::DefineInlineSub(name, order, sub_lists);
+}
+
+/* a pointer to the ParameterInterfaceT of the given subordinate */
+ParameterInterfaceT* J2SSKStV::NewSub(const StringT& name) const
+{
+	ParameterInterfaceT* sub = NULL;
+
+	/* try each base class */
+	sub = SSSolidMatT::NewSub(name);
+	if (sub) return sub;
+
+	sub = IsotropicT::NewSub(name);
+	if (sub) return sub;
+
+	sub = HookeanMatT::NewSub(name);
+	if (sub) return sub;
+	
+	return J2SSC0HardeningT::NewSub(name);
+}
+
+/* accept parameter list */
+void J2SSKStV::TakeParameterList(const ParameterListT& list)
+{
+	/* inherited */
+	SSSolidMatT::TakeParameterList(list);
+	IsotropicT::TakeParameterList(list);
+	J2SSC0HardeningT::TakeParameterList(list);
+	HookeanMatT::TakeParameterList(list);
+}
+
+/*************************************************************************
+ * Protected
+ *************************************************************************/
+
+/* set modulus */
+void J2SSKStV::SetModulus(dMatrixT& modulus)
+{
+	IsotropicT::ComputeModuli(modulus);
+}
---
+++ branches/BRANCH_david_4/tahoe/src/primitives/globalmatrix/SPOOLES/SPOOLESMatrixT_mpi.h
@@ -0,0 +1,51 @@
+/* $Id: SPOOLESMatrixT_mpi.h,v 1.8 2005-01-07 21:23:02 paklein Exp $ */
+/* created: paklein (09/13/2000) */
+#ifndef _SPOOLES_MATRIX_T_MPI_H_
+#define _SPOOLES_MATRIX_T_MPI_H_
+
+/* base class */
+#include "SPOOLESMatrixT.h"
+
+/* library support options */
+#ifdef __SPOOLES_MPI__
+#ifdef __TAHOE_MPI__
+
+namespace Tahoe {
+
+/* forward declarations */
+class CommunicatorT;
+
+class SPOOLESMatrixT_mpi: public SPOOLESMatrixT
+{
+public:
+
+	/** constuctor */
+	SPOOLESMatrixT_mpi(ostream& out, int check_code, bool symmetric,
+		bool pivoting, CommunicatorT& comm);
+
+	/** destructor */
+	~SPOOLESMatrixT_mpi(void);
+
+	/** clear values for next assembly */
+	virtual void Clear(void);
+
+	/** assignment operator */
+	SPOOLESMatrixT_mpi& operator=(const SPOOLESMatrixT_mpi& rhs);
+
+protected:
+
+	/** precondition matrix */
+	virtual void Factorize(void);
+
+	/** determine new search direction and put the results in result */
+	virtual void BackSubstitute(dArrayT& result);
+	
+private:
+
+	/** the MP layer */
+	CommunicatorT& fComm;
+};
+} // namespace Tahoe 
+#endif /* __TAHOE_MPI__ */
+#endif /*__SPOOLES_MPI__ */
+#endif /* _SPOOLES_MATRIX_T_MPI_H_ */
