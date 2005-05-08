e59a4bcb0676d7fc22567eb2581170f8c89f4c04
nobody <nobody@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2005-05-08 15:33:39 +0000

    This commit was manufactured by cvs2svn to create branch 'BRANCH_david_spatial'.

---
+++ branches/BRANCH_david_spatial/tahoe/src/integrators/static/StaticIntegrator.cpp
@@ -0,0 +1,16 @@
+/* $Id: StaticIntegrator.cpp,v 1.3 2004-07-15 08:30:48 paklein Exp $ */
+/* created: paklein (10/14/1996) */
+#include "StaticIntegrator.h"
+#include <iostream.h>
+
+using namespace Tahoe;
+
+/* constructor */
+StaticIntegrator::StaticIntegrator(void) { }
+
+/***********************************************************************
+* Protected
+***********************************************************************/
+
+/* recalculate time stepping constants */
+void StaticIntegrator::ComputeParameters(void) { }
