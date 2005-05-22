f84de27601a86746f06dc7711e9e5ef1d264b98c
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2005-05-22 21:29:41 +0000

    need to write/read neighbor lists to/from restart files

--- branches/BRANCH_david_spatial/tahoe/src/elements/particle/ParticlePairT.cpp
+++ branches/BRANCH_david_spatial/tahoe/src/elements/particle/ParticlePairT.cpp
@@ -1,5 +1,4 @@
-/* $Id: ParticlePairT.cpp,v 1.44.4.1 2005-05-18 18:30:42 paklein Exp $ */
-
+/* $Id: ParticlePairT.cpp,v 1.44.4.2 2005-05-22 21:29:41 paklein Exp $ */
 #include "ParticlePairT.h"
 
 #include "PairPropertyT.h"
@@ -12,6 +11,8 @@
 #include "dArray2DT.h"
 #include "iGridManagerT.h"
 #include "ParameterContainerT.h"
+#include "ifstreamT.h"
+#include "ofstreamT.h"
 
 #include <iostream.h>
 #include <iomanip.h>
@@ -458,6 +459,46 @@ void ParticlePairT::WriteOutput(void)
 
 	/* send */
 	ElementSupport().WriteOutput(fOutputID, n_values, e_values);
+}
+
+/* write restart data to the output stream */
+void ParticlePairT::WriteRestart(ofstreamT& out) const
+{
+	/* inherited */
+	ParticleT::WriteRestart(out);
+
+	/* open stream */	
+	StringT file = out.filename();
+	file.Append(".neighbor");
+	ofstreamT my_out(file);
+	if (!my_out.is_open())
+		ExceptionT::GeneralFail("ParticlePairT::WriteRestart",
+			"could not open file \"%s\"", file.Pointer());
+
+	/* write neighbor lists */
+	fNeighbors.Write(my_out); my_out << '\n';
+	fNearestNeighbors.Write(my_out); my_out << '\n';
+	fRefNearestNeighbors.Write(my_out); my_out << '\n';
+}
+
+/* read restart data to the output stream */
+void ParticlePairT::ReadRestart(ifstreamT& in)
+{
+	/* inherited */
+	ParticleT::ReadRestart(in);
+
+	/* open stream */	
+	StringT file = in.filename();
+	file.Append(".neighbor");
+	ifstreamT my_in(file);
+	if (!my_in.is_open())
+		ExceptionT::GeneralFail("ParticlePairT::ReadRestart",
+			"could not open file \"%s\"", file.Pointer());
+
+	/* read neighbor lists */
+	fNeighbors.Read(my_in);
+	fNearestNeighbors.Read(my_in);
+	fRefNearestNeighbors.Read(my_in);
 }
 
 /* compute the part of the stiffness matrix */
@@ -1296,8 +1337,7 @@ void ParticlePairT::SetConfiguration(void)
 	/* reset neighbor lists */
 	CommManagerT& comm_manager = ElementSupport().CommManager();
 	const ArrayT<int>* part_nodes = comm_manager.PartitionNodes();
-	if (fActiveParticles) 
-		part_nodes = fActiveParticles;
+	if (fActiveParticles) part_nodes = fActiveParticles;
 	
 	/* neighbor lists */	
 	GenerateNeighborList(part_nodes, fNearestNeighborDistance, fNearestNeighbors, true, true);
