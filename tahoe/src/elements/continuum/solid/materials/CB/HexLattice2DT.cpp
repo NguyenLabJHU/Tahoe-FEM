4394da9066eeb0887d7733e43581deb10206661c
jzimmer <jzimmer@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2005-08-24 03:00:54 +0000

    Added capability to rotate Cauchy-Born "crystal" for Hex2D lattice: 	- Rotation function has not yet been tested and verified 	- All benchmarks are PASS

--- tahoe/src/elements/continuum/solid/materials/CB/HexLattice2DT.cpp
+++ tahoe/src/elements/continuum/solid/materials/CB/HexLattice2DT.cpp
@@ -1,5 +1,6 @@
-/* $Id: HexLattice2DT.cpp,v 1.3 2004-07-15 08:26:42 paklein Exp $ */
+/* $Id: HexLattice2DT.cpp,v 1.4 2005-08-24 03:00:54 jzimmer Exp $ */
 #include "HexLattice2DT.h"
+#include "ParameterContainerT.h"
 
 using namespace Tahoe;
 
@@ -10,13 +11,98 @@ static int AtomsInShells(int nshells) {
 	if (nshells < 0 || nshells > 5) ExceptionT::OutOfRange();
 	return atoms_in_shells[nshells-1];
 };
+const double sqrt2 = sqrt(2.0);
 const double sqrt3 = sqrt(3.0);
 
 /* constructor */
 HexLattice2DT::HexLattice2DT(int nshells):
+        ParameterInterfaceT("CB_lattice_Hex"),
 	fNumShells(nshells)
 {
 
+}
+
+/* information about subordinate parameter lists */
+void HexLattice2DT::DefineSubs(SubListT& sub_list) const
+{
+        /* inherited */
+        ParameterInterfaceT::DefineSubs(sub_list);
+
+        sub_list.AddSub("Hex_lattice_orientation", ParameterListT::Once, true);
+}
+
+/* a pointer to the ParameterInterfaceT of the given subordinate */
+ParameterInterfaceT* HexLattice2DT::NewSub(const StringT& name) const
+{
+        if (name == "Hex_lattice_orientation")
+        {
+                ParameterContainerT* orientation = new ParameterContainerT(name);
+                orientation->SetListOrder(ParameterListT::Choice);
+
+                ParameterContainerT natural("HEX2D_natural");
+                orientation->AddSub(natural);
+
+                ParameterContainerT HEX2D_90("HEX2D_90");
+                ParameterT HEX2D_90_type(ParameterT::Enumeration, "sense");
+                HEX2D_90_type.AddEnumeration("90 degree rotation of lattice",0);
+                HEX2D_90_type.SetDefault(0);
+                HEX2D_90.AddParameter(HEX2D_90_type);
+                orientation->AddSub(HEX2D_90);
+
+                ParameterContainerT Rotation_angle("HEX2D_Rotation_angle");
+                Rotation_angle.AddParameter(ParameterT::Double, "phi");
+                orientation->AddSub(Rotation_angle);
+
+                return orientation;
+        }
+        else /* inherited */
+                return ParameterInterfaceT::NewSub(name);
+}
+
+/* accept parameter list */
+void HexLattice2DT::TakeParameterList(const ParameterListT& list)
+{
+        const char caller[] = "HexLattice2DT::TakeParameterList";
+
+        /* inherited */
+        ParameterInterfaceT::TakeParameterList(list);
+
+        /* set Q */
+        const ParameterListT& orientation = list.GetListChoice(*this, "Hex_lattice_orientation");
+        dMatrixT Q;
+        SetQ(orientation, Q);
+
+        /* initialize bond table */
+        Initialize(&Q);
+}
+
+/* set the transformation matrix for the given orientation */
+void HexLattice2DT::SetQ(const ParameterListT& list, dMatrixT& Q)
+{
+        const char caller[] = "HexLattice2DT::SetQ";
+
+        /* dimension */
+        Q.Dimension(2);
+        Q = 0.0;
+
+        /* extract orientation */
+        if (list.Name() == "HEX2D_natural")
+                Q.Identity();
+        else if (list.Name() == "HEX2D_90")
+        {
+                double cos45 = 0.5*sqrt2;
+
+                int sense = list.GetParameter("sense");
+                if (sense == 0) /* 90 degree rotation of lattice */ {
+                        Q(0,0) = Q(1,1) = cos45;
+                        Q(0,1) =-cos45;
+                        Q(1,0) = cos45;
+                }
+                else
+                        ExceptionT::GeneralFail(caller, "unrecognized 90 sense %d", sense);
+        }
+        else
+                ExceptionT::GeneralFail(caller, "unrecognized orientation \"%s\"", list.Name().Pointer());
 }
 
 /* initialize bond table values */
--- tahoe/src/elements/continuum/solid/materials/CB/HexLattice2DT.h
+++ tahoe/src/elements/continuum/solid/materials/CB/HexLattice2DT.h
@@ -1,24 +1,57 @@
-/* $Id: HexLattice2DT.h,v 1.3 2004-07-15 08:26:42 paklein Exp $ */
+/* $Id: HexLattice2DT.h,v 1.4 2005-08-24 03:00:54 jzimmer Exp $ */
 #ifndef _HEX_LATTICE_2D_T_H_
 #define _HEX_LATTICE_2D_T_H_
 
 /* base class */
 #include "CBLatticeT.h"
+#include "ParameterInterfaceT.h"
 
 namespace Tahoe {
 
 /** a 2D hexagonal lattice. The number of shells of neighbors can be
  * selected up to 5th nearest. The bonds are scaled such that the
- * nearest neighbor bond distance is 1. */
-class HexLattice2DT: public CBLatticeT
+ * nearest neighbor bond distance is 1.
+ * \note the lattice can be initialized either by
+ * -# HEXLattice2DT::Initialize
+ * -# HEXLattice2DT::TakeParameterList */
+class HexLattice2DT: public CBLatticeT, public ParameterInterfaceT
 {
 public:
+
+        /** orientation codes - for crystal axes rotated wrt global axes*/
+        enum OrientationCodeT {
+        kHEX2Dnatural = 0,
+            kHEX2D_90 = 1,
+            kRotationAngle = 2};
 
 	/** constructor */
 	HexLattice2DT(int nshells);
 
 	/** number of shells */
 	int NumShells(void) const { return fNumShells; };
+
+        /** \name implementation of the ParameterInterfaceT interface */
+        /*@{*/
+        /** information about subordinate parameter lists */
+        virtual void DefineSubs(SubListT& sub_list) const;
+
+        /** a pointer to the ParameterInterfaceT of the given subordinate */
+        virtual ParameterInterfaceT* NewSub(const StringT& name) const;
+
+        /** accept parameter list */
+        virtual void TakeParameterList(const ParameterListT& list);
+        /*@}*/
+
+        /** set the transformation matrix for the given orientation, where Q is
+         * defined as:
+         *
+         *                      Q = d x_natural / d x_global
+         *
+         * So that the vectors are transformed by:
+         *
+         *                      r_global = Transpose[Q].r_natural
+         */
+        static void SetQ(const ParameterListT& list, dMatrixT& Q);
 
 protected:
 
