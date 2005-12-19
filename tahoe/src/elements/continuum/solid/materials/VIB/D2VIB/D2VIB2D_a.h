6972f74ccdad3077a3c7162a4441028f8c0b7bd8
tdnguye <tdnguye@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2005-12-19 17:16:18 +0000

    removed declaration of undefined virtual function Print

--- tahoe/src/elements/continuum/solid/materials/VIB/D2VIB/D2VIB2D_a.h
+++ tahoe/src/elements/continuum/solid/materials/VIB/D2VIB/D2VIB2D_a.h
@@ -1,4 +1,4 @@
-/* $Id: D2VIB2D_a.h,v 1.4 2003-01-29 07:34:55 paklein Exp $ */
+/* $Id: D2VIB2D_a.h,v 1.5 2005-12-19 17:16:18 tdnguye Exp $ */
 /* created: paklein (10/23/1999) */
 #ifndef _D2_VIB_2D_A_H_
 #define _D2_VIB_2D_A_H_
@@ -14,9 +14,6 @@ public:
 
 	/* constructor */
 	D2VIB2D_a(ifstreamT& in, const D2FSMatSupportT& support);
-
-	/* print parameters */
-	virtual void Print(ostream& out) const;
 
 	/* material internal stress terms */
 	virtual void StressTerms(dMatrixT& DW, dMatrixT& DDW);
