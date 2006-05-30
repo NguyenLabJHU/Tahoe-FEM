0bb819d1ee871e9df44b82166800720ff86e5ae0
tdnguye <tdnguye@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2006-05-30 23:22:04 +0000

    update LinearDamageT to TahoeII (XML)

--- tahoe/src/elements/cohesive_surface/cohesive_models/LinearDamageT.cpp
+++ tahoe/src/elements/cohesive_surface/cohesive_models/LinearDamageT.cpp
@@ -1,4 +1,4 @@
-/* $Id: LinearDamageT.cpp,v 1.18 2004-07-15 08:26:02 paklein Exp $ */
+/* $Id: LinearDamageT.cpp,v 1.19 2006-05-30 23:22:04 tdnguye Exp $ */
 /* created: paklein (08/21/2000) */
 #include "LinearDamageT.h"
 
@@ -23,22 +23,51 @@ ExceptionT::GeneralFail("LinearDamageT::LinearDamageT", "out of date");
 	/* traction potential parameters */
 	in >> fd_c_n; if (fd_c_n < 0.0) throw ExceptionT::kBadInputValue;
 	in >> fd_c_t; if (fd_c_t < 0.0) throw ExceptionT::kBadInputValue;
-	
-	/* stiffness multiplier */
-	in >> fpenalty; if (fpenalty < 0.0) throw ExceptionT::kBadInputValue;
-
-	/* penetration stiffness */
-	fK = 0.0;
-//TEMP: decide on penalty stiffness
 #endif
 }
+
+/* describe the parameters  */
+void LinearDamageT::DefineParameters(ParameterListT& list) const
+{
+	/* inherited */
+	SurfacePotentialT::DefineParameters(list);
+
+	ParameterT del_c_n(fd_c_n, "delta_crit_n");
+	del_c_n.AddLimit(0.0, LimitT::Lower);
+	list.AddParameter(del_c_n);
+
+	ParameterT del_c_t(fd_c_t, "delta_crit_t");
+	del_c_t.AddLimit(0.0, LimitT::LowerInclusive);
+	list.AddParameter(del_c_t);
+
+	ParameterT L_max(fL_max, "max opening ratio");
+	L_max.AddLimit(0.0, LimitT::Lower);
+	list.AddParameter(L_max);
+
+	ParameterT K(fK, "penetration stiffness");
+	K.AddLimit(0.0, LimitT::Lower);
+	list.AddParameter(K);
+}
+
+/* accept parameter list */
+void LinearDamageT::TakeParameterList(const ParameterListT& list)
+{
+	/* inherited */
+	SurfacePotentialT::TakeParameterList(list);
+
+	fd_c_n = list.GetParameter("delta_crit_n");
+	fd_c_t = list.GetParameter("delta_crit_t");
+	fL_max = list.GetParameter("max opening ratio");
+	fK = list.GetParameter("penetration stiffness");
+}
+
 
 /* return the number of state variables */
 int LinearDamageT::NumStateVariables(void) const
 {
 	return 2*fInitTraction.Length() + // initiation traction
 	       1 +                      // max opening
-	       1;                       // trial max opening
+  	       1;                       // trial max opening
 }
 
 /* initialize the state variable array */
@@ -52,6 +81,7 @@ void LinearDamageT::InitStateVariables(ArrayT<double>& state)
 	state[  kMaxOpening] = 0.0; // max opening
 	state[kTrialOpening] = 0.0; // trial opening	
 }
+
 
 /* surface potential */
 double LinearDamageT::FractureEnergy(const ArrayT<double>& state)
@@ -82,27 +112,24 @@ const dArrayT& LinearDamageT::Traction(const dArrayT& jump_u, ArrayT<double>& st
 	double u_n = jump_u.Last();
 	
 	/* opening parameter */
-	double r_n = (u_n < 0.0) ? 0.0 : u_n/fd_c_n; // no damage evolution in compression
+	double r_n = (u_n < 0.0) ? 0.0 : u_n/fd_c_n;
 	double r_t2;
 	if (jump_u.Length() == 2)
 		r_t2 = jump_u[0]*jump_u[0]/fd_c_t/fd_c_t;
 	else
 		r_t2 = (jump_u[0]*jump_u[0] + jump_u[1]*jump_u[1])/fd_c_t/fd_c_t;
+		
 	double L  = sqrt(r_t2 + r_n*r_n);
-	if (qIntegrate)
-		state[kTrialOpening] = L;
 
 	if (L > kSmall)
 	{
 		/* damage law */
 		double L_max = state[kMaxOpening];
 		double f;
-		if (L > 1.0)
+		if (L > fL_max)
 			f = 0.0;     // failed
-		else if (L > L_max || L_max < kSmall)
-			f = 1.0 - L; // loading
 		else
-			f = L*(1.0 - L_max)/L_max; // unloading to origin
+			f = L/fL_max*(fL_max/L - 1.0); // unloading to origin
 
 		/* traction */
 		double* init_traction = state.Pointer(kInitTraction);
@@ -112,16 +139,8 @@ const dArrayT& LinearDamageT::Traction(const dArrayT& jump_u, ArrayT<double>& st
 	}
 	else
 		fTraction = 0.0;
-
 	/* penetration */
 	if (u_n < 0) fTraction.Last() += fK*u_n;
-
-	if (qIntegrate)
-	{
-		/* update state variables (in place) */
-		if (state[kTrialOpening] > state[kMaxOpening])
-			state[kMaxOpening] = state[kTrialOpening];
-	}
 
 	return fTraction;
 }
@@ -147,43 +166,39 @@ const dMatrixT& LinearDamageT::Stiffness(const dArrayT& jump_u, const ArrayT<dou
 		r_t2 = (jump_u[0]*jump_u[0] + jump_u[1]*jump_u[1])/fd_c_t/fd_c_t;
 	double L = sqrt(r_t2 + r_n*r_n);
 
-	if (L > kSmall)
+	if (L > kSmall && (fL_max - L) > kSmall)
 	{
 		/* damage law */
-		double L_max = state[kMaxOpening];
-		double Df;
-		if (L > 1.0)
-			Df = 0.0;     // failed
-		else if (L > L_max || L_max < kSmall)
-			Df = -1.0; // loading
-		else
-			Df = (1.0 - L_max)/L_max; // unloading
-			
 		const double* init_traction = state.Pointer(kInitTraction);
-		double DfbyL = Df/L;
 		if (nsd == 2)
 		{
-			double r_t = DfbyL*jump_u[0]/fd_c_t/fd_c_t;
-			double r_n = DfbyL*jump_u[1]/fd_c_n/fd_c_n;
-			fStiffness(0,0) = init_traction[0]*r_t;
-			fStiffness(0,1) = init_traction[0]*r_n;
-			fStiffness(1,0) = init_traction[1]*r_t;
-			fStiffness(1,1) = init_traction[1]*r_n;
+			double k22 = (fL_max/L - 1.0) - (u_n*u_n*fL_max)/(L*L*L);
+			double k11 = (fL_max/L - 1.0) - (jump_u[0]*jump_u[0]*fL_max)/(L*L*L);
+			double k12 = - u_n*jump_u[0]*fL_max/(L*L*L);
+			
+			fStiffness(0,0) = init_traction[0]*k11;
+			fStiffness(0,1) = init_traction[0]*k12;
+			fStiffness(1,0) = init_traction[1]*k12;
+			fStiffness(1,1) = init_traction[1]*k22;
 		}
 		else
 		{
-			double r_t0 = DfbyL*jump_u[0]/fd_c_t/fd_c_t;
-			double r_t1 = DfbyL*jump_u[1]/fd_c_t/fd_c_t;
-			double r_n  = DfbyL*jump_u[2]/fd_c_n/fd_c_n;
-			fStiffness(0,0) = init_traction[0]*r_t0;
-			fStiffness(0,1) = init_traction[0]*r_t1;
-			fStiffness(0,2) = init_traction[0]*r_n;
-			fStiffness(1,0) = init_traction[1]*r_t0;
-			fStiffness(1,1) = init_traction[1]*r_t1;
-			fStiffness(1,2) = init_traction[1]*r_n;
-			fStiffness(2,0) = init_traction[2]*r_t0;
-			fStiffness(2,1) = init_traction[2]*r_t1;
-			fStiffness(2,2) = init_traction[2]*r_n;
+			double k33 = (fL_max/L - 1.0) - (u_n*u_n*fL_max)/(L*L*L);
+			double k22 = (fL_max/L - 1.0) - (jump_u[1]*jump_u[1]*fL_max)/(L*L*L);
+			double k11 = (fL_max/L - 1.0) - (jump_u[0]*jump_u[0]*fL_max)/(L*L*L);
+			double k13 = - u_n*jump_u[0]*fL_max/(L*L*L);
+			double k23 = - u_n*jump_u[1]*fL_max/(L*L*L);
+			double k12 = - jump_u[1]*jump_u[0]*fL_max/(L*L*L);
+			
+			fStiffness(0,0) = init_traction[0]*k11;
+			fStiffness(0,1) = init_traction[0]*k12;
+			fStiffness(0,2) = init_traction[0]*k23;
+			fStiffness(1,0) = init_traction[1]*k12;
+			fStiffness(1,1) = init_traction[1]*k22;
+			fStiffness(1,2) = init_traction[1]*k23;
+			fStiffness(2,0) = init_traction[2]*k13;
+			fStiffness(2,1) = init_traction[2]*k23;
+			fStiffness(2,2) = init_traction[2]*k33;
 		}
 	}
 	else
@@ -202,14 +217,22 @@ SurfacePotentialT::StatusT LinearDamageT::Status(const dArrayT& jump_u,
 {
 #pragma unused(state)
 
-	double u_t = jump_u[0];
-	double u_n = jump_u[1];
+//	double u_t = jump_u[0];
+//	double u_n = jump_u[1];
 
-	double r_t = u_t/fd_c_t;
-	double r_n = u_n/fd_c_n;
-	double L = sqrt(r_t*r_t + r_n*r_n); // (1.1)
+	int nsd = jump_u.Length();
+	double u_n = jump_u.Last();
 	
-	if (L > 1.0)
+	/* opening parameter */
+	double r_n = (u_n < 0.0) ? 0.0 : u_n/fd_c_n; // no damage evolution in compression
+	double r_t2;
+	if (nsd == 2)
+		r_t2 = jump_u[0]*jump_u[0]/fd_c_t/fd_c_t;
+	else
+		r_t2 = (jump_u[0]*jump_u[0] + jump_u[1]*jump_u[1])/fd_c_t/fd_c_t;
+	double L = sqrt(r_t2 + r_n*r_n);
+
+	if (L > fL_max)
 		return Failed;
 	else if (L > 0.0)
 		return Critical;
