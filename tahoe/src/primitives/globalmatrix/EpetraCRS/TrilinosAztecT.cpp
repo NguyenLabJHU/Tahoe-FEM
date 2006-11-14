981182dda1d99f0f5f83ae63486291250f842cde
paklein <paklein@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2006-11-14 04:31:40 +0000

    updates for changes to EpetraCRSMatrixT

--- tahoe/src/primitives/globalmatrix/EpetraCRS/TrilinosAztecT.cpp
+++ tahoe/src/primitives/globalmatrix/EpetraCRS/TrilinosAztecT.cpp
@@ -1,4 +1,4 @@
-/* $Id: TrilinosAztecT.cpp,v 1.1 2006-11-01 05:15:31 paklein Exp $ */
+/* $Id: TrilinosAztecT.cpp,v 1.2 2006-11-14 04:31:40 paklein Exp $ */
 #include "TrilinosAztecT.h"
 
 /* library support options */
@@ -66,42 +66,15 @@ void TrilinosAztecT::BackSubstitute(dArrayT& result)
 {
 	const char caller[] = "TrilinosAztecT::BackSubstitute";
 
-	/* set-up Epetra structures */
-#ifdef __TAHOE_MPI__
-	Epetra_MpiComm epetra_comm(fComm);
-#else
-	Epetra_SerialComm epetra_comm;
-#endif
-	iArrayT active_tmp;
-	active_tmp.Alias(factive);
-	active_tmp--; /* solver uses 0 indexing */
-	Epetra_Map map(fTotNumEQ, fLocNumEQ, active_tmp.Pointer(), 0, epetra_comm);
-	iArrayT row_count(fLocNumEQ);
-	for (int i = 0; i < fLocNumEQ - 1; i++) {
-		row_count[i] = frowptr[i+1] - frowptr[i];
-	}
-	row_count[fLocNumEQ-1] = fnzval.Length() - frowptr[fLocNumEQ-1];
-	Epetra_CrsMatrix A(Copy, map, row_count.Pointer(), true);
-
-	/* copy data into epetra_matrix */
-	for (int i = 0; i < factive.Length(); i++) {
-		int offset = frowptr[i];
-		int ret = A.InsertGlobalValues(factive[i], row_count[i], fnzval.Pointer(offset), fcolind.Pointer(offset));
-		if (ret != 0) {
-			ExceptionT::GeneralFail(caller, "InsertGlobalValues: error %d in row %d", ret, i+1);
-		}
-	}  
-	int ret = A.FillComplete();
-	if (ret != 0) {
-		ExceptionT::GeneralFail(caller, "FillComplete: error %d", ret);
-	}
+	/* get Epetra matrix */
+	Epetra_CrsMatrix* A = Translate();
 
 	/* Create x and b vectors */
-	Epetra_Vector b(Copy, map, result.Pointer());
-	Epetra_Vector x(View, map, result.Pointer());
+	Epetra_Vector b(Copy, *fepetra_map, result.Pointer());
+	Epetra_Vector x(View, *fepetra_map, result.Pointer());
 
 	/* create linear problem */
-	Epetra_LinearProblem problem(&A, &x, &b);
+	Epetra_LinearProblem problem(A, &x, &b);
 
 	/* create AztecOO instance */
 	AztecOO solver(problem);
@@ -111,7 +84,9 @@ void TrilinosAztecT::BackSubstitute(dArrayT& result)
 
 	/* solve */
 	solver.Iterate(1000, 1.0E-8);
-	active_tmp++; /* restore first active row = 1 */
+	
+	/* clean-up */
+	delete A;
 }
 
 #endif /* __TRILINOS__ */
