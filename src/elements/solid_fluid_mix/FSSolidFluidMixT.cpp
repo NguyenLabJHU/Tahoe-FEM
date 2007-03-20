0570b25af093d68871b22555c8896244febeba2b
ebrahimi <ebrahimi@75a0ec24-64db-4a18-816e-f4d92fc68425>
2007-03-20 19:27:35 +0000

    commiting last file to the server. theta_dotdot_0 should be assigned to theta_dot_0.

--- src/elements/solid_fluid_mix/FSSolidFluidMixT.cpp
+++ src/elements/solid_fluid_mix/FSSolidFluidMixT.cpp
@@ -722,6 +722,7 @@ void FSSolidFluidMixT::RHSDriver_monolithic(void)
     for (int i=0; i<n_sd; i++)
 	fGravity_column_matrix(i,0)=fGravity_vector[i];
 
+
     while (NextElement())
     {
 	fFd_int_N1_vector = 0.0;
@@ -998,7 +999,7 @@ void FSSolidFluidMixT::RHSDriver_monolithic(void)
 		    const double* Weight = fShapes_displ->IPWeights();
 		    fShapes_displ->TopIP();
 		    fShapes_press->TopIP();
-		    press_dot.SetGlobal((*fPress)[2]);
+
 		    while (fShapes_displ->NextIP() && fShapes_press->NextIP())
 		    {
 			const int IP = fShapes_displ->CurrIP();	
