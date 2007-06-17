8082cf54ddc738b5fc174baebc26a48effb33d39
paklein <paklein@75a0ec24-64db-4a18-816e-f4d92fc68425>
2007-06-17 04:02:34 +0000

    working prototypes

--- src/elements/surface_CB_Tersoff/Tersoff_inc.h
+++ src/elements/surface_CB_Tersoff/Tersoff_inc.h
@@ -1,4 +1,4 @@
-/* $Id: Tersoff_inc.h,v 1.1 2007-06-17 03:27:30 paklein Exp $ */
+/* $Id: Tersoff_inc.h,v 1.2 2007-06-17 04:02:34 paklein Exp $ */
 #ifndef TERSOFF_INC_H
 #define TERSOFF_INC_H
 
@@ -6,10 +6,7 @@
 extern "C" {
 #endif
 
-/* function to compute derivatives of the potential function wrt to the
- * internal degrees of freedom 
- *
- * Sequence of parameters is:
+/* Sequence of parameters is:
  * A
  * B
  * Mass
@@ -24,7 +21,14 @@ extern "C" {
  * R
  * S
  */
+
+/* function to compute derivatives of the potential function wrt to the
+ * internal degrees of freedom */
 void get_dXsi(double* params, double *Xsi, double *Xa, double *Ya, double *Za, double* Cmat, double* dXsi, double* ddXsi); 
+
+/* function to compute derivatives of the potential function wrt to the
+ * internal degrees of freedom */
+void get_dUdC(double* params, double *Xsi, double *Xa, double *Ya, double *Za, double* Cmat, double* dUdC); 
 
 #ifdef __cplusplus
 }
