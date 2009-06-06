4fc2e0851eab5400f06df5613c7f96879221321d
hspark <hspark@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2009-06-06 17:28:49 +0000

    Additions for edge CB

--- tahoe/src/elements/continuum/solid/materials/CB/BondLatticeT.cpp
+++ tahoe/src/elements/continuum/solid/materials/CB/BondLatticeT.cpp
@@ -1,4 +1,4 @@
-/* $Id: BondLatticeT.cpp,v 1.8 2006-07-03 20:19:32 hspark Exp $ */
+/* $Id: BondLatticeT.cpp,v 1.9 2009-06-06 17:28:49 hspark Exp $ */
 /* created: paklein (01/07/1997) */
 #include "BondLatticeT.h"
 #include <math.h>
@@ -15,13 +15,15 @@ BondLatticeT::~BondLatticeT(void) { }
 void BondLatticeT::Initialize(const dMatrixT* Q)
 {
 	const char caller[] = "BondLatticeT::Initialize";
-
+	
 	/* pure virtual */
 	LoadBondTable();
 
 	/* dimension work space */
 	int nsd = fBonds.MinorDim();
 	fBondDp.Dimension(nsd);
+	fBondDpS.Dimension(nsd);
+	fBondDpE.Dimension(nsd);
 //	fLatDimMatrix.Dimension(nsd);
 	fStrain.Dimension(nsd);
 	fStretch.Dimension(nsd);
@@ -88,6 +90,114 @@ void BondLatticeT::Initialize(const dMatrixT* Q)
 			/* transform */
 			fQ.MultTx(fBondDpS2, fBondShS2);		
 		}
+		/* TRANSFORM BONDS FOR EDGE CB STUFF */
+		for (int bond = 0; bond < fAllBulkBonds.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fAllBulkBonds.RowAlias(bond, fBondShBE);
+			
+			/* temp */
+			fBondDpBE = fBondShBE;
+		
+			/* transform */
+			fQ.MultTx(fBondDpBE, fBondShBE);		
+		}
+		/* Edge 1 atoms */
+		for (int bond = 0; bond < fEdgeBonds1.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fEdgeBonds1.RowAlias(bond, fBondShE1);
+			
+			/* temp */
+			fBondDpE1 = fBondShE1;
+		
+			/* transform */
+			fQ.MultTx(fBondDpE1, fBondShE1);		
+		}
+		/* Edge 2 atoms */
+		for (int bond = 0; bond < fEdgeBonds2.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fEdgeBonds2.RowAlias(bond, fBondShE2);
+			
+			/* temp */
+			fBondDpE2 = fBondShE2;
+		
+			/* transform */
+			fQ.MultTx(fBondDpE2, fBondShE2);		
+		}
+		/* Edge 3 atoms */
+		for (int bond = 0; bond < fEdgeBonds3.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fEdgeBonds3.RowAlias(bond, fBondShE3);
+			
+			/* temp */
+			fBondDpE3 = fBondShE3;
+		
+			/* transform */
+			fQ.MultTx(fBondDpE3, fBondShE3);		
+		}
+		/* Edge 4 atoms */
+		for (int bond = 0; bond < fEdgeBonds4.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fEdgeBonds4.RowAlias(bond, fBondShE4);
+			
+			/* temp */
+			fBondDpE4 = fBondShE4;
+		
+			/* transform */
+			fQ.MultTx(fBondDpE4, fBondShE4);		
+		}
+		/* Edge 5 atoms */
+		for (int bond = 0; bond < fEdgeBonds5.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fEdgeBonds5.RowAlias(bond, fBondShE5);
+			
+			/* temp */
+			fBondDpE5 = fBondShE5;
+		
+			/* transform */
+			fQ.MultTx(fBondDpE5, fBondShE5);		
+		}
+		/* Edge 6 atoms */
+		for (int bond = 0; bond < fEdgeBonds6.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fEdgeBonds6.RowAlias(bond, fBondShE6);
+			
+			/* temp */
+			fBondDpE6 = fBondShE6;
+		
+			/* transform */
+			fQ.MultTx(fBondDpE6, fBondShE6);		
+		}
+		/* Edge 7 atoms */
+		for (int bond = 0; bond < fEdgeBonds7.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fEdgeBonds7.RowAlias(bond, fBondShE7);
+			
+			/* temp */
+			fBondDpE7 = fBondShE7;
+		
+			/* transform */
+			fQ.MultTx(fBondDpE7, fBondShE7);		
+		}
+		/* Edge 8 atoms */
+		for (int bond = 0; bond < fEdgeBonds8.MajorDim(); bond++)
+		{
+			/* get bond vector */
+			fEdgeBonds8.RowAlias(bond, fBondShE8);
+			
+			/* temp */
+			fBondDpE8 = fBondShE8;
+		
+			/* transform */
+			fQ.MultTx(fBondDpE8, fBondShE8);		
+		}
 	}
 }
 
@@ -128,6 +238,34 @@ void BondLatticeT::ComputeDeformedLengths(const dSymMatrixT& strain)
 	}
 }
 
+/* Compute deformed surface bond lengths */
+void BondLatticeT::ComputeDeformedSurfaceLengths(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+//	fStretch.Identity();
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fBonds.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fBonds.RowAlias(bond, fBondShS);
+
+		/* using symmetry in C */
+		fStretch.Multx(fBondShS, fBondDpS);
+
+		/* deformed length */
+		fDefLength[bond] = sqrt(dArrayT::Dot(fBondShS, fBondDpS));
+	}
+}
+
 /* BELOW ARE NEW SURFACE CB SPECIFIC FUNCTIONS */
 /* Compute deformed lengths for a representative bulk atom */
 void BondLatticeT::ComputeDeformedBulkBonds(const dSymMatrixT& strain)
@@ -141,6 +279,7 @@ void BondLatticeT::ComputeDeformedBulkBonds(const dSymMatrixT& strain)
 	/* compute stretch tensor */
 	fStretch.SetToScaled(2.0, fStrain);
 	fStretch.PlusIdentity(1.0);
+//	fStretch.Identity();
 
 	/* loop over all bonds */
 	for (int bond = 0; bond < fBulkBonds.MajorDim(); bond++)
@@ -168,6 +307,7 @@ void BondLatticeT::ComputeDeformedSurf1Bonds(const dSymMatrixT& strain)
 	/* compute stretch tensor */
 	fStretch.SetToScaled(2.0, fStrain);
 	fStretch.PlusIdentity(1.0);
+//	fStretch.Identity();
 
 	/* loop over all bonds */
 	for (int bond = 0; bond < fSurf1Bonds.MajorDim(); bond++)
@@ -195,6 +335,7 @@ void BondLatticeT::ComputeDeformedSurf2Bonds(const dSymMatrixT& strain)
 	/* compute stretch tensor */
 	fStretch.SetToScaled(2.0, fStrain);
 	fStretch.PlusIdentity(1.0);
+//	fStretch.Identity();
 
 	/* loop over all bonds */
 	for (int bond = 0; bond < fSurf2Bonds.MajorDim(); bond++)
@@ -207,5 +348,274 @@ void BondLatticeT::ComputeDeformedSurf2Bonds(const dSymMatrixT& strain)
 		
 		/* deformed length */
 		fDefSurf2[bond] = sqrt(dArrayT::Dot(fBondShS2, fBondDpS2));
+	}
+}
+
+/* Compute deformed EDGE bond lengths */
+void BondLatticeT::ComputeDeformedEdgeLengths(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fBonds.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fBonds.RowAlias(bond, fBondShE);
+
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE, fBondDpE);
+
+		/* deformed length */
+		fDefLength[bond] = sqrt(dArrayT::Dot(fBondShE, fBondDpE));
+	}
+}
+
+void BondLatticeT::ComputeDeformedEdgeBulkBonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fAllBulkBonds.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fAllBulkBonds.RowAlias(bond, fBondShBE);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShBE, fBondDpBE);
+		
+		/* deformed length */
+		fDefBulkLength[bond] = sqrt(dArrayT::Dot(fBondShBE, fBondDpBE));
+	}
+}
+
+/* Type-1 Edge Atoms */
+void BondLatticeT::ComputeDeformedEdge1Bonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fEdgeBonds1.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fEdgeBonds1.RowAlias(bond, fBondShE1);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE1, fBondDpE1);
+		
+		/* deformed length */
+		fDefEdge1[bond] = sqrt(dArrayT::Dot(fBondShE1, fBondDpE1));
+	}
+}
+
+/* Type-2 Edge Atoms */
+void BondLatticeT::ComputeDeformedEdge2Bonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fEdgeBonds2.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fEdgeBonds2.RowAlias(bond, fBondShE2);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE2, fBondDpE2);
+		
+		/* deformed length */
+		fDefEdge2[bond] = sqrt(dArrayT::Dot(fBondShE2, fBondDpE2));
+	}
+}
+
+/* Type-3 Edge Atoms */
+void BondLatticeT::ComputeDeformedEdge3Bonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fEdgeBonds3.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fEdgeBonds3.RowAlias(bond, fBondShE3);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE3, fBondDpE3);
+		
+		/* deformed length */
+		fDefEdge3[bond] = sqrt(dArrayT::Dot(fBondShE3, fBondDpE3));
+	}
+}
+
+/* Type-4 Edge Atoms */
+void BondLatticeT::ComputeDeformedEdge4Bonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fEdgeBonds4.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fEdgeBonds4.RowAlias(bond, fBondShE4);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE4, fBondDpE4);
+		
+		/* deformed length */
+		fDefEdge4[bond] = sqrt(dArrayT::Dot(fBondShE4, fBondDpE4));
+	}
+}
+
+/* Type-5 Edge Atoms */
+void BondLatticeT::ComputeDeformedEdge5Bonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fEdgeBonds5.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fEdgeBonds5.RowAlias(bond, fBondShE5);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE5, fBondDpE5);
+		
+		/* deformed length */
+		fDefEdge5[bond] = sqrt(dArrayT::Dot(fBondShE5, fBondDpE5));
+	}
+}
+
+/* Type-6 Edge Atoms */
+void BondLatticeT::ComputeDeformedEdge6Bonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fEdgeBonds6.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fEdgeBonds6.RowAlias(bond, fBondShE6);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE6, fBondDpE6);
+		
+		/* deformed length */
+		fDefEdge6[bond] = sqrt(dArrayT::Dot(fBondShE6, fBondDpE6));
+	}
+}
+
+/* Type-7 Edge Atoms */
+void BondLatticeT::ComputeDeformedEdge7Bonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fEdgeBonds7.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fEdgeBonds7.RowAlias(bond, fBondShE7);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE7, fBondDpE7);
+		
+		/* deformed length */
+		fDefEdge7[bond] = sqrt(dArrayT::Dot(fBondShE7, fBondDpE7));
+	}
+}
+
+/* Type-8 Edge Atoms */
+void BondLatticeT::ComputeDeformedEdge8Bonds(const dSymMatrixT& strain)
+{
+	/* spatial vs. lattice dimension translation */
+	if (fStrain.Rows() == 3 && strain.Rows() == 2)
+		fStrain.ExpandFrom2D(strain);
+	else
+		fStrain = strain;
+
+	/* compute stretch tensor */
+	fStretch.SetToScaled(2.0, fStrain);
+	fStretch.PlusIdentity(1.0);
+
+	/* loop over all bonds */
+	for (int bond = 0; bond < fEdgeBonds8.MajorDim(); bond++)
+	{
+		/* get bond vector */
+		fEdgeBonds8.RowAlias(bond, fBondShE8);
+		
+		/* using symmetry in C */
+		fStretch.Multx(fBondShE8, fBondDpE8);
+		
+		/* deformed length */
+		fDefEdge8[bond] = sqrt(dArrayT::Dot(fBondShE8, fBondDpE8));
 	}
 }
--- tahoe/src/elements/continuum/solid/materials/CB/BondLatticeT.h
+++ tahoe/src/elements/continuum/solid/materials/CB/BondLatticeT.h
@@ -1,4 +1,4 @@
-/* $Id: BondLatticeT.h,v 1.9 2007-06-12 16:03:17 hspark Exp $ */
+/* $Id: BondLatticeT.h,v 1.10 2009-06-06 17:28:49 hspark Exp $ */
 /* created: paklein (01/07/1997) */
 #ifndef _BONDLATTICET_H_
 #define _BONDLATTICET_H_
@@ -44,31 +44,67 @@ public:
 	const iArrayT& BulkCounts(void) const;
 	const iArrayT& Surf1Counts(void) const;
 	const iArrayT& Surf2Counts(void) const;
+	const iArrayT& EdgeBulkCounts(void) const;
+	const iArrayT& Edge1Counts(void) const;
+	const iArrayT& Edge2Counts(void) const;
+	const iArrayT& Edge3Counts(void) const;
+	const iArrayT& Edge4Counts(void) const;
+	const iArrayT& Edge5Counts(void) const;
+	const iArrayT& Edge6Counts(void) const;
+	const iArrayT& Edge7Counts(void) const;
+	const iArrayT& Edge8Counts(void) const;	
 	
 	/* Surf3 arrays for {110} surfaces */
 	const iArrayT& Surf3Counts(void) const;
 	const dArrayT& DeformedLengths(void) const;
+	const dArrayT& DeformedSurfaceLengths(void) const;
+	const dArrayT& DeformedEdgeLengths(void) const;
 	const dArrayT& DeformedBulk(void) const;
 	const dArrayT& DeformedSurf1(void) const;
 	const dArrayT& DeformedSurf2(void) const;
 	const dArrayT& DeformedSurf3(void) const;
+	const dArrayT& DeformedEdge1(void) const;
+	const dArrayT& DeformedEdge2(void) const;
+	const dArrayT& DeformedEdge3(void) const;
+	const dArrayT& DeformedEdge4(void) const;
+	const dArrayT& DeformedEdge5(void) const;
+	const dArrayT& DeformedEdge6(void) const;
+	const dArrayT& DeformedEdge7(void) const;
+	const dArrayT& DeformedEdge8(void) const;	
+	const dArrayT& DeformedEdgeBulk(void) const;
 	dArrayT& DeformedLengths(void);
+	dArrayT& DeformedSurfaceLengths(void);
+	dArrayT& DeformedEdgeLengths(void);
 	const dArray2DT& Bonds(void) const;
 //	int NumberOfLatticeDim(void) const;
 //	int NumberOfSpatialDim(void) const;
 	int NumberOfBonds(void) const { return fBonds.MajorDim(); };
 	dSymMatrixT& Stretch(void) { return fStretch; };
 	iArrayT& AtomTypes(void) {return fAtomType; };
+	iArrayT& EdgeTypes(void) {return fEdgeType; };	
 	/*@}*/
 
 	/* compute deformed bond lengths from the given Green strain */
 	void ComputeDeformedLengths(const dSymMatrixT& strain);
+	void ComputeDeformedSurfaceLengths(const dSymMatrixT& strain);
+	void ComputeDeformedEdgeLengths(const dSymMatrixT& strain);
 
 	/* Similar deformed lengths function but for representative bulk/surface atoms */
 	void ComputeDeformedBulkBonds(const dSymMatrixT& strain);
 	void ComputeDeformedSurf1Bonds(const dSymMatrixT& strain);
 	void ComputeDeformedSurf2Bonds(const dSymMatrixT& strain);
 	void ComputeDeformedSurf3Bonds(const dSymMatrixT& strain);
+
+	/* EDGE STUFF */
+	void ComputeDeformedEdgeBulkBonds(const dSymMatrixT& strain);
+	void ComputeDeformedEdge1Bonds(const dSymMatrixT& strain);
+	void ComputeDeformedEdge2Bonds(const dSymMatrixT& strain);
+	void ComputeDeformedEdge3Bonds(const dSymMatrixT& strain);
+	void ComputeDeformedEdge4Bonds(const dSymMatrixT& strain);
+	void ComputeDeformedEdge5Bonds(const dSymMatrixT& strain);
+	void ComputeDeformedEdge6Bonds(const dSymMatrixT& strain);
+	void ComputeDeformedEdge7Bonds(const dSymMatrixT& strain);
+	void ComputeDeformedEdge8Bonds(const dSymMatrixT& strain);
 
 protected:
 
@@ -82,32 +118,82 @@ protected:
 	iArrayT     fSurf1Counts;
 	iArrayT     fSurf2Counts;
 	iArrayT 	fSurf3Counts;
+	iArrayT		fBulkBondCounts;	// EDGE
+	iArrayT		fEdge1Counts;
+	iArrayT		fEdge2Counts;
+	iArrayT		fEdge3Counts;
+	iArrayT		fEdge4Counts;
+	iArrayT		fEdge5Counts;
+	iArrayT		fEdge6Counts;
+	iArrayT		fEdge7Counts;
+	iArrayT		fEdge8Counts;	
 	dArray2DT	fBonds;			/* undeformed bond vector components */
 	dArray2DT   fBulkBonds;		/* undeformed bond lengths for a representative bulk atom */
 	dArray2DT   fSurf1Bonds;    /* undeformed bond lengths for a representative surface atom */
 	dArray2DT   fSurf2Bonds;    /* undeformed bond lengths for a representative 1 layer into the bulk atom */
 	dArray2DT 	fSurf3Bonds;	/* undeformed bond lengths for a representative 2 layer into the bulk atom */
+	dArray2DT  	fEdgeBonds1;	/* undeformed bond lengths for type-1 edge atom */
+	dArray2DT  	fEdgeBonds2;	/* undeformed bond lengths for type-2 edge atom */
+	dArray2DT  	fEdgeBonds3;	/* undeformed bond lengths for type-3 edge atom */
+	dArray2DT  	fEdgeBonds4;	/* undeformed bond lengths for type-4 edge atom */
+	dArray2DT  	fEdgeBonds5;	/* undeformed bond lengths for type-5 edge atom */
+	dArray2DT  	fEdgeBonds6;	/* undeformed bond lengths for type-6 edge atom */
+	dArray2DT  	fEdgeBonds7;	/* undeformed bond lengths for type-7 edge atom */
+	dArray2DT  	fEdgeBonds8;	/* undeformed bond lengths for type-8 edge atom */		
+	dArray2DT	fAllBulkBonds;	/* undeformed bond lengths for a representative bulk atom - edge case */
 	dArrayT 	fDefLength;		/* list of deformed bond lengths */
 	dArrayT     fDefBulk;		/* list of deformed bulk bonds */
 	dArrayT     fDefSurf1;		/* list of deformed surface bonds */
 	dArrayT     fDefSurf2;      /* list of deformed bonds for atom 1 layer into the bulk */
 	dArrayT		fDefSurf3;		/* list of deformed bonds for atom 2 layers into the bulk */
+	dArrayT		fDefBulkLength;	/* list of deformed bulk bonds (edge case) */
+	dArrayT		fDefEdge1;		/* list of deformed type-1 edge atom bond lengths */
+	dArrayT		fDefEdge2;		/* list of deformed type-2 edge atom bond lengths */
+	dArrayT		fDefEdge3;		/* list of deformed type-3 edge atom bond lengths */
+	dArrayT		fDefEdge4;		/* list of deformed type-4 edge atom bond lengths */
+	dArrayT		fDefEdge5;		/* list of deformed type-5 edge atom bond lengths */
+	dArrayT		fDefEdge6;		/* list of deformed type-6 edge atom bond lengths */
+	dArrayT		fDefEdge7;		/* list of deformed type-7 edge atom bond lengths */
+	dArrayT		fDefEdge8;		/* list of deformed type-8 edge atom bond lengths */		
 	dMatrixT	fQ;				/* bond vector transformation matrix */
 	iArrayT     fAtomType;		/* interaction indicator type (0-6) for surface CB */
 	/* 0=s1/s1, 1=s1/s2, 2=s1/bulk, 3=s2/s1, 4=s2/s2, 5=s2/bulk, 6=bulk/bulk */
+	iArrayT 	fEdgeType;		/* interaction indicator type for edge CB */
 			
 	/** \name work space */
 	/*@{*/
 	dArrayT		fBondSh;		/**< shallow bond vector */
 	dArrayT     fBondShB;		/**< shallow bond vector for bulk atom */
+	dArrayT     fBondShS;		/**< shallow bond vector for all surface atoms */
 	dArrayT     fBondShS1;		/**< shallow bond vector for surface atom 1 */
 	dArrayT     fBondShS2;		/**< shallow bond vector for surface atom 2 */
 	dArrayT		fBondShS3;		/**< shallow bond vector for surface atom 3 */
+	dArrayT		fBondShBE;		/**< shallow bond vector for bulk atom for edge CB case */
+	dArrayT		fBondShE;		/**< shallow bond vector for all edge atoms */
+	dArrayT		fBondShE1;		/**< shallow bond vector for type-1 edge atoms */	
+	dArrayT		fBondShE2;		/**< shallow bond vector for type-2 edge atoms */
+	dArrayT		fBondShE3;		/**< shallow bond vector for type-3 edge atoms */
+	dArrayT		fBondShE4;		/**< shallow bond vector for type-4 edge atoms */
+	dArrayT		fBondShE5;		/**< shallow bond vector for type-5 edge atoms */
+	dArrayT		fBondShE6;		/**< shallow bond vector for type-6 edge atoms */
+	dArrayT		fBondShE7;		/**< shallow bond vector for type-7 edge atoms */
+	dArrayT		fBondShE8;		/**< shallow bond vector for type-8 edge atoms */	
 	dArrayT 	fBondDp;		/**< deep bond vector */
+	dArrayT     fBondDpS;		/**< deep bond vector for all surface atoms */
 	dArrayT     fBondDpB;		/**< deep bond vector for bulk atom */
 	dArrayT     fBondDpS1;		/**< deep bond vector for surface atom 1 */
 	dArrayT     fBondDpS2;		/**< deep bond vector for surface atom 2 */
 	dArrayT		fBondDpS3;		/**< deep bond vector for surface atom 3 */
+	dArrayT		fBondDpBE;		/**< deep bond vector for bulk atom (edge CB case) */
+	dArrayT		fBondDpE;		/**< deep bond vector for all edge atoms */
+	dArrayT		fBondDpE1;		/**< deep bond vector for type-1 edge atoms */
+	dArrayT		fBondDpE2;		/**< deep bond vector for type-2 edge atoms */
+	dArrayT		fBondDpE3;		/**< deep bond vector for type-3 edge atoms */
+	dArrayT		fBondDpE4;		/**< deep bond vector for type-4 edge atoms */
+	dArrayT		fBondDpE5;		/**< deep bond vector for type-5 edge atoms */
+	dArrayT		fBondDpE6;		/**< deep bond vector for type-6 edge atoms */
+	dArrayT		fBondDpE7;		/**< deep bond vector for type-7 edge atoms */
+	dArrayT		fBondDpE8;		/**< deep bond vector for type-8 edge atoms */		
 //	dMatrixT	fLatDimMatrix;	/**< matrix with same dimensions as lattice */
 	dSymMatrixT	fStrain;		/**< needed if LatticeDim != SpatialDim */  		
 	dSymMatrixT	fStretch;		/**< stretch tensor */
@@ -120,12 +206,34 @@ inline const iArrayT& BondLatticeT::BulkCounts(void) const { return fBulkCounts;
 inline const iArrayT& BondLatticeT::Surf1Counts(void) const { return fSurf1Counts; }
 inline const iArrayT& BondLatticeT::Surf2Counts(void) const { return fSurf2Counts; }
 inline const iArrayT& BondLatticeT::Surf3Counts(void) const { return fSurf3Counts; }
+inline const iArrayT& BondLatticeT::EdgeBulkCounts(void) const { return fBulkBondCounts; }
+inline const iArrayT& BondLatticeT::Edge1Counts(void) const { return fEdge1Counts; }
+inline const iArrayT& BondLatticeT::Edge2Counts(void) const { return fEdge2Counts; }
+inline const iArrayT& BondLatticeT::Edge3Counts(void) const { return fEdge3Counts; }
+inline const iArrayT& BondLatticeT::Edge4Counts(void) const { return fEdge4Counts; }
+inline const iArrayT& BondLatticeT::Edge5Counts(void) const { return fEdge5Counts; }
+inline const iArrayT& BondLatticeT::Edge6Counts(void) const { return fEdge6Counts; }
+inline const iArrayT& BondLatticeT::Edge7Counts(void) const { return fEdge7Counts; }
+inline const iArrayT& BondLatticeT::Edge8Counts(void) const { return fEdge8Counts; }
 inline const dArrayT& BondLatticeT::DeformedLengths(void) const { return fDefLength; }
 inline dArrayT& BondLatticeT::DeformedLengths(void) { return fDefLength; }
+inline const dArrayT& BondLatticeT::DeformedSurfaceLengths(void) const { return fDefLength; }
+inline dArrayT& BondLatticeT::DeformedSurfaceLengths(void) {return fDefLength; }
+inline const dArrayT& BondLatticeT::DeformedEdgeLengths(void) const { return fDefLength; }
+inline dArrayT& BondLatticeT::DeformedEdgeLengths(void) {return fDefLength; }
 inline const dArrayT& BondLatticeT::DeformedBulk(void) const { return fDefBulk; }
 inline const dArrayT& BondLatticeT::DeformedSurf1(void) const { return fDefSurf1; }
 inline const dArrayT& BondLatticeT::DeformedSurf2(void) const { return fDefSurf2; }
 inline const dArrayT& BondLatticeT::DeformedSurf3(void) const { return fDefSurf3; }
+inline const dArrayT& BondLatticeT::DeformedEdgeBulk(void) const { return fDefBulkLength; }
+inline const dArrayT& BondLatticeT::DeformedEdge1(void) const { return fDefEdge1; }
+inline const dArrayT& BondLatticeT::DeformedEdge2(void) const { return fDefEdge2; }
+inline const dArrayT& BondLatticeT::DeformedEdge3(void) const { return fDefEdge3; }
+inline const dArrayT& BondLatticeT::DeformedEdge4(void) const { return fDefEdge4; }
+inline const dArrayT& BondLatticeT::DeformedEdge5(void) const { return fDefEdge5; }
+inline const dArrayT& BondLatticeT::DeformedEdge6(void) const { return fDefEdge6; }
+inline const dArrayT& BondLatticeT::DeformedEdge7(void) const { return fDefEdge7; }
+inline const dArrayT& BondLatticeT::DeformedEdge8(void) const { return fDefEdge8; }
 inline const dArray2DT& BondLatticeT::Bonds(void) const { return fBonds; }
 
 } /* namespace Tahoe */
