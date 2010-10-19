7343faa32ee90b7198ba4122593fb594082fe297
isbuga <isbuga@75a0ec24-64db-4a18-816e-f4d92fc68425>
2010-10-19 23:08:49 +0000

    *** empty log message ***

--- src/elements/micromorphic/FSMicromorphic3DT.cpp
+++ src/elements/micromorphic/FSMicromorphic3DT.cpp
@@ -5540,8 +5540,9 @@ for(int i=0;i<3;i++)
 				//summation
 				for(int K=0;K<3;K++)
 				{
-					TFn_5(row,col)+=Fn[i][K]*Finv[K][m]*fIdentity_matrix(l,k);
+					TFn_5(row,col)+=Fn[i][K]*Finv[K][m]*fIdentity_matrix(l,i);
 				}
+				row++;
 			}
 		}
 		col++;
