ce758bc97e284298d095a838506cfd9e6e793343
hspark <hspark@75a0ec24-64db-4a18-816e-f4d92fc68425>
2010-10-29 20:53:32 +0000

    separate calculations for all necessary stress/modulus for ideal dielectric elastomer

--- src/elements/solid/materials/dielectric_elastomer/FSDE_inc.h
+++ src/elements/solid/materials/dielectric_elastomer/FSDE_inc.h
@@ -11,14 +11,20 @@ extern "C" {
  * Mu
  */
 
-/* Gets first and second derivatives of free energy with respect to E */
-void get_dXsi(const double* params, const double *Xsi, const double* Cmat, double* dXsi, double* ddXsi); 
+/* Gets first derivative of free energy with respect to E (electric displacement) */
+void get_dUdE(const double* params, const double *Xsi, const double* Cmat, double* dXsi); 
+
+/* Gets second derivative of free energy with respect to E */
+void get_ddE(const double* params, const double *Xsi, const double* Cmat, double* ddXsi); 
 
 /* function to compute first derivative of free energy wrt to the stretch tensor C */
 void get_dUdC(const double* params, const double *Xsi, const double* Cmat, double* dUdC); 
 
-/* function to compute second derivative of the potential function for both purely mechanical and mixed electromechanical modulus */
-void get_ddC(const double* params, const double *Xsi, const double* Cmat, double* dCdC, double* dCdXsi);
+/* function to compute second derivative of the potential function for mixed electromechanical modulus */
+void get_ddCE(const double* params, const double *Xsi, const double* Cmat, double* dCdXsi);
+
+/* function to compute second derivative of the potential function for both purely mechanical modulus */
+void get_ddC(const double* params, const double *Xsi, const double* Cmat, double* dCdC);
 
 /* function to get the bulk strain energy density - not implemented yet */
 //double get_energy(const double* params, const double *Xsi, const double* Cmat); 
