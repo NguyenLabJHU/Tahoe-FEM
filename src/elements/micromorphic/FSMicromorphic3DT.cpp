c4d94c48e7cb7c8bc860ed29e9a8b7312ab7a6ca
regueiro <regueiro@75a0ec24-64db-4a18-816e-f4d92fc68425>
2011-03-11 18:15:53 +0000

    updating residual printout for local iteration of Delgamma

--- src/elements/micromorphic/FSMicromorphic3DT.cpp
+++ src/elements/micromorphic/FSMicromorphic3DT.cpp
@@ -2541,6 +2541,7 @@ void FSMicromorphic3DT::RHSDriver_monolithic(void)
 
 	    				fs_micromorph3D_out << "Local iteration counter reached maximum number allowed: iter_count = " << iIterationMax << endl;
 	    				fs_micromorph3D_out << "Current relative residual = " << fabs(fYield_function/fYield_function_tr) << endl;
+	    				fs_micromorph3D_out << "Current residual = " << fYield_function << endl;
 		    			fs_micromorph3D_out<< "iter_count = " << iter_count<<endl;
 		    			fs_micromorph3D_out<< "Element Number = "<< e <<endl;
 		    			fs_micromorph3D_out<< "Gauss Point = "<< IP <<endl;
@@ -2557,6 +2558,7 @@ void FSMicromorphic3DT::RHSDriver_monolithic(void)
 
 	    				cout << "Local iteration counter reached maximum number allowed: iter_count = " << iIterationMax << endl;
 	    				cout << "Current relative residual = " << fabs(fYield_function/fYield_function_tr) << endl;
+	    				cout << "Current residual = " << fYield_function << endl;
 	    			//	 ExceptionT::GeneralFail(caller, "Local iteration counter %d reached maximum number allowed %d.",iter_count, iIterationMax);
 	    			}
 
