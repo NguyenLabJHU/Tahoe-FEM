278a82c793e006c556554f705dc35ee1c6a0d5d9
nobody <nobody@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2011-12-01 21:11:41 +0000

    This commit was manufactured by cvs2svn to create branch 'formicromophic'.

--- PSPASES/PSPASES.v1.0.3/TEST/craydep.f
+++
@@ -1,5 +0,0 @@
-	subroutine getarg(iarg,str)
-	integer iarg,i,j
-	character*64 str
-	call pxfgetarg(iarg,str,i,j)
-	end
--- contrib/fortran_to_C/double_c.c
+++
@@ -1,6 +0,0 @@
-void double_it_c(double* a, int* i);
-void double_it_c(double* a, int* i)
-{
-	*a *= 2.0;
-	*i *= 2;
-}
