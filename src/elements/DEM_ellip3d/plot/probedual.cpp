c80ab394444038e60efdda7d94c6b46fc7e22ad0
tahoe.yanb <tahoe.yanb@75a0ec24-64db-4a18-816e-f4d92fc68425>
2014-06-25 07:01:50 +0000

    *** empty log message ***

--- src/elements/DEM_ellip3d/plot/probedual.cpp
+++ src/elements/DEM_ellip3d/plot/probedual.cpp
@@ -7,71 +7,181 @@
 #include <cmath>
 
 using namespace std;
+static const int OWID = 15;
 
 int main(int argc, char *argv[])
 {
-    if(argc < 5)
+  if(argc < 5)
     {
-	cout << endl
-	     << "-- Colletct Dual Particles' Contact Information --" << endl
-	     << "Usage: probedual contact_file_prefix  first_suffix  last_suffix  suffix_increment" << endl
-	     << "   --example: probedual dep_contact 0 100 5" << endl << endl;
-	return -1;
+      cout << endl
+	   << "-- Colletct Dual Particles' Contact Information --" << endl
+	   << "Usage: probedual contact_file_prefix  first_suffix  last_suffix  suffix_increment" << endl
+	   << "   --example: probedual dep_contact 0 100 5" << endl << endl;
+      return -1;
     }	
 
-    int first, last, incre, n, ic;
-    first   = atoi(argv[2]);
-    last    = atoi(argv[3]);
-    incre   = atoi(argv[4]);
+  int first, last, incre, n, ic;
+  first   = atoi(argv[2]);
+  last    = atoi(argv[3]);
+  incre   = atoi(argv[4]);
 
-    ifstream ifs1;
-    ofstream ofs;
-    char filein1[50];
-    char fileout[50];
-    char num[4], s[20];
+  ifstream ifs1;
+  ofstream ofs;
+  char filein1[50];
+  char fileout[50];
+  char num[4], s[20];
 
-    int TotalCntct, ptcl_1, ptcl_2;
-    long double point1_x, point1_y, point1_z, point2_x, point2_y, point2_z;
-    long double radius_1, radius_2, penetration, tang_dispmt, contact_radius, R0, E0, normal_force, shear_force;
+  int TotalCntct;
+  int ptcl_1;
+  int ptcl_2;
+  double point1_x;
+  double point1_y;
+  double point1_z;
+  double point2_x;
+  double point2_y;
+  double point2_z;
+  double radius_1;
+  double radius_2;
+  double penetration;
+  double tangt_disp;
+  double contact_radius;
+  double R0;
+  double E0;
+  double normal_force;
+  double tangt_force;
+  double contact_x;
+  double contact_y;
+  double contact_z;
+  double normal_x;
+  double normal_y;
+  double normal_z;
+  double tangt_x;
+  double tangt_y;
+  double tangt_z;
+  double vibra_t_step;
+  double impact_t_step;
 
-    strcpy(fileout, argv[1]);
-    strcat(fileout, "_dual");
-    ofs.open(fileout);
-    if(!ofs)   { cout<<"ofs stream error!"<<endl; exit(-1);}
-    ofs.setf(ios::scientific, ios::floatfield);
-    ofs<<"  snapshot     ptcl_1     ptcl_2    point1_x        point1_y        point1_z        "
-       <<"point2_x       point2_y        point2_z         radius_1        radius_2      "
-       <<"penetration     tang_dispmt     contact_radius       R0              E0         normal_force     tang_force"<<endl;
+  strcpy(fileout, argv[1]);
+  strcat(fileout, "_dual");
+  ofs.open(fileout);
+  if(!ofs)   { cout<<"ofs stream error!"<<endl; exit(-1);}
+  ofs.setf(ios::scientific, ios::floatfield);
+  ofs << std::setw(OWID) << "snapshot"
+      << std::setw(OWID) << "ptcl_1"
+      << std::setw(OWID) << "ptcl_2"
+      << std::setw(OWID) << "point1_x"
+      << std::setw(OWID) << "point1_y"
+      << std::setw(OWID) << "point1_z"
+      << std::setw(OWID) << "point2_x"
+      << std::setw(OWID) << "point2_y"
+      << std::setw(OWID) << "point2_z"
+      << std::setw(OWID) << "radius_1"
+      << std::setw(OWID) << "radius_2"
+      << std::setw(OWID) << "penetration"
+      << std::setw(OWID) << "tangt_disp"
+      << std::setw(OWID) << "contact_radius"
+      << std::setw(OWID) << "R0"
+      << std::setw(OWID) << "E0"
+      << std::setw(OWID) << "normal_force"
+      << std::setw(OWID) << "tangt_force"
+      << std::setw(OWID) << "contact_x"
+      << std::setw(OWID) << "contact_y"
+      << std::setw(OWID) << "contact_z"
+      << std::setw(OWID) << "normal_x"
+      << std::setw(OWID) << "normal_y"
+      << std::setw(OWID) << "normal_z"
+      << std::setw(OWID) << "tangt_x"
+      << std::setw(OWID) << "tangt_y"
+      << std::setw(OWID) << "tangt_z"
+      << std::setw(OWID) << "vibra_t_step"
+      << std::setw(OWID) << "impact_t_step"
+      << std::endl;
  
-    for(n=first; n<=last; n+=incre)
+  for(n=first; n<=last; n+=incre)
     {
-	sprintf(num, "%03d", n);
-	strcpy(filein1, argv[1]);
-	strcat(filein1, "_");
-	strcat(filein1, num);
+      sprintf(num, "%03d", n);
+      strcpy(filein1, argv[1]);
+      strcat(filein1, "_");
+      strcat(filein1, num);
 
-	ifs1.open(filein1);
-	if(!ifs1)  { cout<<"ifs1 stream error!"<<endl; exit(-1);}
-	ifs1 >> TotalCntct;
-	ifs1 >>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s;
-	if (TotalCntct==0)
-	    ofs <<setw(10)<<n<<setw(10)<<0<<setw(10)<<0<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0
-		<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0
-		<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0<<setw(16)<<0<<endl;
-	else{
-	    for (ic=0;ic<TotalCntct;++ic) {
-		ifs1>>ptcl_1>>ptcl_2>>point1_x>>point1_y>>point1_z>>point2_x>>point2_y>>point2_z
-		    >>radius_1>>radius_2>>penetration>>tang_dispmt>>contact_radius>>R0>>E0>>normal_force>>shear_force;
-		ofs <<setw(10)<<n<<setw(10)<<ptcl_1<<setw(10)<<ptcl_2<<setw(16)<<point1_x<<setw(16)<<point1_y<<setw(16)<<point1_z
-		    <<setw(16)<<point2_x<<setw(16)<<point2_y<<setw(16)<<point2_z<<setw(16)<<radius_1<<setw(16)<<radius_2
-		    <<setw(16)<<penetration<<setw(16)<<tang_dispmt<<setw(16)<<contact_radius<<setw(16)<<R0<<setw(16)<<E0
-		    <<setw(16)<<normal_force<<setw(16)<<shear_force<<endl;
-	    }
-	    
+      ifs1.open(filein1);
+      if(!ifs1)  { cout<<"ifs1 stream error!"<<endl; exit(-1);}
+      ifs1 >> TotalCntct;
+      ifs1 >>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s
+	   >>s>>s>>s>>s>>s>>s>>s>>s>>s>>s>>s;
+      if (TotalCntct==0)
+	ofs <<setw(OWID)<<n<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0
+	    <<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0
+	    <<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0
+	    <<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0
+	    <<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<setw(OWID)<<0<<endl;
+      else{
+	for (ic=0;ic<TotalCntct;++ic) {
+	  ifs1 >> ptcl_1
+	       >> ptcl_2
+	       >> point1_x
+	       >> point1_y
+	       >> point1_z
+	       >> point2_x
+	       >> point2_y
+	       >> point2_z
+	       >> radius_1
+	       >> radius_2
+	       >> penetration
+	       >> tangt_disp
+	       >> contact_radius
+	       >> R0
+	       >> E0
+	       >> normal_force
+	       >> tangt_force
+	       >> contact_x
+	       >> contact_y
+	       >> contact_z
+	       >> normal_x
+	       >> normal_y
+	       >> normal_z
+	       >> tangt_x
+	       >> tangt_y
+	       >> tangt_z
+	       >> vibra_t_step
+	       >> impact_t_step;
+
+	  ofs << std::setw(OWID) << n
+	      << std::setw(OWID) << ptcl_1       
+	      << std::setw(OWID) << ptcl_2       
+	      << std::setw(OWID) << point1_x     
+	      << std::setw(OWID) << point1_y     
+	      << std::setw(OWID) << point1_z     
+	      << std::setw(OWID) << point2_x     
+	      << std::setw(OWID) << point2_y     
+	      << std::setw(OWID) << point2_z     
+	      << std::setw(OWID) << radius_1     
+	      << std::setw(OWID) << radius_2     
+	      << std::setw(OWID) << penetration  
+	      << std::setw(OWID) << tangt_disp   
+	      << std::setw(OWID) << contact_radius
+	      << std::setw(OWID) << R0           
+	      << std::setw(OWID) << E0           
+	      << std::setw(OWID) << normal_force 
+	      << std::setw(OWID) << tangt_force  
+	      << std::setw(OWID) << contact_x    
+	      << std::setw(OWID) << contact_y    
+	      << std::setw(OWID) << contact_z    
+	      << std::setw(OWID) << normal_x     
+	      << std::setw(OWID) << normal_y     
+	      << std::setw(OWID) << normal_z     
+	      << std::setw(OWID) << tangt_x      
+	      << std::setw(OWID) << tangt_y      
+	      << std::setw(OWID) << tangt_z      
+	      << std::setw(OWID) << vibra_t_step 
+	      << std::setw(OWID) << impact_t_step
+	      << std::endl;
 	}
-	ifs1.close();
+	    
+      }
+      ifs1.close();
     }
-    ofs.close();
+  ofs.close();
 
-    return 0;
+  return 0;
 }
