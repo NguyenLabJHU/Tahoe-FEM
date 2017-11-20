f0c0f2f0d491aae348363b307d68292c9075bc93
tahoe.yanb <tahoe.yanb@75a0ec24-64db-4a18-816e-f4d92fc68425>
2017-11-20 18:31:08 +0000

    get surface centers and vertices

--- src/elements/DEM_ellip3d/paraEllip3d/Rectangle.h
+++ src/elements/DEM_ellip3d/paraEllip3d/Rectangle.h
@@ -10,11 +10,11 @@ namespace dem {
     
   public:
     Rectangle()
-      :dimx(0), dimy(0), dimz(0), center(0), v1(0), v2(0)
+      :dimX(0), dimY(0), dimZ(0), center(0), v1(0), v2(0)
       {}
     
     Rectangle(REAL dx, REAL dy, REAL dz, Vec c)
-      :dimx(dx), dimy(dy), dimz(dz), center(c) {
+      :dimX(dx), dimY(dy), dimZ(dz), center(c) {
       v1.setX( c.getX() - dx/2.0);
       v1.setY( c.getY() - dy/2.0);
       v1.setZ( c.getZ() - dz/2.0);
@@ -24,7 +24,7 @@ namespace dem {
     }
 		     
     Rectangle(REAL dx, REAL dy, REAL dz, Vec ref, int i)
-      :dimx(dx), dimy(dy), dimz(dz), v1(ref){
+      :dimX(dx), dimY(dy), dimZ(dz), v1(ref){
       center.setX(v1.getX() + dx/2.0);
       center.setY(v1.getY() + dy/2.0);
       center.setZ(v1.getZ() + dz/2.0);
@@ -34,7 +34,7 @@ namespace dem {
     }
   
     Rectangle(REAL x1, REAL y1, REAL z1, REAL x2, REAL y2, REAL z2)
-      :dimx(x2-x1), dimy(y2-y1), dimz(z2-z1), v1(x1, y1, z1), v2(x2, y2, z2) {
+      :dimX(x2-x1), dimY(y2-y1), dimZ(z2-z1), v1(x1, y1, z1), v2(x2, y2, z2) {
       center = (v1 + v2) / 2;
     }
 
@@ -42,31 +42,33 @@ namespace dem {
       :v1(_v1), v2(_v2) {
       center = (v1 + v2) / 2;
       Vec vt = v2 - v1;
-      dimx = vt.getX();
-      dimy = vt.getY();
-      dimz = vt.getZ();
+      dimX = vt.getX();
+      dimY = vt.getY();
+      dimZ = vt.getZ();
     }
 
-    REAL getDimx() const {return dimx;}
-    REAL getDimy() const {return dimy;}
-    REAL getDimz() const {return dimz;}
+    REAL getDimX() const {return dimX;}
+    REAL getDimY() const {return dimY;}
+    REAL getDimZ() const {return dimZ;}
     Vec  getCenter() const {return center;}
     Vec  getMinCorner() const {return v1;}
     Vec  getMaxCorner() const {return v2;}
-    REAL getVolume() const {return dimx*dimy*dimz;}
+    REAL getVolume() const {return dimX*dimY*dimZ;}
+    Vec  getSurfaceCenter(int i) const;
+    Vec  getVertice(int i) const;
     
-    void setDimx(REAL dx) {dimx=dx;}
-    void setDimy(REAL dy) {dimy=dy;}
-    void setDimz(REAL dz) {dimz=dz;}
+    void setDimX(REAL dx) {dimX=dx;}
+    void setDimY(REAL dy) {dimY=dy;}
+    void setDimZ(REAL dz) {dimZ=dz;}
     void setCenter(Vec v) {center=v;}
     void setV1(Vec v) {v1=v;}
     void setV2(Vec v) {v2=v;}
     Vec  randomPoint() const;
     void print() const;
     void set(REAL dx, REAL dy, REAL dz, Vec c) {
-      dimx = dx;
-      dimy = dy;
-      dimz = dz;
+      dimX = dx;
+      dimY = dy;
+      dimZ = dz;
       center = c;
       v1.setX( c.getX() - dx/2.0);
       v1.setY( c.getY() - dy/2.0);
@@ -77,9 +79,9 @@ namespace dem {
     }
     
   private:
-    REAL dimx;
-    REAL dimy;
-    REAL dimz;
+    REAL dimX;
+    REAL dimY;
+    REAL dimZ;
     Vec  center;
     Vec  v1; // lower corner of the rectangle, minimum x,y,z value
     Vec  v2; // lower corner of the rectangle, maximum x,y,z value  
@@ -87,9 +89,9 @@ namespace dem {
     friend class boost::serialization::access;
     template<class Archive>
       void serialize(Archive & ar, const unsigned int version) {
-      ar & dimx;
-      ar & dimy;
-      ar & dimz;
+      ar & dimX;
+      ar & dimY;
+      ar & dimZ;
       ar & center;
       ar & v1;
       ar & v2;
