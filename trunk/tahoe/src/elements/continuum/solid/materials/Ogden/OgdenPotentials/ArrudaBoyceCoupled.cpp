3cb090eddfb8256371f275d0d43c684009bad1c7
zl-wang <zl-wang@2ab433c0-bb1a-47cc-9e60-ed0e829e925a>
2021-01-20 21:05:19 +0000

    ArrudaBoyce in decoupled form, for the Hydrogel model

---
+++ trunk/tahoe/src/elements/continuum/solid/materials/Ogden/OgdenPotentials/ArrudaBoyceCoupled.cpp
@@ -0,0 +1,157 @@
+/* $Id: ArrudaBoyce.cpp,v 1.5 2016-03-17 13:44:58 tdnguye Exp $ */
+/* created:   TDN (7/2007) */
+/* Arruda, E.M and Boyce, M.C., JMPS, v41, pp. 389-412*/
+#include "ArrudaBoyceCoupled.h"
+#include "ExceptionT.h"
+
+#include <cmath>
+#include <iostream>
+
+
+using namespace Tahoe;
+const double third = 1.0/3.0;
+
+ArrudaBoyceCoupled::ArrudaBoyceCoupled(void)
+{
+	SetName("arruda-boyce-coupled");
+}
+
+/* set parameters */
+void ArrudaBoyceCoupled::SetKappaMu(double kappa, double mu)
+{
+	fMu = mu;
+	SetKappa(kappa);
+}
+
+void ArrudaBoyceCoupled::DefineParameters(ParameterListT& list) const
+{
+	/* inherited */
+	PotentialT::DefineParameters(list);
+
+	LimitT zero(0.0, LimitT::Lower);
+	LimitT one(1.0, LimitT::Lower);
+
+	ParameterT muN(ParameterT::Double, "network_stiffness");
+	ParameterT lambdaL(ParameterT::Double, "locking_stretch");
+	ParameterT T0(ParameterT::Double, "reference_temperature");
+
+	muN.AddLimit(zero);
+	T0.AddLimit(zero);
+	lambdaL.AddLimit(one);
+
+	list.AddParameter(muN);
+	list.AddParameter(lambdaL);
+	list.AddParameter(T0);
+}
+
+void ArrudaBoyceCoupled::TakeParameterList(const ParameterListT& list)
+{
+	/* inherited */
+	PotentialT::TakeParameterList(list);
+
+	fmuN = list.GetParameter("network_stiffness");
+	flambdaL = list.GetParameter("locking_stretch");
+	fT0 = list.GetParameter("reference_temperature");
+    fMu = fmuN*flambdaL*fLangevin.Function(1.0/flambdaL);
+
+}
+
+double ArrudaBoyceCoupled::Energy(const dArrayT& lambda_bar, const double& J, double temperature)
+{
+  double lam_eff = sqrt(1.0/3.0*(lambda_bar[0]+lambda_bar[1]+lambda_bar[2]));
+  double r = lam_eff/flambdaL;
+  double r0 = 1.0/flambdaL;
+  double x = fLangevin.Function(r);
+  double y = fLangevin.Function(r0);
+  
+  double phi = 0.5*fmuN*(temperature/fT0)*flambdaL*flambdaL*(r*x + log(x/sinh(x)) - 1.0/flambdaL*y - log(y/sinh(y)) - 1.0/flambdaL*y*log(J) );
+  phi += MeanEnergy(J);
+
+  return(phi);
+}
+
+
+/*void ArrudaBoyceCoupled::DevStress(const dArrayT& lambda_bar,dArrayT& tau, double temperature)
+{
+  int nsd = tau.Length();
+  
+  double lam_eff = sqrt(1.0/3.0*(lambda_bar[0]+lambda_bar[1]+lambda_bar[2]));
+  double r = lam_eff/flambdaL;
+  
+  fMu = fmuN*(temperature/fT0)/r*fLangevin.Function(r);
+  
+  const double& l0 = lambda_bar[0];
+  const double& l1 = lambda_bar[1];
+  const double& l2 = lambda_bar[2];
+  
+  tau[0] = fMu*third*(2.0*l0-l1-l2);
+  tau[1] = fMu*third*(2.0*l1-l0-l2);
+  
+  if (nsd == 3)
+    tau[2] = fMu*third*(2.0*l2-l0-l1);
+}*/
+
+void ArrudaBoyceCoupled::DevMod(const dArrayT& lambda_bar, dSymMatrixT& eigenmodulus,  double temperature)
+{
+
+	/*dtau_A/depsilon_B*/
+  int nsd = eigenmodulus.Rows();
+  double ninth = third*third;
+  
+  
+  double lam_eff = sqrt(1.0/3.0*(lambda_bar[0]+lambda_bar[1]+lambda_bar[2]));
+  double r = lam_eff/flambdaL;
+  
+  fMu = fmuN*(temperature/fT0)/r*fLangevin.Function(r);
+  double dMu_dleff = fmuN*(temperature/fT0)/(lam_eff) * (1.0/lam_eff*fLangevin.DFunction(r) - fLangevin.Function(r)/r);
+
+  const double& l0 = lambda_bar[0];
+  const double& l1 = lambda_bar[1];
+  const double& l2 = lambda_bar[2];
+  
+  double coeff = dMu_dleff/lam_eff * third;
+  eigenmodulus[0] = 2.0*fMu*ninth*(4.0*l0+l1+l2) + coeff*ninth*(2.0*l0-l1-l2)*(2.0*l0-l1-l2);
+  eigenmodulus[1] = 2.0*fMu*ninth*(4.0*l1+l2+l0) + coeff*ninth*(2.0*l1-l0-l2)*(2.0*l1-l0-l2);
+  if (nsd == 2)
+  {
+    eigenmodulus[2] = 2.0*fMu*ninth*(-2.0*l0-2.0*l1+l2) + coeff*ninth*(2.0*l0-l1-l2)*(2.0*l1-l0-l2); 
+  }
+  else 
+  {
+    eigenmodulus[2] = 2.0*fMu*ninth*(4.0*l2+l0+l1) + coeff*ninth*(2.0*l2-l0-l1)*(2.0*l2-l0-l1);	
+    eigenmodulus[3] = 2.0*fMu*ninth*(-2.0*l1-2.0*l2+l0) + coeff*ninth*(2.0*l1-l0-l2)*(2.0*l2-l0-l1);
+    eigenmodulus[4] = 2.0*fMu*ninth*(-2.0*l0-2.0*l2+l1) + coeff*ninth*(2.0*l0-l1-l2)*(2.0*l2-l0-l1);
+    eigenmodulus[5] = 2.0*fMu*ninth*(-2.0*l0-2.0*l1+l2) + coeff*ninth*(2.0*l0-l1-l2)*(2.0*l1-l0-l2);
+  }
+}
+
+/* set parameters */
+double ArrudaBoyceCoupled::GetMu(void)
+{
+	return(fMu);
+}
+
+/* Obtain Stresses for Coupled Compressible Form, i.e. the potential is written in terms of original
+    stretches rather than modified (deviatoric) stretches */
+/* Termed DevStress, but it's really the part that's related to shear modulus, not necessarily deviatoric*/
+void ArrudaBoyceCoupled::DevStress(const dArrayT& lambda,dArrayT& tau, double temperature)
+{
+  //The input lambda is the square of principal stretches
+  int nsd = tau.Length();
+  
+  double lam_eff = sqrt(1.0/3.0*(lambda[0]+lambda[1]+lambda[2]));
+  double r = lam_eff/flambdaL;
+  double beta_0 = fLangevin.Function(1.0/flambdaL);
+  double beta   = fLangevin.Function(r);
+
+  
+  const double& l0 = lambda[0];
+  const double& l1 = lambda[1];
+  const double& l2 = lambda[2];
+  
+  tau[0] =  fmuN*flambdaL*third*(l0/lam_eff*beta-beta_0);
+  tau[1] =  fmuN*flambdaL*third*(l1/lam_eff*beta-beta_0);
+
+  if (nsd == 3)
+      tau[2] =  fmuN*(temperature/fT0)*flambdaL*third*(l2/lam_eff*beta-beta_0);
+}
