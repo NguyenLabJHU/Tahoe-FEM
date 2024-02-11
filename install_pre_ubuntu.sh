927f13607691ff01a3d621bd8c86a3581fc62420
bibekananda-datta <datta.bibek11@gmail.com>
2024-02-11 03:58:53 -0500

    Update install_pre_ubuntu.sh

--- install_pre_ubuntu.sh
+++ install_pre_ubuntu.sh
@@ -3,17 +3,21 @@
 ## Please note this file was created in Windows Subsystem for Linux so it might have DOS format
 ## If UNIX conversion is required, then install (Ubuntu): sudo apt install dos2unix
 
-## Before running the script, open bash terminal and create a directory like following
-## mkdir /home/bibek/TAHOE
+
+## Before running the script, open the bash terminal and create a directory like the following
+## mkdir /home/user-name/TAHOE
 ## Copy this file to the above directory and change the directory name below as you created
-## Convert to UNIX format: dos2unix tahoe_pre.sh
-## To give permission: chmod a+x tahoe_pre.sh
-## To run this script: ./tahoe_pre.sh
+## Convert to UNIX format: dos2unix install_pre_ubuntu.sh
+## To give permission: chmod a+x install_pre_ubuntu.sh
+## To run this script: ./install_pre_ubuntu.sh
 
 
-## tahoe main directory: make sure to change this here
-TAHOE_MAIN="/home/bibek/TAHOE"
+## Tahoe main directory: make sure to change this here with your directory
+
+TAHOE_MAIN="/home/user-name/TAHOE"
 cd $TAHOE_MAIN
+
+
 ## Installing pre-requisite packages 
 ## Ubuntu already comes with some of these packages installed
 sudo apt update && sudo apt upgrade
@@ -42,16 +46,17 @@ sudo apt install cvs -y
 sudo apt dist-upgrade
 
 
-
 ## Installing SEACAS
+
 echo MAIN TAHOE INSTALLATION DIRECTORY IS: $PWD
-git clone https://github.com/gsjaardema/seacas.git
+git clone https://github.com/sandialabs/seacas.git
 cd seacas
 ./install-tpl.sh
 mkdir build && cd build
-# use cmake-exodus instead of cmake-config for exodus file format to be used in TAHOE
+# use cmake-exodus instead of cmake-config for the exodus file format to be used in TAHOE
 ../cmake-exodus
 make && make install
+
 
 
 ## Add environment variables to bash profile
@@ -62,11 +67,11 @@ echo export LD_LIBRARY_PATH=$ACCESS/lib >> ~/.bashrc
 
 source ~/.bashrc
 
-# Soft link needs to be created (only for first time compilation)
-# It's needed for the modules that depends on SEACAS
-# If you make changes to any of those modules then this will be rquired
+# Soft link needs to be created (only for first-time compilation)
+# It's needed for the modules that depend on SEACAS
+# If you make changes to any of those modules then this will be required
 cd $ACCESS
-# this might have been already done
+## this might have been already done
 ln -s include/ inc
 cd lib
 ln -s libexodus.dylib libexoIIv2c.dylib
